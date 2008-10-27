3c242a53d975ea378f812c5a51ddf7d8fe932cc8
yamamoto@kyms.ne.jp <yamamoto@kyms.ne.jp@0ada2093-eb46-0410-9c1d-63be55e7407d>
2008-10-27 14:50:56 +0000

    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@52 0ada2093-eb46-0410-9c1d-63be55e7407d

--- language-set/assets/plugins/tinymce303/lang/japanese-utf8.inc.php
+++ language-set/assets/plugins/tinymce303/lang/japanese-utf8.inc.php
@@ -15,8 +15,8 @@ $_lang['tinymce_editor_custom_plugins_title'] = "利用プラグイン(カスタ
 $_lang['tinymce_editor_custom_plugins_message'] = "テーマ設定を「カスタム」とした時の利用プラグインをここで設定します。利用できるプラグインについてはTinyMCE開発元のドキュメントを参照してください。";
 $_lang['tinymce_editor_custom_buttons_title'] = "利用するボタン(カスタム指定):";
 $_lang['tinymce_editor_custom_buttons_message'] = "テーマ設定を「カスタム」とした時のボタンの並びをここで設定します。利用できるボタンについてはTinyMCE開発元のドキュメントを参照してください。";
-$_lang['tinymce_editor_css_selectors_title'] = "CSSスタイルセレクタ:";
-$_lang['tinymce_editor_css_selectors_message'] = "class=xxxxxという形で任意のタグに割り当てる「CSSセレクタ」をここで設定できます。<br />書式：'等幅フォント=mono;小さい文字=smallText'<br />上記のように、複数のスタイルをセミコロンで区切って指定します。最後の項目の後ろにはセミコロンを付けないでください。";
+$_lang['tinymce_editor_css_selectors_title'] = "スタイルリスト:";
+$_lang['tinymce_editor_css_selectors_message'] = "class=xxxxxという形で任意のタグに割り当てるスタイルのリストをここで設定できます。<br />書式：'等幅フォント=mono;小さい文字=smallText'<br />上記のように、複数のスタイルをセミコロンで区切って指定します。最後の項目の後ろにはセミコロンを付けないでください。";
 $_lang['tinymce_settings'] = "TinyMCEの設定";
 $_lang['tinymce_theme_simple'] = "シンプル";
 $_lang['tinymce_theme_advanced'] = "アドバンスド";
