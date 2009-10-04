4caab8eed9e191063f574d0897f1eac68c54afa1
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2009-10-04 15:39:03 +0000

    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@379 0ada2093-eb46-0410-9c1d-63be55e7407d

--- manager/includes/charsets.php
+++
@@ -1,7 +0,0 @@
-<option value="euc-jp" <?php echo $modx_charset=="euc-jp" ? "selected='selected'" : "" ; ?> >Japanese (EUC) - euc-jp</option>
-<option value="iso-2022-jp" <?php echo $modx_charset=="iso-2022-jp" ? "selected='selected'" : "" ; ?> >Japanese (JIS) - iso-2022-jp</option>
-<option value="shift_jis" <?php echo $modx_charset=="shift_jis" ? "selected='selected'" : "" ; ?> >Japanese (Shift-JIS) - shift_jis</option>
-<option value="unicode" <?php echo $modx_charset=="unicode" ? "selected='selected'" : "" ; ?> >Unicode - unicode</option>
-<option value="UTF-8" <?php echo $modx_charset=="UTF-8"? "selected='selected'" : "" ; ?> >Unicode (UTF-8) - utf-8</option>
-<option value="us-ascii" <?php echo $modx_charset=="us-ascii" ? "selected='selected'" : "" ; ?> >US-ASCII - us-ascii</option>
-<option value="iso-8859-1" <?php echo ($modx_charset=="iso-8859-1"  || !isset($modx_charset) /* sets default */) ? "selected='selected'" : "" ; ?> >Western European (ISO) - iso-8859-1</option>
--- manager/includes/version.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$version = '1.0.0J';   /* Current version */
-$code_name = 'rev 5053(jp:r175)';    /* SVN version number */
-$full_appname = 'MODx'.' '.$version.($code_name ? " ($code_name)":"");
-$small_version = '0';
-$patch_level = '0';
