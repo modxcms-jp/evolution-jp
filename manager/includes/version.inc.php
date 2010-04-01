1926a4801f7d54e4f0671486777df7810a470708
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2010-04-01 04:45:16 +0000

    [#MODX-1768] Update version.inc.php for 1.0.3 release
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@434 0ada2093-eb46-0410-9c1d-63be55e7407d

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,19 +1,19 @@
 <?php
 if (!function_exists('getSVNRev')) {
     function getSVNRev() {
-        // SVN property required to be set, e.g. $Rev: 6065 $
-        $svnrev = '$Rev: 6065 $';
+        // SVN property required to be set, e.g. $Rev: 6643 $
+        $svnrev = '$Rev: 6643 $';
         $svnrev = substr($svnrev, 6);
         return intval(substr($svnrev, 0, strlen($svnrev) - 2));
     }
 }
 if (!function_exists('getSVNDate')) {
     function getSVNDate() {
-        // $Date: 2009-11-05 13:28:21 +0900 (Thu, 05 11 2009) $ SVN property required to be set: $Date: 2009-11-05 13:28:21 +0900 (Thu, 05 11 2009) $
-        $svndate = '$Date: 2009-11-05 13:28:21 +0900 (Thu, 05 11 2009) $';
-        $svndate = substr($svndate, 40);
+        // $Date: 2010-04-01 07:14:21 +0900 (Thu, 01 4 2010) $ SVN property required to be set: $Date: 2010-04-01 07:14:21 +0900 (Thu, 01 4 2010) $
+        $svndate = '$Date: 2010-04-01 07:14:21 +0900 (Thu, 01 4 2010) $';
+        $svndate = substr($svndate, 39);
         // need to convert this to a timestamp for using MODx native date format function
-        return strval(substr($svndate, 0, strlen($svndate) - 3));
+        return trim(strval(substr($svndate, 0, strlen($svndate) - 3)));
     }
 }
 
@@ -21,4 +21,4 @@ $modx_version = '1.0.3J'; // Current version
 $modx_branch = 'Evolution';
 $code_name = 'rev '.getSVNRev(); // SVN version number
 $modx_release_date = getSVNDate();
-$modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' (Rev: '.getSVNRev().' Date:'.getSVNDate();
+$modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' (Rev: '.getSVNRev().' Date: '.getSVNDate().')';
