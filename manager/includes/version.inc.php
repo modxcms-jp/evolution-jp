cda254ea3a7c9edb639b5ce8834e1356477c49ad
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2010-04-01 17:22:14 +0000

    [#MODX-1768] Update version.inc.php for 1.0.3 release (simplify further) Fix typo in changelog
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@441 0ada2093-eb46-0410-9c1d-63be55e7407d

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,24 +1,15 @@
 <?php
 if (!function_exists('getSVNRev')) {
     function getSVNRev() {
-        // SVN property required to be set, e.g. $Rev: 6643 $
-        $svnrev = '$Rev: 6643 $';
+        // SVN property required to be set, e.g. $Rev: 6648 $
+        $svnrev = '$Rev: 6648 $';
         $svnrev = substr($svnrev, 6);
         return intval(substr($svnrev, 0, strlen($svnrev) - 2));
     }
 }
-if (!function_exists('getSVNDate')) {
-    function getSVNDate() {
-        // $Date: 2010-04-01 07:14:21 +0900 (Thu, 01 4 2010) $ SVN property required to be set: $Date: 2010-04-01 07:14:21 +0900 (Thu, 01 4 2010) $
-        $svndate = '$Date: 2010-04-01 07:14:21 +0900 (Thu, 01 4 2010) $';
-        $svndate = substr($svndate, 39);
-        // need to convert this to a timestamp for using MODx native date format function
-        return trim(strval(substr($svndate, 0, strlen($svndate) - 3)));
-    }
-}
 
-$modx_version = '1.0.3J'; // Current version
-$modx_branch = 'Evolution';
-$code_name = 'rev '.getSVNRev(); // SVN version number
-$modx_release_date = getSVNDate();
-$modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' (Rev: '.getSVNRev().' Date: '.getSVNDate().')';
+$modx_version = '1.0.3J';           // Current version number
+$modx_release_date = '1 Apr 2010'; // Date of release
+$modx_branch = 'Evolution';        // Codebase name
+$code_name = 'rev '.getSVNRev();   // SVN version number (used mult places)
+$modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' (Rev: '.getSVNRev().' Date: '.$modx_release_date;
