1773f0fb988c4cc923263edfa7162631a60b886e
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2010-07-27 12:18:03 +0000

    version.inc.php更新
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@606 0ada2093-eb46-0410-9c1d-63be55e7407d

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -8,8 +8,8 @@ if (!function_exists('getSVNRev')) {
     }
 }
 
-$modx_version = '1.0.4J';           // Current version number
-$modx_release_date = '8 Jun 2010'; // Date of release
+$modx_version = '1.0.4J-r1';           // Current version number
+$modx_release_date = '27 Jul 2010'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $code_name = 'rev '.getSVNRev();   // SVN version number (used mult places)
 $modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' (Rev: '.getSVNRev().' Date: '.$modx_release_date.')';
