4badeb151642920002439b69a32b5459aced31db
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2010-08-27 04:11:29 +0000

    [#321] トップページ(site_start)をプライベートとして保存できないようにする
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@638 0ada2093-eb46-0410-9c1d-63be55e7407d

--- manager/actions/mutate_content.dynamic.php
+++ manager/actions/mutate_content.dynamic.php
@@ -1079,7 +1079,7 @@ if ($use_udperms == 1) {
 		foreach ($inputAttributes as $k => $v) $inputString[] = $k.'="'.$v.'"';
 
 		// Make the <input> HTML
-		$inputHTML = '<input '.implode(' ', $inputString).' />';
+		$inputHTML = '<input '.implode(' ', $inputString). (($id==$modx->config['site_start']) ? ' disabled' : '') . ' />';
 
 		// does user have this permission?
 		$sql = "SELECT COUNT(mg.id) FROM {$tbl_membergroup_access} mga, {$tbl_member_groups} mg
