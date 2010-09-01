4fcc0f61c8a0afccbd0b70d80871d7f50b5c810a
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2010-09-01 11:05:51 +0000

    新規インストール・アップデートの判定ミス修正
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@676 0ada2093-eb46-0410-9c1d-63be55e7407d

--- install/action.mode.php
+++ install/action.mode.php
@@ -39,7 +39,7 @@ if (file_exists("../manager/includes/config.inc.php")) {
 			<p><strong><?php echo $_lang['installation_install_new_note']?></strong></p>
 		</div>
 	</div>
-	<div style="margin:0;padding:0;<?php if($upgradeable !== 1 || $upgradeable !== 2) echo 'display:none;'; ?>">
+	<div style="margin:0;padding:0;<?php if($upgradeable !== 1 && $upgradeable !== 2) echo 'display:none;'; ?>">
 	<hr />
 	<div>
 		<div class="installImg"><img src="img/install_upg.png" alt="upgrade existing install" /></div>
