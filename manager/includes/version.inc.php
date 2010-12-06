fc6df112f48693fdae7196da4d746a639622ebd7
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2010-12-06 09:29:55 +0000

    バージョンを1.0.4J-r5に更新
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@845 0ada2093-eb46-0410-9c1d-63be55e7407d

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.4J-r4';           // Current version number
-$modx_release_date = '2010/11/1'; // Date of release
+$modx_version = '1.0.4J-r5';           // Current version number
+$modx_release_date = '2010/12/6'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
