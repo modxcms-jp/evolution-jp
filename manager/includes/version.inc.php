db1331404be3a0817d7e7f9630553aaaa5b17c26
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2011-03-20 05:50:59 +0000

    バージョン更新
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@1038 0ada2093-eb46-0410-9c1d-63be55e7407d

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.5J-r1';           // Current version number
-$modx_release_date = '2011/02/21'; // Date of release
+$modx_version = '1.0.5J-r2';           // Current version number
+$modx_release_date = '2011/03/21'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
