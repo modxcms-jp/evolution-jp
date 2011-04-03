db006b1bded7d7dbfa7a90207fe5e0e0d171ca36
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2011-04-03 04:17:04 +0000

    breadcrumbsスニペットのテンプレートをチャンクに書けるようにする
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@1075 0ada2093-eb46-0410-9c1d-63be55e7407d

--- assets/snippets/wayfinder/configs/breadcrumb.config.php
+++ assets/snippets/wayfinder/configs/breadcrumb.config.php
@@ -1,21 +1,29 @@
 <?php
-	$hideSubMenus = 1;
+	$hideSubMenus = !isset($hideSubMenus) ? 1 : $hideSubMenus;
+	$ignoreHidden = !isset($ignoreHidden) ? true : $ignoreHidden;
 	
-	if ($modx->config['site_start'] == $modx->documentObject['id']) {
+	if ($modx->config['site_start'] == $modx->documentIdentifier)
+	{
 		$homeLink = '';
-	} else {
-		$homeLink = "<a href=\"{$modx->config['site_url']}\" title=\"home\">Home</a> &raquo; ";
+	}
+	else 
+	{
+		$home = $modx->getDocumentObject('id', $modx->config['site_start']);
+		$home_title = $home['menutitle'] ? $home['menutitle'] : $home['pagetitle'];
+		$homeLink = '<a href="' . $modx->config['site_url'] . '" title="' . $home_title . '">' . $home_title . '</a> &raquo; ';
 	}
 	
-	$outerTpl = "@CODE:<div id=\"breadcrumbnav\">
-		{$homeLink}[+wf.wrapper+]
-	</div>";
+	if ($modx->config['site_start'] !== $modx->documentIdentifier)
+	{
+		$outerTpl = '@CODE:<div id="breadcrumbnav">' . $homeLink . '[+wf.wrapper+]</div>';
+	}
+	else
+	{
+		$outerTpl = '@CODE: ';
+	}
 	
 	$innerTpl = '@CODE:[+wf.wrapper+]';
-	
-	$rowTpl = '@CODE: ';
-	
+	$rowTpl   = '@CODE: ';
+	$hereTpl  = '@CODE:[+wf.linktext+]';
 	$activeParentRowTpl = '@CODE:<a href="[+wf.link+]" title="[+wf.title+]">[+wf.linktext+]</a> &raquo; [+wf.wrapper+]';
-	
-	$hereTpl = '@CODE:[+wf.linktext+]';
 ?>
