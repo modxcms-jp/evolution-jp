13836312cd9e0697ae9183a71412fd814861892c
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2011-04-03 15:25:50 +0000

    Wayfinderのbredcrumbコンフィグを改善。&startIdパラメータの指定を不要にするなど
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@1090 0ada2093-eb46-0410-9c1d-63be55e7407d

--- assets/snippets/wayfinder/configs/breadcrumb.config.php
+++ assets/snippets/wayfinder/configs/breadcrumb.config.php
@@ -1,29 +1,40 @@
 <?php
+////////////////// default settings
 	$hideSubMenus = !isset($hideSubMenus) ? 1 : $hideSubMenus;
 	$ignoreHidden = !isset($ignoreHidden) ? true : $ignoreHidden;
+	$startId      = !isset($startId)      ? 0 : $startId;
 	
-	if ($modx->config['site_start'] == $modx->documentIdentifier)
-	{
-		$homeLink = '';
-	}
-	else 
+////////////////// template
+	$outerTpl = '<div id="breadcrumbnav">[+home+][+wf.wrapper+]</div>';
+	$innerTpl = '[+wf.wrapper+]';
+	$rowTpl   = ' ';
+	$hereTpl  = '[+wf.linktext+]';
+	$delim    = ' &raquo; ';
+	$activeParentRowTpl = '<a href="[+wf.link+]" title="[+wf.title+]">[+wf.linktext+]</a>[+delim+][+wf.wrapper+]';
+	
+////////////////// build
+	$activeParentRowTpl = str_replace('[+delim+]',$delim,$activeParentRowTpl);
+	if ($modx->config['site_start'] !== $modx->documentIdentifier)
 	{
 		$home = $modx->getDocumentObject('id', $modx->config['site_start']);
 		$home_title = $home['menutitle'] ? $home['menutitle'] : $home['pagetitle'];
-		$homeLink = '<a href="' . $modx->config['site_url'] . '" title="' . $home_title . '">' . $home_title . '</a> &raquo; ';
+		$homeLink = '<a href="' . $modx->config['site_url'] . '" title="' . $home_title . '">' . $home_title . '</a>' . $delim;
+	}
+	else 
+	{
+		$homeLink = '';
 	}
 	
 	if ($modx->config['site_start'] !== $modx->documentIdentifier)
 	{
-		$outerTpl = '@CODE:<div id="breadcrumbnav">' . $homeLink . '[+wf.wrapper+]</div>';
+		$outerTpl = '@CODE:' . str_replace('[+home+]',$homeLink,$outerTpl);
 	}
 	else
 	{
 		$outerTpl = '@CODE: ';
 	}
 	
-	$innerTpl = '@CODE:[+wf.wrapper+]';
-	$rowTpl   = '@CODE: ';
-	$hereTpl  = '@CODE:[+wf.linktext+]';
-	$activeParentRowTpl = '@CODE:<a href="[+wf.link+]" title="[+wf.title+]">[+wf.linktext+]</a> &raquo; [+wf.wrapper+]';
-?>
+	$innerTpl = '@CODE:' . $innerTpl;
+	$rowTpl   = '@CODE:' . $rowTpl;
+	$hereTpl  = '@CODE:' . $hereTpl;
+	$activeParentRowTpl = '@CODE:' . $activeParentRowTpl;
