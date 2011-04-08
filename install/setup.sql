ff7c732ff36cd5a4cd29e51acb79693613caa921
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2011-04-08 09:42:00 +0000

    captcha_wordsを変更
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@1101 0ada2093-eb46-0410-9c1d-63be55e7407d

--- install/setup.sql
+++ install/setup.sql
@@ -752,7 +752,7 @@ INSERT IGNORE INTO `{PREFIX}system_settings`
 ('failed_login_attempts','3'),
 ('blocked_minutes','60'),
 ('use_captcha','0'),
-('captcha_words','isono,fuguta,sazae,masuo,katsuo,wakame,tarao,namihei,fune,tama,mokuzu,umihei,norisuke,taiko,ikura,sakeo,norio,isasaka,hanazawa,hanako,anago'),
+('captcha_words','pyonkichi,hiroshi,kyouko,ume,minami,yoshiko,goriraimo,goro,mogura,machida'),
 ('emailsender','{ADMINEMAIL}'),
 ('emailsubject','ログイン情報のお知らせ'),
 ('number_of_logs','100'),
