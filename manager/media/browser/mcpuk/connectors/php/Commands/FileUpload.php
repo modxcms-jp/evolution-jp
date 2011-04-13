75f23eb3da3a20b2425552fc91bdee2fcdf43fe4
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2011-04-13 06:02:18 +0000

    [#599] ファイルブラウザでのPHP側アップロードエラーのチェック
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@1109 0ada2093-eb46-0410-9c1d-63be55e7407d

--- manager/media/browser/mcpuk/connectors/php/Commands/FileUpload.php
+++ manager/media/browser/mcpuk/connectors/php/Commands/FileUpload.php
@@ -78,7 +78,7 @@ class FileUpload {
 		header ("content-type: text/html");
 		if (sizeof($_FILES)>0) {
 			if (array_key_exists("NewFile",$_FILES)) {
-				if ($_FILES['NewFile']['size']<($typeconfig['MaxSize'])) {	// (*1)
+				if (! $_FILES['NewFile']['error'] && $_FILES['NewFile']['size']<($typeconfig['MaxSize'])) {	// (*1)
 
 //					$filename=basename(str_replace("\\","/",$_FILES['NewFile']['name']));
 					$filename=end(explode('/',str_replace("\\","/",$_FILES['NewFile']['name'])));	// (*2)
