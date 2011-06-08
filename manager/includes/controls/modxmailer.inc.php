1ee0d7cf4b43df1b8954824873570ca643a53e51
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2011-06-08 14:31:12 +0000

    PHPMailerまわり修正・アップデート(1.7.3→2.0.4)
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@1170 0ada2093-eb46-0410-9c1d-63be55e7407d

--- manager/includes/controls/modxmailer.inc.php
+++ manager/includes/controls/modxmailer.inc.php
@@ -3,73 +3,85 @@
  *
  * MODxMailer Class extends PHPMailer
  * Created by ZeRo (http://www.petit-power.com/)
+ * updated by yama (http://kyms.ne.jp/)
  *
  * -----------------------------------------------------
  * [History]
- * Ver1.4.4.6    2009/02/28
+ * Ver1.4.4.7    2011/06/07
  * -----------------------------------------------------
- * Update $Date: 2009-03-01 17:17:18 +0900 (日, 01 3 2009) $ 
- *        $Revision: 59 $
+ * Update $Date: 2011-06-07 23:28:18 +0900 $ 
+ *        $Revision: 61 $
  *******************************************************
  */
 
-if (file_exists(dirname(__FILE__)."/class.phpmailer.php")) {
-	include_once dirname(__FILE__)."/class.phpmailer.php";
-} else {
-	include_once MODX_BASE_PATH . 'manager/includes/controls/class.phpmailer.php';
-}
+include_once MODX_MANAGER_PATH . 'includes/controls/class.phpmailer.php';
 
 /* ------------------------------------------------------------------
  *
- * MODxMailer - PhpMailerの派生クラス
+ * MODxMailer - Extended PHPMailer
  *
  * -----------------------------------------------------------------
  */
+
 class MODxMailer extends PHPMailer
 {
-var $mb_config = null;
-var $charset_array = array(
-            'japanese-utf8' =>
-                array('charset' => 'iso-2022-jp','bit' => '7bit','lang' => 'japanese'),
-            'japanese-euc' =>
-                array('charset' => 'iso-2022-jp','bit' => '7bit','lang' => 'japanese'),
-                
-            );
-
-
-    /*
-     * コンストラクタ(PHP4風）
-     */
-    function MODxMailer()
-	{
-	global $modx;
+	var $mb_language          = null;
+	var $encode_header_method = null;
 	
-        $this->CharSet = $modx->config['modx_charset'];
-
-	    if (array_key_exists($modx->config['manager_language'],$this->charset_array))
+	function MODxMailer()
+	{
+		global $modx;
+		
+		$this->IsMail();
+		$this->From     = $modx->config['emailsender'];
+		$this->FromName = $modx->config['site_name'];
+		
+		switch(strtolower($modx->config['manager_language']))
 		{
-			$this->mb_config = $this->charset_array[$modx->config['manager_language']];
-			$this->CharSet = $this->mb_config['charset'];
-			$this->Encoding = $this->mb_config['bit'];
-        }
+			case 'japanese-utf8':
+			case 'japanese-euc':
+				$this->CharSet     = 'ISO-2022-JP';
+				$this->Encoding    = '7bit';
+				$this->mb_language = 'Japanese';
+				$this->encode_header_method = 'mb_encode_mimeheader';
+				$this->IsHTML(false);
+				break;
+			case 'russian-utf8':
+				$this->CharSet     = 'UTF-8';
+				$this->Encoding    = 'base64';
+				$this->mb_language = 'UNI';
+				break;
+			case 'english':
+				$this->CharSet     = 'iso-8859-1';
+				$this->Encoding    = 'quoted-printable';
+				$this->mb_language = 'English';
+				break;
+			default:
+				$this->CharSet     = 'UTF-8';
+				$this->Encoding    = 'base64';
+				$this->mb_language = 'UNI';
+		}
 	}
-
-    /*
-     * メール送信関数（オーバーライド）
-     */
+	
 	function Send()
 	{
-	global $modx;
-
-		if (!empty($this->mb_config))
-		{	mb_language('ja');
+		global $modx;
+		
+	    if(function_exists(mb_language))
+		{
+			mb_language($this->mb_language);
 			mb_internal_encoding($modx->config['modx_charset']);
-			$this->FromName	= mb_encode_mimeheader(mb_convert_encoding($this->FromName,'JIS',$modx->config['modx_charset']));
-			$this->Subject	= mb_encode_mimeheader(mb_convert_encoding($this->Subject,'JIS',$modx->config['modx_charset']));
-			$this->Body= mb_convert_encoding($this->Body,'JIS',$modx->config['modx_charset']);
+			$this->Body = mb_convert_encoding($this->Body, $this->CharSet, $modx->config['modx_charset']);
 		}
 		return parent::Send();
 	}
+	
+	function EncodeHeader($str, $position = 'text')
+	{
+		if($this->encode_header_method=='mb_encode_mimeheader')
+		{
+			 return mb_encode_mimeheader($str, $this->CharSet, 'B');
+		}
+		else return parent::EncodeHeader($str, $position);
+	}
 }
-
-?>
