e3cf2727a1811266f3260a84c0e5326bd67be152
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2011-10-13 12:09:30 +0000

    ManagerManager - IE7・IE8でmm_renameSection()とmm_hideSections()が正常に機能しないため修正（仮対応)
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@1392 0ada2093-eb46-0410-9c1d-63be55e7407d

--- assets/plugins/managermanager/mm.inc.php
+++ assets/plugins/managermanager/mm.inc.php
@@ -372,17 +372,19 @@ $j(document).ready(function() {
 	try {
 						   					
 	  // Change section index depending on Content History running or not                  
-      var sidx = ($j("div.sectionBody:eq(1)").attr("id") == "ch-body")?1:0;  //ch-body is the CH id name (currently at least)
-      
-      // Give IDs to the sections of the form
-      // This assumes they appear in a certain order
-      $j("div.sectionHeader:eq(sidx)").attr("id", "sectionContentHeader");
-      $j("div.sectionHeader:eq(sidx+1)").attr("id", "sectionTVsHeader");
-   
-      $j("div.sectionBody:eq(sidx+1)").attr("id", "sectionContentBody");
-      $j("div.sectionBody:eq(sidx+2)").attr("id", "sectionTVsBody");						   
-
-	'			
+	if(jQuery.bindReady())
+	{
+		var sidx = ($j("div.sectionBody:eq(1)").attr("id") == "ch-body")?1:0;  //ch-body is the CH id name (currently at least)
+		
+		// Give IDs to the sections of the form
+		// This assumes they appear in a certain order
+		$j("div.sectionHeader:eq(sidx)").attr("id", "sectionContentHeader");
+		$j("div.sectionHeader:eq(sidx+1)").attr("id", "sectionTVsHeader");
+		
+		$j("div.sectionBody:eq(sidx+1)").attr("id", "sectionContentBody");
+		$j("div.sectionBody:eq(sidx+2)").attr("id", "sectionTVsBody");
+	}
+	'
 			);
 	
 	
