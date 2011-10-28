15a053a34c99bb0ed9e63272bdcc8a9208e7f731
yama.kyms <yama.kyms@0ada2093-eb46-0410-9c1d-63be55e7407d>
2011-10-28 12:28:20 +0000

    MODXのタグ(スニペットコールなど)をGPCでまるごと削除せず適当に無害化するように改善
    git-svn-id: http://modx-ja.googlecode.com/svn/trunk@1407 0ada2093-eb46-0410-9c1d-63be55e7407d

--- manager/includes/protect.inc.php
+++ manager/includes/protect.inc.php
@@ -60,45 +60,38 @@ if (@ ini_get('register_globals')) {
         unset ($$key); // unset may not work.
     }
 }
-$modxtags = array (
-    '@<script[^>]*?>.*?</script>@si',
-    '@&#(\d+);@e',
-    '@\[\[(.*?)\]\]@si',
-    '@\[!(.*?)!\]@si',
-    '@\[\~(.*?)\~\]@si',
-    '@\[\((.*?)\)\]@si',
-    '@{{(.*?)}}@si',
-    '@\[\+(.*?)\+\]@si',
-    '@\[\*(.*?)\*\]@si'
-);
 if (!function_exists('modx_sanitize_gpc'))
 {
-	function modx_sanitize_gpc(& $target, $modxtags, $limit= 3)
+	function modx_sanitize_gpc(& $target, $limit= 3)
 	{
+		$tags = array ('@<script[^>]*?>.*?</script>@si','@&#(\d+);@e',);
+		$s = array('[[',']]','[!','!]','[*','*]','[(',')]','{{','}}','[+','+]','[~','~]','[^','^]');
+		$r = array('[ [','] ]','[ !','! ]','[ *','* ]','[ (',') ]','{ {','} }','[ +','+ ]','[ ~','~ ]','[ ^','^ ]');
 		foreach ($target as $key => $value)
 		{
 			if (is_array($value) && $limit > 0)
 			{
-				modx_sanitize_gpc($value, $modxtags, $limit - 1);
+				modx_sanitize_gpc($value, $limit - 1);
 			}
 			else
 			{
-				$target[$key] = preg_replace($modxtags, "", $value);
+				$value = str_replace($s,$r,$value);
+				$target[$key] = preg_replace($tags, '', $value);
 			}
 		}
 		return $target;
 	}
 }
-modx_sanitize_gpc($_GET, $modxtags);
+modx_sanitize_gpc($_GET);
 if (!defined('IN_MANAGER_MODE') || (defined('IN_MANAGER_MODE') && (!IN_MANAGER_MODE || IN_MANAGER_MODE == 'false'))) {
-    modx_sanitize_gpc($_POST, $modxtags);
+    modx_sanitize_gpc($_POST);
 }
-modx_sanitize_gpc($_COOKIE, $modxtags);
-modx_sanitize_gpc($_REQUEST, $modxtags);
+modx_sanitize_gpc($_COOKIE);
+modx_sanitize_gpc($_REQUEST);
 
 foreach (array ('PHP_SELF', 'HTTP_USER_AGENT', 'HTTP_REFERER', 'QUERY_STRING') as $key) {
     $_SERVER[$key] = isset ($_SERVER[$key]) ? htmlspecialchars($_SERVER[$key], ENT_QUOTES) : null;
 }
 
 // Unset vars
-unset ($modxtags, $key, $value);
+unset ($key, $value);
