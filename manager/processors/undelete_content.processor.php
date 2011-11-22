977d2ca3b68cf35cf5b60008e3097dbc43ee565d
yama <yamamoto@kyms.ne.jp>
2011-11-22 21:56:06 +0900

    undelete_content.processor.php コード整理

--- manager/processors/undelete_content.processor.php
+++ manager/processors/undelete_content.processor.php
@@ -1,45 +1,69 @@
 <?php 
 if(IN_MANAGER_MODE!="true") die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODx Content Manager instead of accessing this file directly.");
-if(!$modx->hasPermission('delete_document')) {	
+if(!$modx->hasPermission('delete_document'))
+{
 	$e->setError(3);
-	$e->dumpError();	
+	$e->dumpError();
 }
 
-$id=$_REQUEST['id'];
+$id = intval($_REQUEST['id']);
 
 // check permissions on the document
-include_once "./processors/user_documents_permissions.class.php";
-$udperms = new udperms();
-$udperms->user = $modx->getLoginUserID();
-$udperms->document = $id;
-$udperms->role = $_SESSION['mgrRole'];
+if(!check_group_perm($id)) disp_access_permission_denied();
 
-if(!$udperms->checkPermissions()) {
-	include "header.inc.php";
-	?><div class="sectionHeader"><?php echo $_lang['access_permissions']; ?></div>
-	<div class="sectionBody">
-	<p><?php echo $_lang['access_permission_denied']; ?></p>
-	<?php
-	include("footer.inc.php");
-	exit;	
-}
+$tbl_site_content = $modx->getFullTableName('site_content');
 
 // get the timestamp on which the document was deleted.
-$sql = "SELECT deletedon FROM $dbase.`".$table_prefix."site_content` WHERE $dbase.`".$table_prefix."site_content`.id=".$id." AND deleted=1;";
-$rs = mysql_query($sql);
-$limit = mysql_num_rows($rs);
-if($limit!=1) {
+$where = "id='{$id}' AND deleted=1";
+$rs = $modx->db->select('deletedon',$tbl_site_content,$where);
+if(mysql_num_rows($rs)!=1)
+{
 	echo "Couldn't find document to determine it's date of deletion!";
 	exit;
-} else {
-	$row=mysql_fetch_assoc($rs);
-	$deltime = $row['deletedon'];
+}
+else
+{
+	$deltime = $modx->db->getValue($rs);
 }
 
 $children = array();
+getChildren($id);
 
-function getChildren($parent) {
-	
+$field = array();
+$field['deleted']   = '0';
+$field['deletedby'] = '0';
+$field['deletedon'] = '0';
+
+if(0 < count($children))
+{
+	$docs_to_undelete = implode(' ,', $children);
+	$rs = $modx->db->update($field,$tbl_site_content,"id IN({$docs_to_undelete})");
+	if(!$rs)
+	{
+		echo "Something went wrong while trying to set the document's children to undeleted status...";
+		exit;
+	}
+}
+//'undelete' the document.
+$rs = $modx->db->update($field,$tbl_site_content,"id=$id");
+if(!$rs)
+{
+	echo "Something went wrong while trying to set the document to undeleted status...";
+	exit;
+}
+else
+{
+	// empty cache
+	$modx->clearCache();
+	// finished emptying cache - redirect
+	$header="Location: index.php?r=1&a=7";
+	header($header);
+}
+
+
+
+function getChildren($parent)
+{
 	global $dbase;
 	global $table_prefix;
 	global $children;
@@ -61,32 +85,25 @@ function getChildren($parent) {
 	}
 }
 
-getChildren($id);
+function check_group_perm($id)
+{
+	global $modx;
+	include_once './processors/user_documents_permissions.class.php';
+	$udperms = new udperms();
+	$udperms->user = $modx->getLoginUserID();
+	$udperms->document = $id;
+	$udperms->role = $_SESSION['mgrRole'];
+	return $udperms->checkPermissions();
+}
 
-if(count($children)>0) {
-	$docs_to_undelete = implode(" ,", $children);
-	$sql = "UPDATE $dbase.`".$table_prefix."site_content` SET deleted=0, deletedby=0, deletedon=0 WHERE id IN($docs_to_undelete);";
-	$rs = @mysql_query($sql);
-	if(!$rs) {
-		echo "Something went wrong while trying to set the document's children to undeleted status...";
-		exit;
-	}
-}
-//'undelete' the document.
-$sql = "UPDATE $dbase.`".$table_prefix."site_content` SET deleted=0, deletedby=0, deletedon=0 WHERE id=$id;";
-$rs = mysql_query($sql);
-if(!$rs) {
-	echo "Something went wrong while trying to set the document to undeleted status...";
+function disp_access_permission_denied()
+{
+	global $_lang;
+	include "header.inc.php";
+	?><div class="sectionHeader"><?php echo $_lang['access_permissions']; ?></div>
+	<div class="sectionBody">
+	<p><?php echo $_lang['access_permission_denied']; ?></p>
+	<?php
+	include("footer.inc.php");
 	exit;
-} else {
-	// empty cache
-	include_once "cache_sync.class.processor.php";
-	$sync = new synccache();
-	$sync->setCachepath("../assets/cache/");
-	$sync->setReport(false);
-	$sync->emptyCache(); // first empty the cache		
-	// finished emptying cache - redirect
-	$header="Location: index.php?r=1&a=7";
-	header($header);
 }
-?>
