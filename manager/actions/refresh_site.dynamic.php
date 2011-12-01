4ef8e62ddc2d517baee6ab07e4d1eb49cfcd454d
yama <yamamoto@kyms.ne.jp>
2011-12-01 13:18:40 +0900

    refresh_site.dynamic.php コード整理

--- manager/actions/refresh_site.dynamic.php
+++ manager/actions/refresh_site.dynamic.php
@@ -1,17 +1,15 @@
 <?php
 if(IN_MANAGER_MODE!="true") die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODx Content Manager instead of accessing this file directly.");
 
-// (un)publishing of documents, version 2!
-// first, publish document waiting to be published
 $now = time();
-$tbl_sc = $modx->getFullTableName('site_content');
+$tbl_site_content = $modx->getFullTableName('site_content');
 
-$sql = "UPDATE {$tbl_sc} SET published=1 WHERE pub_date < {$now}   AND pub_date!=0   AND published=0 AND ({$now} < unpub_date or unpub_date=0)";
-$rs = $modx->db->query($sql);
+$where = "pub_date < {$now} AND pub_date!=0 AND published=0 AND ({$now} < unpub_date or unpub_date=0)";
+$rs = $modx->db->update(array('published'=>'1'),$tbl_site_content,$where);
 $num_rows_pub = $modx->db->getAffectedRows();
 
-$sql = "UPDATE {$tbl_sc} SET published=0 WHERE unpub_date < {$now} AND unpub_date!=0 AND published=1";
-$rs = $modx->db->query($sql);
+$where = "unpub_date < {$now} AND unpub_date!=0 AND published=1";
+$rs = $modx->db->update(array('published'=>'0'),$tbl_site_content,$where);
 $num_rows_unpub = $modx->db->getAffectedRows();
 
 ?>
@@ -26,11 +24,8 @@ doRefresh(1);
 if(0<$num_rows_pub)   printf('<p>'.$_lang["refresh_published"].'</p>', $num_rows_pub);
 if(0<$num_rows_unpub) printf('<p>'.$_lang["refresh_unpublished"].'</p>', $num_rows_unpub);
 
-include_once MODX_BASE_PATH . 'manager/processors/cache_sync.class.processor.php';
-$sync = new synccache();
-$sync->setCachepath(MODX_BASE_PATH . 'assets/cache/');
-$sync->setReport(true);
-$sync->emptyCache();
+$params['showReport'] = true;
+$modx->clearCache($params);
 
 // invoke OnSiteRefresh event
 $modx->invokeEvent("OnSiteRefresh");
