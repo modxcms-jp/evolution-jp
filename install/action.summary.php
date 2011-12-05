024fb19bf230ff75bcafaed1e629c6b037d978a1
yama <yamamoto@kyms.ne.jp>
2011-12-05 10:20:00 +0900

    install/action.summary.php コード整理

--- install/action.summary.php
+++ install/action.summary.php
@@ -1,4 +1,7 @@
 <?php
+$upgradephp_path = '../manager/includes/extenders/upgradephp/';
+if (!version_compare('5.0.0', phpversion(),'<')) include_once $upgradephp_path . 'php0500.php';
+
 $installMode = intval($_POST['installmode']);
 echo "<h2>" . $_lang['preinstall_validation'] . "</h2>";
 echo "<h3>" . $_lang['summary_setup_check'] . "</h3>";
@@ -9,116 +12,124 @@ $php_ver_comp = version_compare(phpversion(), "4.4.2");
 $php_ver_comp2 = version_compare(phpversion(), "4.4.9");
 // -1 if left is less, 0 if equal, +1 if left is higher
 if ($php_ver_comp < 0) {
-    echo echo_failed().$_lang['you_running_php'] . phpversion() . $_lang["modx_requires_php"]."</p>";
+    echo echo_failed().$_lang['you_running_php'] . phpversion() . $_lang["modx_requires_php"];
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
-    if ($php_ver_comp2 < 0) {
+    echo echo_ok();
+    if ($php_ver_comp2 < 0 && 0 <= $php_ver_comp) {
         echo "<fieldset>" . $_lang['php_security_notice'] . "</fieldset>";
     }
 }
+echo '</p>';
 // check php register globals off
 echo "<p>" . $_lang['checking_registerglobals'];
 $register_globals = (int) ini_get('register_globals');
 if ($register_globals == '1'){
-    echo echo_failed() . "</p><p><strong>".$_lang['checking_registerglobals_note']."</strong></p>";
+    echo echo_failed() . "</p><p><strong>".$_lang['checking_registerglobals_note']."</strong>";
     // $errors += 1; // comment out for now so we still allow installs if folks are simply stubborn
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // check sessions
 echo "<p>" . $_lang['checking_sessions'];
 if ($_SESSION['test'] != 1) {
-    echo echo_failed() . "</p>";
+    echo echo_failed();
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // check directories
 // cache exists?
 echo "<p>" . $_lang['checking_if_cache_exist'];
 if (!file_exists("../assets/cache") || !file_exists("../assets/cache/rss")) {
-    echo echo_failed() . "</p>";
+    echo echo_failed();
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // cache writable?
 echo "<p>" . $_lang['checking_if_cache_writable'];
 if (!is_writable("../assets/cache") || !file_exists("../assets/media")) {
-    echo echo_failed() . "</p>";
+    echo echo_failed();
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // cache files writable?
 echo "<p>" . $_lang['checking_if_cache_file_writable'];
 if (!file_exists("../assets/cache/siteCache.idx.php")) {
     // make an attempt to create the file
-    @ $hnd = fopen("../assets/cache/siteCache.idx.php", 'w');
-    @ fwrite($hnd, "<?php //MODx site cache file ?>");
-    @ fclose($hnd);
+    file_put_contents('../assets/cache/siteCache.idx.php','<?php //MODx site cache file ?>');
 }
 if (!is_writable("../assets/cache/siteCache.idx.php")) {
-    echo echo_failed() . "</p>";
+    echo echo_failed();
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 echo "<p>".$_lang['checking_if_cache_file2_writable'];
 if (!is_writable("../assets/cache/sitePublishing.idx.php")) {
-    echo echo_failed() . "</p>";
+    echo echo_failed();
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // File Browser directories exists?
 echo "<p>".$_lang['checking_if_images_exist'];
 if (!file_exists("../assets/images") || !file_exists("../assets/files") || !file_exists("../assets/flash") || !file_exists("../assets/media")) {
-    echo echo_failed() . "</p>";
+    echo echo_failed();
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // File Browser directories writable?
 echo "<p>".$_lang['checking_if_images_writable'];
 if (!is_writable("../assets/images") || !is_writable("../assets/files") || !is_writable("../assets/flash") || !is_writable("../assets/media")) {
-    echo echo_failed() . "</p>";
+    echo echo_failed();
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // export exists?
 echo "<p>".$_lang['checking_if_export_exists'];
 if (!file_exists("../assets/export")) {
-    echo echo_failed() . "</p>";
+    echo echo_failed();
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // export writable?
 echo "<p>".$_lang['checking_if_export_writable'];
 if (!is_writable("../assets/export")) {
-    echo echo_failed() . "</p>";
+    echo echo_failed();
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // config.inc.php writable?
 echo "<p>".$_lang['checking_if_config_exist_and_writable'];
 if (!file_exists("../manager/includes/config.inc.php")) {
     // make an attempt to create the file
-    @ $hnd = fopen("../manager/includes/config.inc.php", 'w');
-    @ fwrite($hnd, "<?php //MODx configuration file ?>");
-    @ fclose($hnd);
+    file_put_contents('../manager/includes/config.inc.php','<?php //MODx configuration file ?>');
 }
 $isWriteable = is_writable("../manager/includes/config.inc.php");
 if (!$isWriteable) {
-    echo echo_failed() . "</p><p><strong>".$_lang['config_permissions_note']."</strong></p>";
+    echo echo_failed() . "</p><p><strong>".$_lang['config_permissions_note']."</strong>";
     $errors += 1;
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // connect to the database
 if ($installMode == 1) {
     include "../manager/includes/config.inc.php";
@@ -137,14 +148,15 @@ if ($installMode == 1) {
 echo "<p>".$_lang['creating_database_connection'];
 if (!@ $conn = mysql_connect($database_server, $database_user, $database_password)) {
     $errors += 1;
-    echo echo_failed($_lang['database_connection_failed']) . "<p />".$_lang['database_connection_failed_note']."</p>";
+    echo echo_failed($_lang['database_connection_failed']) . "<p />".$_lang['database_connection_failed_note'];
 } else {
-    echo echo_ok() . "</p>";
+    echo echo_ok();
 }
+echo '</p>';
 // make sure we can use the database
 if ($installMode > 0 && !@ mysql_query("USE {$dbase}")) {
     $errors += 1;
-    echo echo_failed($_lang['database_use_failed']) . "<p />".$_lang["database_use_failed_note"]."</p>";
+    echo echo_failed($_lang['database_use_failed']) . "<p>".$_lang['database_use_failed_note']."</p>";
 }
 
 // check the database collation if not specified in the configuration
@@ -172,8 +184,8 @@ if ($conn && $installMode == 0) {
     echo "<p>" . $_lang['checking_table_prefix'] . $table_prefix . "`: ";
     if ($rs= @ mysql_query("SELECT COUNT(*) FROM $dbase.`" . $table_prefix . "site_content`")) {
         echo echo_failed() . "</b>" . $_lang['table_prefix_already_inuse'] . "</p>";
-        $errors += 1;
         echo "<p>" . $_lang['table_prefix_already_inuse_note'] . "</p>";
+        $errors += 1;
     } else {
         echo echo_ok() . "</p>";
     }
@@ -223,16 +235,10 @@ if ($conn) {
 
 // andrazk 20070416 - add install flag and disable manager login
 // assets/cache writable?
-if (is_writable("../assets/cache")) {
-    if (file_exists('../assets/cache/installProc.inc.php')) {
-        @chmod('../assets/cache/installProc.inc.php', 0755);
-        unlink('../assets/cache/installProc.inc.php');
-    }
 
+if (is_writable("../assets/cache")) {
     // make an attempt to create the file
-    @ $hnd = fopen("../assets/cache/installProc.inc.php", 'w');
-    @ fwrite($hnd, '<?php $installStartTime = '.time().'; ?>');
-    @ fclose($hnd);
+    file_put_contents('../assets/cache/installProc.inc.php','<?php $installStartTime = '.time().'; ?>');
 }
 
 if($installMode > 0 && $_POST['installdata'] == "1") {
@@ -263,39 +269,38 @@ $nextAction= $errors > 0 ? 'summary' : 'install';
 $nextButton= $errors > 0 ? $_lang['retry'] : $_lang['install'];
 $nextVisibility= $errors > 0 || isset($_POST['chkagree']) ? 'visible' : 'hidden';
 $agreeToggle= $errors > 0 ? '' : ' onclick="if(document.getElementById(\'chkagree\').checked){document.getElementById(\'nextbutton\').style.visibility=\'visible\';}else{document.getElementById(\'nextbutton\').style.visibility=\'hidden\';}"';
-?>
-<form name="install" id="install_form" action="index.php?action=<?php echo $nextAction ?>" method="post">
+$trimed_db_name = trim($_POST['database_name'], '`');
+echo <<< EOT
+<form name="install" id="install_form" action="index.php?action={$nextAction}" method="post">
   <div>
-    <input type="hidden" value="<?php echo $install_language?>" name="language" />
-	<input type="hidden" value="<?php echo $manager_language?>" name="managerlanguage" />
-    <input type="hidden" value="<?php echo $installMode ?>" name="installmode" />
-    <input type="hidden" value="<?php echo trim($_POST['database_name'], '`'); ?>" name="database_name" />
-    <input type="hidden" value="<?php echo $_POST['tableprefix'] ?>" name="tableprefix" />
-    <input type="hidden" value="<?php echo $_POST['database_collation'] ?>" name="database_collation" />
-    <input type="hidden" value="<?php echo $_POST['database_connection_charset'] ?>" name="database_connection_charset" />
-    <input type="hidden" value="<?php echo $_POST['database_connection_method'] ?>" name="database_connection_method" />
-    <input type="hidden" value="<?php echo $_POST['databasehost'] ?>" name="databasehost" />
-    <input type="hidden" value="<?php echo $_POST['cmsadmin'] ?>" name="cmsadmin" />
-    <input type="hidden" value="<?php echo $_POST['cmsadminemail'] ?>" name="cmsadminemail" />
-    <input type="hidden" value="<?php echo $_POST['cmspassword'] ?>" name="cmspassword" />
-    <input type="hidden" value="<?php echo $_POST['cmspasswordconfirm'] ?>" name="cmspasswordconfirm" />
-    
+    <input type="hidden" value="{$install_language}" name="language" />
+	<input type="hidden" value="{$manager_language}" name="managerlanguage" />
+    <input type="hidden" value="{$installMode}" name="installmode" />
+    <input type="hidden" value="{$trimed_db_name}" name="database_name" />
+    <input type="hidden" value="{$_POST['tableprefix']}" name="tableprefix" />
+    <input type="hidden" value="{$_POST['database_collation']}" name="database_collation" />
+    <input type="hidden" value="{$_POST['database_connection_charset']}" name="database_connection_charset" />
+    <input type="hidden" value="{$_POST['database_connection_method']}" name="database_connection_method" />
+    <input type="hidden" value="{$_POST['databasehost']}" name="databasehost" />
+    <input type="hidden" value="{$_POST['cmsadmin']}" name="cmsadmin" />
+    <input type="hidden" value="{$_POST['cmsadminemail']}" name="cmsadminemail" />
+    <input type="hidden" value="{$_POST['cmspassword']}" name="cmspassword" />
+    <input type="hidden" value="{$_POST['cmspasswordconfirm']}" name="cmspasswordconfirm" />
     <input type="hidden" value="1" name="options_selected" />
-    
-    <input type="hidden" value="<?php echo $_POST['installdata'] ?>" name="installdata" />
-<?php
+    <input type="hidden" value="{$_POST['installdata']}" name="installdata" />
+EOT;
 $templates = isset ($_POST['template']) ? $_POST['template'] : array ();
-foreach ($templates as $i => $template) echo "<input type=\"hidden\" name=\"template[]\" value=\"$template\" />\n";
+foreach ($templates as $i => $template) echo '<input type="hidden" name="template[]" value="$template" />' . "\n";
 $tvs = isset ($_POST['tv']) ? $_POST['tv'] : array ();
-foreach ($tvs as $i => $tv) echo "<input type=\"hidden\" name=\"tv[]\" value=\"$tv\" />\n";
+foreach ($tvs as $i => $tv) echo '<input type="hidden" name="tv[]" value="$tv" />' . "\n";
 $chunks = isset ($_POST['chunk']) ? $_POST['chunk'] : array ();
-foreach ($chunks as $i => $chunk) echo "<input type=\"hidden\" name=\"chunk[]\" value=\"$chunk\" />\n";
+foreach ($chunks as $i => $chunk) echo '<input type="hidden" name="chunk[]" value="$chunk" />' . "\n";
 $snippets = isset ($_POST['snippet']) ? $_POST['snippet'] : array ();
-foreach ($snippets as $i => $snippet) echo "<input type=\"hidden\" name=\"snippet[]\" value=\"$snippet\" />\n";
+foreach ($snippets as $i => $snippet) echo '<input type="hidden" name="snippet[]" value="$snippet" />' . "\n";
 $plugins = isset ($_POST['plugin']) ? $_POST['plugin'] : array ();
-foreach ($plugins as $i => $plugin) echo "<input type=\"hidden\" name=\"plugin[]\" value=\"$plugin\" />\n";
+foreach ($plugins as $i => $plugin) echo '<input type="hidden" name="plugin[]" value="$plugin" />' . "\n";
 $modules = isset ($_POST['module']) ? $_POST['module'] : array ();
-foreach ($modules as $i => $module) echo "<input type=\"hidden\" name=\"module[]\" value=\"$module\" />\n";
+foreach ($modules as $i => $module) echo '<input type="hidden" name="module[]" value="$module" />' . "\n";
 ?>
 </div>
 
