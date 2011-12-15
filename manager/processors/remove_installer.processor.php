d49bc6b1a0a394a3b0411c4125bfe28897b17615
yama <yamamoto@kyms.ne.jp>
2011-12-15 19:37:36 +0900

    remove_installer.processor.php コード整理

--- manager/processors/remove_installer.processor.php
+++ manager/processors/remove_installer.processor.php
@@ -3,9 +3,9 @@
 /**
  * Installer remover processor
  * --------------------------------
- * This little script will be used by the installer to remove 
- * the install folder from the web root after an install. Having 
- * the install folder present after an install is considered a 
+ * This little script will be used by the installer to remove
+ * the install folder from the web root after an install. Having
+ * the install folder present after an install is considered a
  * security risk
  *
  * This file is mormally called from the installer
@@ -13,28 +13,32 @@
  */
 
 $msg ='';
-$pth = dirname(dirname(dirname(__FILE__)))."/install/";
-$pth = str_replace("\\","/",$pth);
-if(isset($_GET["rminstall"])) {
-	if(is_dir($pth)) {
-		if(!rmdirRecursive($pth)) $msg="An error occured while attempting to remove the install folder";
+$install_dir = dirname(dirname(dirname(__FILE__))).'/install/';
+$install_dir = str_replace('\\','/',$install_dir);
+if(isset($_GET["rminstall"]))
+{
+	if(is_dir($install_dir))
+	{
+		if(!rmdirRecursive($install_dir)) $msg=addslashes('An error occured while attempting to remove the install folder');
 	}
 }
-if($msg) echo "<script>alert('".addslashes($msg)."');</script>";
+if($msg) echo "<script>alert('{$msg}');</script>";
 echo "<script>window.location='../index.php?a=2';</script>";
 
 // rmdirRecursive - detects symbollic links on unix
-function rmdirRecursive($path,$followLinks=false) {   
-   $dir = opendir($path) ;
-   while ($entry = readdir($dir)) {       
-	   if (is_file("$path/$entry") || ((!$followLinks) && is_link("$path/$entry"))) {
-		   @unlink( "$path/$entry" );
-	   }
-	   elseif (is_dir("$path/$entry") && $entry!='.' && $entry!='..') {
-		   rmdirRecursive("$path/$entry"); // recursive
-	   }
-   }
-   closedir($dir);
-   return @rmdir($path);
+function rmdirRecursive($path,$followLinks=false)
+{
+	$dir = opendir($path) ;
+	while ($entry = readdir($dir))
+	{
+	   if (is_file("{$path}/{$entry}") || ((!$followLinks) && is_link("{$path}/{$entry}")))
+		{
+		   @unlink( "{$path}/{$entry}" );
+		}
+	   elseif (is_dir("{$path}/{$entry}") && $entry!='.' && $entry!='..') {
+		   rmdirRecursive("{$path}/{$entry}"); // recursive
+		}
+	}
+	closedir($dir);
+	return @rmdir($path);
 }
-?>
