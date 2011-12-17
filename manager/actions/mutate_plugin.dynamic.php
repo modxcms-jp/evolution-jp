46eeda8ca1dda7f534856db51480400a6298e74a
yama <yamamoto@kyms.ne.jp>
2011-12-17 19:42:53 +0900

    プラグイン編集画面のイベント一覧の選択イベントを分かりやすくアクティブ表示

--- manager/actions/mutate_plugin.dynamic.php
+++ manager/actions/mutate_plugin.dynamic.php
@@ -614,7 +614,7 @@ if(is_array($evtOut)) echo implode("",$evtOut);
 			echo '<tr><td colspan="2"><div class="split" style="margin:10px 0;"></div></td></tr>';
 			echo '<tr><td colspan="2"><b>'.$row['groupname'].'</b></td></tr>';
 		}
-		$evtnames[] = '<input name="sysevents[]" type="checkbox"'. checked(in_array($row[id],$evts)) . ' class="inputBox" value="'.$row['id'].'" id="'.$row['name'].'"/><label for="'.$row['name'].'">'.$row['name'].'</label>'."\n";
+		$evtnames[] = '<input name="sysevents[]" type="checkbox"'. checked(in_array($row[id],$evts)) . ' class="inputBox" value="'.$row['id'].'" id="'.$row['name'].'"/><label for="'.$row['name']. '"' . bold(in_array($row[id],$evts)) . '>'.$row['name'].'</label>'."\n";
 		if(count($evtnames)==2) echoEventRows($evtnames);
 	}
 	if(count($evtnames)>0) echoEventRows($evtnames);
@@ -658,3 +658,8 @@ function checked($cond=false)
 	else return;
 }
 
+function bold($cond=false)
+{
+	if($cond!==false) return ' style="background-color:#777;color:#fff;"';
+	else return;
+}
