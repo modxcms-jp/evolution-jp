73661661db833516e4e6847aed4c4f21dfcbced3
yama <yamamoto@kyms.ne.jp>
2011-12-19 10:47:34 +0900

    sleep()のタイミングを調整(動作上の意味はない)

--- manager/includes/extenders/dbapi.mysql.class.inc.php
+++ manager/includes/extenders/dbapi.mysql.class.inc.php
@@ -90,14 +90,13 @@ class DBAPI {
       $safe_count = 0;
       while(!$this->conn && $safe_count<3)
       {
-      
           if($persist!=0) $this->conn = mysql_pconnect($host, $uid, $pwd);
           else            $this->conn = mysql_connect($host, $uid, $pwd, true);
           
           if(!$this->conn)
           {
-              sleep(1);
               $modx->logEvent(0, 2,'Connect Error','DB');
+              sleep(1);
               $safe_count++;
           }
       }
