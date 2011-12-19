1b317adf6547520bc69c8921b96dd71eb13f230e
yama <yamamoto@kyms.ne.jp>
2011-12-20 03:34:18 +0900

    コード整理

--- manager/processors/move_document.processor.php
+++ manager/processors/move_document.processor.php
@@ -16,8 +16,7 @@ if(strpos($doc_id,','))
 	$doc_ids = explode(',',$doc_id);
 	$doc_id = substr($doc_id,0,strpos($doc_id,','));
 }
-$sql = "SELECT parent FROM {$tbl_site_content} WHERE id='{$doc_id}';";
-$rs = mysql_query($sql);
+$rs = $modx->db->select('parent',$tbl_site_content,"id='{$doc_id}'");
 if(!$rs)
 {
 	echo "An error occured while attempting to find the document's current parent.";
@@ -56,8 +55,7 @@ $children= allChildren($doc_id);
 
 if (!array_search($new_parent, $children))
 {
-	$sql = "UPDATE {$tbl_site_content} SET isfolder=1 WHERE id={$new_parent};";
-	$rs = mysql_query($sql);
+	$rs = $modx->db->update('isfolder=1',$tbl_site_content,"id={$new_parent}");
 	if(!$rs)
 	{
 		echo "An error occured while attempting to change the new parent to a folder.";
@@ -66,8 +64,7 @@ if (!array_search($new_parent, $children))
 	// increase menu index
 	if (is_null($modx->config['auto_menuindex']) || $modx->config['auto_menuindex'])
 	{
-		$sql = "SELECT max(menuindex) FROM {$tbl_site_content} WHERE parent='{$new_parent}'";
-		$menuindex = $modx->db->getValue($sql)+1;
+		$menuindex = $modx->db->getValue($modx->db->select('max(menuindex)',$tbl_site_content,"parent='{$new_parent}'"))+1;
 	}
 	else $menuindex = 0;
 
@@ -87,19 +84,17 @@ if (!array_search($new_parent, $children))
 	}
 
 	// finished moving the document, now check to see if the old_parent should no longer be a folder.
-	$sql = "SELECT count(id) FROM {$tbl_site_content} WHERE parent={$current_parent};";
-	$rs = mysql_query($sql);
+	$rs = $modx->db->select('count(id)',$tbl_site_content,"parent={$current_parent}");
 	if(!$rs)
 	{
 		echo "An error occured while attempting to find the old parents' children.";
 	}
-	$row = mysql_fetch_assoc($rs);
+	$row = $modx->db->getRow($rs);
 	$limit = $row['count(id)'];
 
 	if(!$limit>0)
 	{
-		$sql = "UPDATE {$tbl_site_content} SET isfolder=0 WHERE id={$current_parent};";
-		$rs = mysql_query($sql);
+		$rs = $modx->db->update('isfolder=0',$tbl_site_content,"id={$current_parent}");
 		if(!$rs)
 		{
 			echo 'An error occured while attempting to change the old parent to a regular document.';
@@ -110,31 +105,20 @@ else
 {
 	echo 'You cannot move a document to a child document!';
 }
-empty_cache($id,$new_parent);
+$modx->clearCache();
+if($pid!==0) $header="Location: index.php?a=3&id={$new_parent}&tab=0&r=1";
+else         $header="Location: index.php?a=2&r=1";
+header($header);
 exit;
 
 
-
-function empty_cache($id,$pid)
-{
-	// empty cache & sync site
-	include_once MODX_MANAGER_PATH . 'processors/cache_sync.class.processor.php';
-	$sync = new synccache();
-	$sync->setCachepath(MODX_BASE_PATH . 'assets/cache/');
-	$sync->setReport(false);
-	$sync->emptyCache(); // first empty the cache
-	if($pid!==0) $header="Location: index.php?a=3&id={$pid}&tab=0&r=1";
-	else           $header="Location: index.php?a=2&r=1";
-	header($header);
-}
 
 function allChildren($docid)
 {
 	global $modx;
 	$tbl_site_content = $modx->getFullTableName('site_content');
 	$children= array();
-	$sql = "SELECT id FROM {$tbl_site_content} WHERE parent = {$docid};";
-	if(!$rs = $modx->db->query($sql))
+	if(!$rs = $modx->db->select('id',$tbl_site_content,"parent={$docid}"))
 	{
 		echo "An error occured while attempting to find all of the document's children.";
 	}
@@ -158,8 +142,10 @@ function update_parentid($doc_id,$new_parent,$user_id,$menuindex,$now)
 {
 	global $modx;
 	$tbl_site_content = $modx->getFullTableName('site_content');
-	$sql = "UPDATE {$tbl_site_content} SET parent={$new_parent}, editedby={$user_id}, menuindex={$menuindex} WHERE id={$doc_id};";
-	$rs = mysql_query($sql);
+	$field['parent']    = $new_parent;
+	$field['editedby']  = $user_id;
+	$field['menuindex'] = $menuindex;
+	$rs = $modx->db->update($field,$tbl_site_content,"id={$doc_id}");
 	if(!$rs)
 	{
 		echo "An error occured while attempting to move the document to the new parent.";
