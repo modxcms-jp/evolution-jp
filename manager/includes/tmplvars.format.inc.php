443903bf6708aaea98ac1bbabb2b709e62cc387c
yama <yamamoto@kyms.ne.jp>
2012-01-14 11:14:53 +0900

    コード整形

--- manager/includes/tmplvars.format.inc.php
+++ manager/includes/tmplvars.format.inc.php
@@ -241,48 +241,68 @@ function getTVDisplayFormat($name,$value,$format,$paramstring="",$tvtype="",$doc
 				}
 				$o = htmlentities($value, ENT_NOQUOTES, $modx->config['modx_charset']);
 				break;
-
-        case 'custom_widget':
-            $widget_output = '';
-            $o = '';
-            /* If we are loading a file */
-            if(substr($params['output'], 0, 5) == "@FILE") {
-                $file_name = MODX_BASE_PATH . trim(substr($params['output'], 6));
-                if( !file_exists($file_name) ) {
-                    $widget_output = $file_name . ' does not exist';
-                } else {
-                    $widget_output = file_get_contents($file_name);
-                }
-            } elseif(substr($params['output'], 0, 8) == '@INCLUDE') {
-                $file_name = MODX_BASE_PATH . trim(substr($params['output'], 9));
-                if( !file_exists($file_name) ) {
-                    $widget_output = $file_name . ' does not exist';
-                } else {
-                    /* The included file needs to set $widget_output. Can be string, array, object */
-                    include $file_name;
-                }
-            } elseif(substr($params['output'], 0, 6) == '@CHUNK' && $value !== '') {
-                $chunk_name = trim(substr($params['output'], 7));
-                $widget_output = $modx->getChunk($chunk_name);
-            } elseif(substr($params['output'], 0, 5) == '@EVAL') {
-                $tvname = $name;
-                $eval_str = trim(substr($params['output'], 6));
-                $widget_output = eval($eval_str);
-            } elseif($value !== '') {
-                $widget_output = $params['output'];
-            } else {
-                $widget_output = '';
-            }
-            if(is_string($widget_output)) {
-                $search        = array('[+value+]', '[+tvname+]');
-                $replace       = array($value, $name);
-                $widget_output = str_replace($search, $replace, $widget_output);
-                $o = $modx->parseDocumentSource($widget_output);
-            } else {
-            $o = $widget_output;
-            }
-            break;
-
+			
+			case 'custom_widget':
+				$widget_output = '';
+				$o = '';
+				/* If we are loading a file */
+				if(substr($params['output'], 0, 5) == "@FILE")
+				{
+					$file_name = MODX_BASE_PATH . trim(substr($params['output'], 6));
+					if( !file_exists($file_name) )
+					{
+						$widget_output = $file_name . ' does not exist';
+					}
+					else
+					{
+						$widget_output = file_get_contents($file_name);
+					}
+				}
+				elseif(substr($params['output'], 0, 8) == '@INCLUDE')
+				{
+					$file_name = MODX_BASE_PATH . trim(substr($params['output'], 9));
+					if( !file_exists($file_name) )
+					{
+						$widget_output = $file_name . ' does not exist';
+					}
+					else
+					{
+						/* The included file needs to set $widget_output. Can be string, array, object */
+						include $file_name;
+					}
+				}
+				elseif(substr($params['output'], 0, 6) == '@CHUNK' && $value !== '')
+				{
+					$chunk_name = trim(substr($params['output'], 7));
+					$widget_output = $modx->getChunk($chunk_name);
+				}
+				elseif(substr($params['output'], 0, 5) == '@EVAL')
+				{
+					$tvname = $name;
+					$eval_str = trim(substr($params['output'], 6));
+					$widget_output = eval($eval_str);
+				}
+				elseif($value !== '')
+				{
+					$widget_output = $params['output'];
+				}
+				else
+				{
+					$widget_output = '';
+				}
+				if(is_string($widget_output)) // Except @INCLUDE
+				{
+					$search        = array('[+value+]', '[+tvname+]');
+					$replace       = array($value, $name);
+					$widget_output = str_replace($search, $replace, $widget_output);
+					$o = $modx->parseDocumentSource($widget_output);
+				}
+				else
+				{
+					$o = $widget_output;
+				}
+				break;
+			
 			default:
 				$value = parseInput($value);
 				if($tvtype=='checkbox'||$tvtype=='listbox-multiple') {
