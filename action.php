6aca49d69cb76430448c0cc374e339eae721e7e1
yama <yamamoto@kyms.ne.jp>
2012-01-15 19:04:02 +0900

    action.php - 任意のphpファイルを$_GET指定でinclude

--- action.php
+++ action.php
@@ -5,3 +5,8 @@ require_once("{$base_path}index.php");
 $modx->db->connect();
 $modx->getSettings();
 $modx->invokeEvent('OnWebPageInit');
+if(isset($_GET['include']) && strpos($_GET['include'],'..')===false && file_exists(MODX_BASE_PATH . $_GET['include']))
+{
+	$path = MODX_BASE_PATH . $_GET['include'];
+	include_once($path);
+}
