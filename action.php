29db04f9b4ecad4686b9e16ebea1273c1de1d603
yama <yamamoto@kyms.ne.jp>
2012-01-22 18:40:27 +0900

    action.phpの読み込み対象をmanagerディレクトリ以下の拡張子phpのファイルのみに制限

--- action.php
+++ action.php
@@ -5,8 +5,8 @@ require_once("{$base_path}index.php");
 $modx->db->connect();
 $modx->getSettings();
 $modx->invokeEvent('OnWebPageInit');
-if(isset($_GET['include']) && strpos($_GET['include'],'..')===false && file_exists(MODX_BASE_PATH . $_GET['include']))
+$path = MODX_BASE_PATH . $_GET['include'];
+if(strpos($path,MODX_BASE_PATH . 'manager/')===0 && substr($path,strrpos($path,'.'))==='.php')
 {
-	$path = MODX_BASE_PATH . $_GET['include'];
-	include_once($path);
+	if(file_exists($path)) include_once($path);
 }
