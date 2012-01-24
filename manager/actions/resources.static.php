4f1ac977cf2b488a0704b64f55b8004b8a34e992
yama <yamamoto@kyms.ne.jp>
2012-01-25 08:07:19 +0900

    エレメント一覧のプラグインの並び順を修正

--- manager/actions/resources.static.php
+++ manager/actions/resources.static.php
@@ -115,7 +115,7 @@ function createResourceList($resourceTable,$action,$nameField = 'name')
 	
 	$fields = "{$pluginsql} {$tbl_elm}.{$nameField} as name, {$tbl_elm}.id, {$tbl_elm}.description, {$tbl_elm}.locked, if(isnull({$tbl_categories}.category),'{$_lang['no_category']}',{$tbl_categories}.category) as category";
 	$from   ="{$tbl_elm} left join {$tbl_categories} on {$tbl_elm}.category = {$tbl_categories}.id";
-	$orderby = ($resourceTable == 'site_plugins') ? "6,{$tbl_elm}.disabled ASC,2" : '5,1';
+	$orderby = ($resourceTable == 'site_plugins') ? "{$tbl_elm}.disabled ASC,5,1" : '5,1';
 
 	$rs = $modx->db->select($fields,$from,'',$orderby);
 	$limit = $modx->db->getRecordCount($rs);
@@ -220,7 +220,7 @@ function createCategoryList()
 			$fields .= "{$tbl_elm}.id, description, locked, {$tbl_categories}.category, {$tbl_categories}.id as catid";
 			
 			$from = "{$tbl_elm} left join {$tbl_categories} on {$tbl_elm}.category = {$tbl_categories}.id";
-			$orderby = ($v['table'] == 'site_plugins') ? "{$tbl_elm}.disabled ASC,6,2" : '5,1';
+			$orderby = ($v['table'] == 'site_plugins') ? "{$tbl_elm}.disabled ASC,5,1" : '5,1';
 			$rs = $modx->db->select($fields,$from,'',$orderby);
 			$limit = $modx->db->getRecordCount($rs);
 			if($limit>0)
