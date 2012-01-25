63d8b081fd026ede89625e59c35b0e526d9e9869
yama <yamamoto@kyms.ne.jp>
2012-01-25 18:03:41 +0900

    コード整理

--- manager/includes/categories.inc.php
+++ manager/includes/categories.inc.php
@@ -3,61 +3,67 @@
 //Kyle Jaebker - 08/07/06
 
 //Create a new category
-function newCategory($newCat) {
-    global $modx;
-    $useTable = $modx->getFullTableName('categories');
-    $sql = 'insert into ' . $useTable . ' (category) values (\''.$modx->db->escape($newCat).'\')';
-    $catrs = $modx->db->query($sql);
-    if(!$catrs) {
-        $categoryId = 0;
-    } else {
-        if(!$newCatid=mysql_insert_id()) {
-            $categoryId = 0;
-		} else {
-            $categoryId = $newCatid;
-        }
-    }
-    return $categoryId;
-}
-//check if new category already exists
-function checkCategory($newCat = '') {
-    global $modx;
-    $useTable = $modx->getFullTableName('categories');
-    $sql = 'select * from ' . $useTable . ' order by category';
-    $cats = $modx->db->query($sql);
-    if($cats) while($row = $modx->fetchRow($cats)) {
-        if ($row['category'] == $newCat) {
-            return $row['id'];
-        }
-    }
-    return 0;
-}
-//Get all categories
-function getCategories() {
-    global $modx;
-    $useTable = $modx->getFullTableName('categories');
-    $sql = 'select id, category from ' . $useTable . ' order by category';
-    $cats = $modx->db->query($sql);
-    $resourceArray = array();
-    if($cats) while($row = $modx->fetchRow($cats)) {
-        array_push($resourceArray,array( 'id' => $row['id'], 'category' => stripslashes( $row['category'] ) )); // pixelchutes
-    }
-    return $resourceArray;
-}
-//Delete category & associations
-function deleteCategory($catId=0) {
-    global $modx;
-    if ($catId) {
-        $resetTables = array('site_plugins', 'site_snippets', 'site_htmlsnippets', 'site_templates', 'site_tmplvars', 'site_modules');
-        foreach ($resetTables as $n=>$v) {
-            $useTable = $modx->getFullTableName($v);
-            $sql = 'update ' . $useTable . ' set category=0 where category=' . $catId . '';
-            $modx->db->query($sql);
-        }
-        $catTable = $modx->getFullTableName('categories');
-        $sql = 'delete from ' . $catTable . ' where id=' . $catId;
-        $modx->db->query($sql);
-    }
+function newCategory($newCat)
+{
+	global $modx;
+	$useTable = $modx->getFullTableName('categories');
+	$newCat = $modx->db->escape($newCat);
+	$field['category'] = $newCat;
+	$newCatid = $modx->db->insert($field,$useTable);
+	if(!$newCatid) $newCatid = 0;
+	return $newCatid;
 }
 
-?>
+	//check if new category already exists
+function checkCategory($newCat = '')
+{
+	global $modx;
+	$tbl_categories = $modx->getFullTableName('categories');
+	$rs = $modx->db->select('id,category',$tbl_categories,'','category');
+	if($rs)
+	{
+		while($row = $modx->db->getRow($rs))
+		{
+			if ($row['category'] == $newCat)
+			{
+				return $row['id'];
+			}
+		}
+	}
+	return 0;
+}
+
+	//Get all categories
+	function getCategories()
+	{
+		global $modx;
+		$tbl_categories = $modx->getFullTableName('categories');
+		$cats = $modx->db->select('id, category', $tbl_categories, '', 'category');
+		$resourceArray = array();
+		if($cats)
+		{
+			while($row = $modx->db->getRow($cats))
+			{
+				array_push($resourceArray,array( 'id' => $row['id'], 'category' => stripslashes( $row['category'] ) )); // pixelchutes
+			}
+		}
+		return $resourceArray;
+	}
+
+	//Delete category & associations
+	function deleteCategory($catId=0)
+	{
+		global $modx;
+		if ($catId)
+		{
+			$resetTables = array('site_plugins', 'site_snippets', 'site_htmlsnippets', 'site_templates', 'site_tmplvars', 'site_modules');
+			foreach ($resetTables as $n=>$v)
+			{
+				$tbl = $modx->getFullTableName($v);
+				$field['category'] = '0';
+				$modx->db->update($field, $tbl, "category={$catId}");
+			}
+			$tbl_categories = $modx->getFullTableName('categories');
+			$modx->db->delete($tbl_categories,"id={$catId}");
+		}
+	}
