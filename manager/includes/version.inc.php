cdb638bfde582307a956029111483a00f75d8fef
yama <yamamoto@kyms.ne.jp>
2012-01-28 19:52:28 +0900

    リリース予定日時をセット

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 $modx_version = '1.0.5J-r10';       // Current version number
-$modx_release_date = '2011/12/20'; // Date of release
+$modx_release_date = '2012/2/10'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
