ccc032a9205d2ef4f195326eee03fd8a40f07463
yama <yamamoto@kyms.ne.jp>
2012-01-31 21:22:51 +0900

    eform 本体コードをダイエット

--- assets/snippets/eform/eform.inc.php
+++ assets/snippets/eform/eform.inc.php
@@ -45,6 +45,77 @@
 $GLOBALS['optionsName'] = 'eform'; //name of pseudo attribute used for format settings
 $GLOBALS['efPostBack'] = false;
 
+//tidying up some casing errors in parameters
+if(isset($eformOnValidate)) $eFormOnValidate = $eformOnValidate;
+if(isset($eformOnBeforeMailSent)) $eFormOnBeforeMailSent = $eformOnBeforeMailSent;
+if(isset($eformOnMailSent)) $eFormOnMailSent = $eformOnMailSent;
+if(isset($eformOnValidate)) $eFormOnValidate = $eformOnValidate;
+if(isset($eformOnBeforeFormMerge)) $eFormOnBeforeFormMerge = $eformOnBeforeFormMerge;
+if(isset($eformOnBeforeFormParse)) $eFormOnBeforeFormParse = $eformOnBeforeFormParse;
+//for sottwell :)
+if(isset($eFormCSS)) $cssStyle = $eFormCSS;
+
+# Snippet customize settings
+$from   = (isset($from)) ? $from : $modx->config['emailsender'];
+$formid = (isset($formid)) ? $formid : '';
+$params = array (
+   // Snippet Path
+   'snipPath' => $snipPath, //includes $snip_dir
+	 'snipFolder' => $snip_dir,
+
+// eForm Params
+   'vericode' => isset($vericode)? $vericode:'',
+   'formid' => $formid,
+   'from' => $from,
+   'fromname' => isset($fromname)? $fromname:$modx->config['site_name'],
+   'to' => isset($to)? $to:$modx->config['emailsender'],
+   'cc' => isset($cc)? $cc:'',
+   'bcc' => isset($bcc)? $bcc:'',
+   'subject' => isset($subject)? $subject:'',
+   'ccsender' => isset($ccsender)? $ccsender:0,
+   'sendirect' => isset($sendirect)? $sendirect:0,
+   'mselector' => isset($mailselector)? $mailselector:0,
+   'mobile' => isset($mobile)? $mobile:'',
+   'mobiletext' => isset($mobiletext)? $mobiletext:'',
+   'autosender' => isset($autosender)? $autosender:$from,
+   'autotext' => isset($automessage)? $automessage:'',
+   'category' => isset($category)? $category:0,
+   'keywords' => isset($keywords)? $keywords:'',
+   'gid' => isset($gotoid)? $gotoid:$modx->documentIdentifier,
+   'noemail' => isset($noemail)? ($noemail):false,
+   'saveform' => isset($saveform)? ($saveform? true:false):true,
+   'tpl' => isset($tpl)? $tpl:'',
+   'report' => isset($report)? $report:'',
+   'allowhtml' => isset($allowhtml)? $allowhtml:0,
+   //Added by JJ
+   'replyto' => isset($replyto)? $replyto:'',
+   'language' => isset($language)? $language:$modx->config['manager_language'],
+   'thankyou' => isset($thankyou)? $thankyou:'',
+   'isDebug' => isset($debug)? $debug:0,
+   'reportAbuse' => isset($reportAbuse)? $reportAbuse:false,
+   'disclaimer' => isset($disclaimer)?$disclaimer:'',
+   'sendAsHtml' => isset($sendAsHtml)?$sendAsHtml:false,
+   'sendAsText' => isset($sendAsText)?$sendAsText:false,
+   'sessionVars' => isset($sessionVars)?$sessionVars:false,
+   'postOverides' => isset($postOverides)?$postOverides:0,
+   'eFormOnBeforeMailSent' => isset($eFormOnBeforeMailSent)?$eFormOnBeforeMailSent:'',
+   'eFormOnMailSent' => isset($eFormOnMailSent)?$eFormOnMailSent:'',
+   'eFormOnValidate' => isset($eFormOnValidate)?$eFormOnValidate:'',
+   'eFormOnBeforeFormMerge' => isset($eFormOnBeforeFormMerge)?$eFormOnBeforeFormMerge:'',
+   'eFormOnBeforeFormParse' => isset($eFormOnBeforeFormParse)?$eFormOnBeforeFormParse:'',
+   'cssStyle' => isset($cssStyle)?$cssStyle:'',
+   'jScript' => isset($jScript)?$jScript:'',
+   'submitLimit' => (isset($submitLimit) &&  is_numeric($submitLimit))?$submitLimit*60:0,
+   'protectSubmit' => isset($protectSubmit)?$protectSubmit:1,
+   'requiredClass' => isset($requiredClass)?$requiredClass:"required",
+   'invalidClass' => isset($invalidClass)?$invalidClass:"invalid",
+   'runSnippet' => ( isset($runSnippet) && !is_numeric($runSnippet) )?$runSnippet:'',
+   'autoSenderName' => isset($autoSenderName)?$autoSenderName:''
+);
+
+// pixelchutes PHx workaround
+foreach( $params as $key=>$val ) $params[ $key ] = str_replace( array('((','))'), array('[+','+]'), $val );
+
 function eForm($modx,$params) {
 global $_lang;
 global $debugText;
