c45d0af7d6a40503dd3593e7bb55732898fbe0b1
yama <yamamoto@kyms.ne.jp>
2012-02-05 00:33:41 +0900

    コード整形

--- action.php
+++ action.php
@@ -5,8 +5,9 @@ require_once("{$base_path}index.php");
 $modx->db->connect();
 $modx->getSettings();
 $modx->invokeEvent('OnWebPageInit');
-$path = MODX_BASE_PATH . $_GET['include'];
-if(strpos($path,MODX_BASE_PATH . 'manager/')===0 && substr($path,strrpos($path,'.'))==='.php')
+$path = $_GET['include'];
+if(strpos($path, 'manager/')===0 && substr($path,strrpos($path,'.'))==='.php')
 {
+	$path = MODX_BASE_PATH . $path;
 	if(file_exists($path)) include_once($path);
 }
