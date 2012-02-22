2672b8a6554a3f8380b65a2ba5729e845af09aec
yama <yamamoto@kyms.ne.jp>
2012-02-22 11:38:24 +0900

    変更 $_GET['include']がセットされてない場合は処理を行なわない

--- action.php
+++ action.php
@@ -5,9 +5,12 @@ require_once("{$base_path}index.php");
 $modx->db->connect();
 $modx->getSettings();
 $modx->invokeEvent('OnWebPageInit');
-$path = $_GET['include'];
-if(strpos($path, 'manager/')===0 && substr($path,strrpos($path,'.'))==='.php')
+if(isset($_GET['include']))
 {
-	$path = MODX_BASE_PATH . $path;
-	if(file_exists($path)) include_once($path);
+	$path = $_GET['include'];
+	if(strpos($path, 'manager/')===0 && substr($path,strrpos($path,'.'))==='.php')
+	{
+		$path = MODX_BASE_PATH . $path;
+		if(file_exists($path)) include_once($path);
+	}
 }
