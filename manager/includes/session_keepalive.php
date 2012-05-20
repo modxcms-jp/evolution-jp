66546ba6d731564fd814da1e72c86a9fc7aa76b3
yama <yamamoto@kyms.ne.jp>
2012-05-20 18:05:20 +0900

    コード整形

--- manager/includes/session_keepalive.php
+++ manager/includes/session_keepalive.php
@@ -7,15 +7,13 @@
 require_once(dirname(__FILE__).'/protect.inc.php');
 
 $ok = false;
-if ($rt = @ include_once('config.inc.php')) {
-// Keep it alive
-  startCMSSession();
-  if($_GET['tok'] == md5(session_id())) {
-      $ok = true;
-  }
-}
-if($ok) {
-    echo '{status:"ok"}';
-} else {
-  echo '{status:"null"}';
+if ($rt = @ include_once('config.inc.php'))
+{
+	// Keep it alive
+	startCMSSession();
+	if(isset($_GET['tok']) && $_GET['tok'] == md5(session_id()))
+	{
+		echo '{status:"ok"}';
+	}
+	else echo '{status:"null"}';
 }
