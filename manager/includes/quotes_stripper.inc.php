6c7906c83f6dbbf772389b60480c5d9bcf215db2
yama <yamamoto@kyms.ne.jp>
2012-06-08 20:04:29 +0900

    quotes_stripper.inc.php - 不要な判定を削除

--- manager/includes/quotes_stripper.inc.php
+++ manager/includes/quotes_stripper.inc.php
@@ -1,15 +1,12 @@
 <?php
 // this simulates magic_quotes_gpc off
-if (get_magic_quotes_gpc())
+function reverse_magic_quotes($array)
 {
-	function reverse_magic_quotes($array)
-	{
-		if(is_array($array)) return array_map('reverse_magic_quotes', $array);
-		else                 return stripslashes($array);
-	}
-	
-	$_GET     = reverse_magic_quotes($_GET);
-	$_POST    = reverse_magic_quotes($_POST);
-	$_REQUEST = reverse_magic_quotes($_REQUEST);
-	$_COOKIE  = reverse_magic_quotes($_COOKIE);
+	if(is_array($array)) return array_map('reverse_magic_quotes', $array);
+	else                 return stripslashes($array);
 }
+
+$_GET     = reverse_magic_quotes($_GET);
+$_POST    = reverse_magic_quotes($_POST);
+$_REQUEST = reverse_magic_quotes($_REQUEST);
+$_COOKIE  = reverse_magic_quotes($_COOKIE);
