4c02e4e5ad3ccb6d90833c4209c2702397117db9
yama <yamamoto@kyms.ne.jp>
2012-06-08 16:37:18 +0900

    quotes_stripper.inc.phpシンプルに書き直し

--- manager/includes/quotes_stripper.inc.php
+++ manager/includes/quotes_stripper.inc.php
@@ -1,23 +1,15 @@
 <?php
-
-// this simulates magic_quotes_gpc = 0...
-// Hope it works!
-function & kill_magic_quotes(&$str) {
-   if(is_array($str)) {
-       while(list($key, $val) = each($str)) {
-           $str[$key] = kill_magic_quotes($val); // this basically loops into arrays...
-       }
-   } else {
-       $str = stripslashes($str); // get rid of those slashes!
-   }   
-   return $str;
+// this simulates magic_quotes_gpc off
+if (get_magic_quotes_gpc())
+{
+	function reverse_magic_quotes($array)
+	{
+		if(is_array($array)) return array_map('reverse_magic_quotes', $array);
+		else                 return stripslashes($array);
+	}
+	
+	$_GET     = reverse_magic_quotes($_GET);
+	$_POST    = reverse_magic_quotes($_POST);
+	$_REQUEST = reverse_magic_quotes($_REQUEST);
+	$_COOKIE  = reverse_magic_quotes($_COOKIE);
 }
-
-if(get_magic_quotes_gpc()) {
-   kill_magic_quotes($_GET);
-   kill_magic_quotes($_POST);
-   kill_magic_quotes($_COOKIE);
-   kill_magic_quotes($_REQUEST);
-}
-
-?>
