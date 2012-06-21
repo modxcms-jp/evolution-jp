12327b74e148688da498015e51aa88fe0df6968e
yama <yamamoto@kyms.ne.jp>
2012-06-21 12:29:29 +0900

    index-ajax.php 修正

--- index-ajax.php
+++ index-ajax.php
@@ -3,9 +3,12 @@ if(isset($_GET['q']) && $_GET['q']!=='')       $q = $_GET['q'];
 elseif(isset($_POST['q']) && $_POST['q']!=='') $q = $_POST['q'];
 else exit;
 
-$q = realpath($q) or die(); 
+$q = realpath($q) or die();
+$q = str_replace('\\','/',$q);
 
 define('MODX_API_MODE', true);
 include_once('index.php');
 
-if(strtolower(substr($q,-4))=='.php') include_once($q);
+if(strpos($q, MODX_BASE_PATH . 'assets/snippets/')!==0) exit;
+if(strtolower(substr($q,-4))!=='.php') exit;
+include_once($q);
