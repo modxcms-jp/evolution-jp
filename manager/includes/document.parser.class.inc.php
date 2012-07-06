d01208b5ec4bd766370ad04d245f07c45e7e4aaf
yama <yamamoto@kyms.ne.jp>
2012-07-06 13:49:00 +0900

    ソース整形

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -3483,21 +3483,21 @@ class DocumentParser {
         if ($text != '') {
 
             $errortype= array (
-                E_ERROR => "Error",
-                E_WARNING => "Warning",
-                E_PARSE => "Parsing Error",
-                E_NOTICE => "Notice",
-                E_CORE_ERROR => "Core Error",
-                E_CORE_WARNING => "Core Warning",
-                E_COMPILE_ERROR => "Compile Error",
-                E_COMPILE_WARNING => "Compile Warning",
-                E_USER_ERROR => "User Error",
-                E_USER_WARNING => "User Warning",
-                E_USER_NOTICE => "User Notice",
-                E_STRICT => "E_STRICT",
-                E_RECOVERABLE_ERROR => "E_RECOVERABLE_ERROR",
-                E_DEPRECATED => "E_DEPRECATED",
-                E_USER_DEPRECATED => "E_USER_DEPRECATED"
+                E_ERROR             => "ERROR",
+                E_WARNING           => "WARNING",
+                E_PARSE             => "PARSING ERROR",
+                E_NOTICE            => "NOTICE",
+                E_CORE_ERROR        => "CORE ERROR",
+                E_CORE_WARNING      => "CORE WARNING",
+                E_COMPILE_ERROR     => "COMPILE ERROR",
+                E_COMPILE_WARNING   => "COMPILE WARNING",
+                E_USER_ERROR        => "USER ERROR",
+                E_USER_WARNING      => "USER WARNING",
+                E_USER_NOTICE       => "USER NOTICE",
+                E_STRICT            => "STRICT NOTICE",
+                E_RECOVERABLE_ERROR => "RECOVERABLE ERROR",
+                E_DEPRECATED        => "DEPRECATED",
+                E_USER_DEPRECATED   => "USER DEPRECATED"
             );
 
             $str .= "<tr><td colspan='3'></td></tr><tr><td colspan='3'><b>PHP error debug</b></td></tr>";
