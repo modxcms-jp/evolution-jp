2bf9f8e62b617e7053099ac553af7d9e7399db9b
yama <yamamoto@kyms.ne.jp>
2012-07-27 09:25:23 +0900

    ページ更新時のサイトキャッシュ更新を無条件に行なう

--- manager/processors/save_content.processor.php
+++ manager/processors/save_content.processor.php
@@ -647,12 +647,9 @@ switch ($actionToTake)
 		include "{$base_path}manager/includes/secure_mgr_documents.inc.php";
 		secureMgrDocument($id);
 
-		if($published  != $was['published'])    $clearcache['target'] = 'pagecache,sitecache';
-		elseif($was['alias']==$fields['alias']) $clearcache['target'] = 'pagecache';
-		else                                    $clearcache['target'] = 'pagecache,sitecache';
 		if ($syncsite == 1)
 		{
-			$modx->clearCache($clearcache);
+			$modx->clearCache('pagecache,sitecache');
 		}
 		
 		if ( empty($header) )
