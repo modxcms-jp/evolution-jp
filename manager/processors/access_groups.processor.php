791cc08d962323460f82399bd1a3252ad808a8e7
yama <yamamoto@kyms.ne.jp>
2012-08-05 17:38:21 +0900

    コード整理

--- manager/processors/access_groups.processor.php
+++ manager/processors/access_groups.processor.php
@@ -16,122 +16,117 @@ $tbl_membergroup_access  = $modx->getFullTableName('membergroup_access');
 $tbl_membergroup_names   = $modx->getFullTableName('membergroup_names');
 
 $updategroupaccess = false;
-$operation = $_REQUEST['operation'];
 
-switch ($operation)
+switch ($_REQUEST['operation'])
 {
 	case "add_user_group" :
-		$newgroup = $_REQUEST['newusergroup'];
-		if(empty($newgroup)) {
-			echo "no group name specified";
-			exit;
-		} else {
-			$f['name'] = $modx->db->escape($newgroup);
-			$id = $modx->db->insert_ignore($f,$tbl_membergroup_names);
-			if(!$id) {
-				echo "Failed to insert new group. Possible duplicate group name?";
-				exit;
+		$groupname = $_REQUEST['newusergroup'];
+		if(empty($groupname)) warning("no group name specified");
+		else
+		{
+			$f['name'] = $modx->db->escape($groupname);
+			$groupid = $modx->db->insert_ignore($f,$tbl_membergroup_names);
+			if(!$groupid)
+			{
+				warning("Failed to insert new group. Possible duplicate group name?");
 			}
 			// invoke OnManagerCreateGroup event
 			$modx->invokeEvent('OnManagerCreateGroup', array(
-				'groupid'   => $id,
-				'groupname' => $newgroup,
+				'groupid'   => $groupid,
+				'groupname' => $groupname,
 			));
 		}
 		break;
 	case "add_document_group" :
-		$newgroup = $_REQUEST['newdocgroup'];
-		if(empty($newgroup)) {
-			echo "no group name specified";
-			exit;
-		} else {
-			$f['name'] = $modx->db->escape($newgroup);
-			$id = $modx->db->insert_ignore($f,$tbl_documentgroup_names);
-			if(!$id) {
-				echo "Failed to insert new group. Possible duplicate group name?";
-				exit;
+		$groupname = $_REQUEST['newdocgroup'];
+		if(empty($groupname))
+		{
+			warning("no group name specified");
+		}
+		else
+		{
+			$f['name'] = $modx->db->escape($groupname);
+			$groupid = $modx->db->insert_ignore($f,$tbl_documentgroup_names);
+			if(!$groupid)
+			{
+				warning("Failed to insert new group. Possible duplicate group name?");
 			}
 			
 			// invoke OnCreateDocGroup event
 			$modx->invokeEvent('OnCreateDocGroup', array(
-				'groupid'   => $id,
-				'groupname' => $newgroup,
+				'groupid'   => $groupid,
+				'groupname' => $groupname,
 			));
 		}
 		break;
 	case "delete_user_group" :
 		$updategroupaccess = true;
 		$usergroup = intval($_REQUEST['usergroup']);
-		if(empty($usergroup)) {
-			echo "No user group name specified for deletion";
-			exit;
-		} else {
-			if(!$modx->db->delete($tbl_membergroup_names,"id='{$usergroup}'")) {
-				echo "Unable to delete group. SQL failed.";
-				exit;
+		if(empty($usergroup))
+		{
+			warning("No user group name specified for deletion");
+		}
+		else
+		{
+			if(!$modx->db->delete($tbl_membergroup_names,"id='{$usergroup}'"))
+			{
+				warning("Unable to delete group. SQL failed.");
 			}
-			if(!$modx->db->delete($tbl_membergroup_access,"membergroup='{$usergroup}'")) {
-				echo "Unable to delete group from access table. SQL failed.";
-				exit;
+			if(!$modx->db->delete($tbl_membergroup_access,"membergroup='{$usergroup}'"))
+			{
+				warning("Unable to delete group from access table. SQL failed.");
 			}
-			if(!$modx->db->delete($tbl_member_groups,"user_group='{$usergroup}'")) {
-				echo "Unable to delete user-group links. SQL failed.";
-				exit;
+			if(!$modx->db->delete($tbl_member_groups,"user_group='{$usergroup}'"))
+			{
+				warning("Unable to delete user-group links. SQL failed.");
 			}
 		}
 		break;
 	case "delete_document_group" :
 		$group = intval($_REQUEST['documentgroup']);
-		if(empty($group)) {
-			echo "No document group name specified for deletion";
-			exit;
-		} else {
-			if(!$modx->db->delete($tbl_documentgroup_names,"id='{$group}'")) {
-				echo "Unable to delete group. SQL failed.";
-				exit;
+		if(empty($group))
+		{
+			warning("No document group name specified for deletion");
+		}
+		else
+		{
+			if(!$modx->db->delete($tbl_documentgroup_names,"id='{$group}'"))
+			{
+				warning("Unable to delete group. SQL failed.");
 			}
-			if(!$modx->db->delete($tbl_membergroup_access, "documentgroup='{$group}'")) {
-				echo "Unable to delete group from access table. SQL failed.";
-				exit;
+			if(!$modx->db->delete($tbl_membergroup_access, "documentgroup='{$group}'"))
+			{
+				warning("Unable to delete group from access table. SQL failed.");
 			}
-			if(!$modx->db->delete($tbl_document_groups, "document_group='{$group}'")) {
-				echo "Unable to delete document-group links. SQL failed.";
-				exit;
+			if(!$modx->db->delete($tbl_document_groups, "document_group='{$group}'"))
+			{
+				warning("Unable to delete document-group links. SQL failed.");
 			}
 		}
 		break;
 	case "rename_user_group" :
 		$newgroupname = $modx->db->escape($_REQUEST['newgroupname']);
-		if(empty($newgroupname)) {
-			echo "no group name specified";
-			exit;
-		}
+		if(empty($newgroupname)) warning("no group name specified");
+		
 		$groupid = intval($_REQUEST['groupid']);
-		if(empty($groupid)) {
-			echo "No group id specified";
-			exit;
-		}
+		if(empty($groupid))      warning("No group id specified");
+		
 		$f['name'] = $newgroupname;
-		if(!$modx->db->update($f,$tbl_membergroup_names,"id='{$groupid}'", '', '1')) {
-			echo "Failed to update group name. Possible duplicate group name?";
-			exit;
+		if(!$modx->db->update($f,$tbl_membergroup_names,"id='{$groupid}'", '', '1'))
+		{
+			warning("Failed to update group name. Possible duplicate group name?");
 		}
 		break;
 	case "rename_document_group" :
 		$newgroupname = $modx->db->escape($_REQUEST['newgroupname']);
-		if(empty($newgroupname)) {
-			echo "no group name specified";
-			exit;
-		}
+		if(empty($newgroupname)) warning("no group name specified");
+		
 		$groupid = intval($_REQUEST['groupid']);
-		if(empty($groupid)) {
-			echo "No group id specified";
-			exit;
-		}
+		if(empty($groupid))      warning("No group id specified");
 		$f['name'] = $newgroupname;
-		if(!$modx->db->update($f,$tbl_documentgroup_names,"id='{$groupid}'", '', '1')) {
-			echo "Failed to update group name. Possible duplicate group name?";
-			exit;
+		if(!$modx->db->update($f,$tbl_documentgroup_names,"id='{$groupid}'", '', '1'))
+		{
+			warning("Failed to update group name. Possible duplicate group name?");
 		}
 		break;
 	case "add_document_group_to_user_group" :
@@ -144,32 +139,31 @@ switch ($operation)
 			$f=array();
 			$f['membergroup']   = $usergroup;
 			$f['documentgroup'] = $docgroup;
-			if(!$modx->db->insert_ignore($f,$tbl_membergroup_access)) {
-				echo "Failed to link document group to user group";
-				exit;
+			if(!$modx->db->insert_ignore($f,$tbl_membergroup_access))
+			{
+				warning("Failed to link document group to user group");
 			}
-		} else {
-			echo "User that coupling already exists";
-			exit;
-			//alert user that coupling already exists?
+		}
+		else
+		{
+			warning("User that coupling already exists"); //alert user that coupling already exists?
 		}
 		break;
 	case "remove_document_group_from_user_group" :
 		$updategroupaccess = true;
 		$coupling = intval($_REQUEST['coupling']);
-		$sql = 'DELETE FROM '.$tbl_membergroup_access.' WHERE id='.$coupling;
-		if(!$modx->db->delete($tbl_membergroup_access,"id='{$coupling}'")) {
-			echo "Failed to remove document group from user group";
-			exit;
+		if(!$modx->db->delete($tbl_membergroup_access,"id='{$coupling}'"))
+		{
+			warning("Failed to remove document group from user group");
 		}
 		break;
 	default :
-		echo "No operation set in request.";
-		exit;
+		warning("No operation set in request.");
 }
 
 // secure manager documents - flag as private
-if($updategroupaccess==true){
+if($updategroupaccess==true)
+{
 	include $base_path."manager/includes/secure_mgr_documents.inc.php";
 	secureMgrDocument();
 
@@ -181,3 +175,9 @@ if($updategroupaccess==true){
 }
 
 header("Location: index.php?a=40");
+
+function warning($msg)
+{
+	echo $msg;
+	exit;
+}
