c1bb31732d0d3ca817c792d96569b26c200d0513
yama <yamamoto@kyms.ne.jp>
2012-08-14 15:27:29 +0900

    非推奨の関数を整理

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -115,6 +115,13 @@ class DocumentParser {
 				}
 				else return false;
 				break;
+			case 'DeprecatedAPI':
+				if(include_once(MODX_BASE_PATH . 'manager/includes/extenders/deprecated.functions.inc.php'))
+				{
+					return true;
+				}
+				else return false;
+				break;
 			default :
 				return false;
 		}
@@ -2541,39 +2548,6 @@ class DocumentParser {
 		return $v;
 	}
 
-	function makeList($array,$ulroot='root',$ulprefix='sub_',$type='',$ordered= false,$tablevel= 0)
-	{
-		// first find out whether the value passed is an array
-		if (!is_array($array)) return "<ul><li>Bad list</li></ul>";
-		
-		$tabs= '';
-		for ($i= 0; $i < $tablevel; $i++)
-		{
-			$tabs .= "\t";
-		}
-		
-		$tag = ($ordered == true) ? 'ol' : 'ul';
-		
-		if(!empty($type)) $typestr= " style='list-style-type: {$type}'";
-		else              $typestr= '';
-		
-		$listhtml= "{$tabs}<{$tag} class='{$ulroot}'{$typestr}>\n";
-		foreach ($array as $key => $value)
-		{
-			if (is_array($value))
-			{
-				$line = $this->makeList($value, "{$ulprefix}{$ulroot}", $ulprefix, $type, $ordered, $tablevel +2);
-				$listhtml .= "{$tabs}\t<li>{$key}\n{$line}{$tabs}\t</li>\n";
-			}
-			else
-			{
-				$listhtml .= "{$tabs}\t<li>{$value}</li>\n";
-			}
-		}
-		$listhtml = "{$tabs}</{$tag}>\n";
-		return $listhtml;
-	}
-		
 	function runSnippet($snippetName, $params= array ())
 	{
 		if (isset ($this->snippetCache[$snippetName]))
@@ -3446,134 +3420,21 @@ class DocumentParser {
 	function dbClose()                   {$this->db->disconnect();}
 	
     // deprecated
-    function getUserData()
-    {
-    	$client['host'] = $_SERVER['REMOTE_ADDR'];
-    	$client['ip']   = $_SERVER['REMOTE_ADDR'];
-    	$client['ua']   = $_SERVER['HTTP_USER_AGENT'];
-    	return $client;
-    }
-    
-	function insideManager()
+	function makeList($array,$ulroot='root',$ulprefix='sub_',$type='',$ordered= false,$tablevel= 0)
 	{
-		$m= false;
-		if (defined('IN_MANAGER_MODE') && IN_MANAGER_MODE == 'true')
-		{
-			$m= true;
-			if(defined('SNIPPET_INTERACTIVE_MODE') && SNIPPET_INTERACTIVE_MODE == 'true')
-			{
-				$m= "interact";
-			}
-			elseif(defined('SNIPPET_INSTALL_MODE') && SNIPPET_INSTALL_MODE == 'true')
-			{
-				$m= "install";
-			}
-		}
-		return $m;
+		$this->loadExtension('DeprecatedAPI');
+		return makeList($array,$ulroot,$ulprefix,$type,$ordered,$tablevel);
 	}
-
-    function putChunk($chunkName) {return $this->getChunk($chunkName);}// deprecated alias name >.<
-    function getDocGroups() {return $this->getUserDocGroups();} // deprecated
-	function changePassword($o, $n) {return changeWebUserPassword($o, $n);} // deprecated
-
-    function mergeDocumentMETATags($template) {
-        if ($this->documentObject['haskeywords'] == 1) {
-            // insert keywords
-            $keywords = $this->getKeywords();
-            if (is_array($keywords) && count($keywords) > 0) {
-	            $keywords = implode(", ", $keywords);
-	            $metas= "\t<meta name=\"keywords\" content=\"$keywords\" />\n";
-            }
-
-	    // Don't process when cached
-	    $this->documentObject['haskeywords'] = '0';
-        }
-        if ($this->documentObject['hasmetatags'] == 1) {
-            // insert meta tags
-            $tags= $this->getMETATags();
-            foreach ($tags as $n => $col) {
-                $tag= strtolower($col['tag']);
-                $tagvalue= $col['tagvalue'];
-                $tagstyle= $col['http_equiv'] ? 'http-equiv' : 'name';
-                $metas .= "\t<meta $tagstyle=\"$tag\" content=\"$tagvalue\" />\n";
-            }
-
-	    // Don't process when cached
-	    $this->documentObject['hasmetatags'] = '0';
-        }
-	if (isset($metas) && $metas) $template = preg_replace("/(<head>)/i", "\\1\n\t" . trim($metas), $template);
-        return $template;
-    }
-
-    function getMETATags($id= 0) {
-        if ($id == 0) {
-            $id= $this->documentObject['id'];
-        }
-        $sql= "SELECT smt.* " .
-        "FROM " . $this->getFullTableName("site_metatags") . " smt " .
-        "INNER JOIN " . $this->getFullTableName("site_content_metatags") . " cmt ON cmt.metatag_id=smt.id " .
-        "WHERE cmt.content_id = '$id'";
-        $ds= $this->db->query($sql);
-        $limit= $this->db->getRecordCount($ds);
-        $metatags= array ();
-        if ($limit > 0) {
-            for ($i= 0; $i < $limit; $i++) {
-                $row= $this->db->getRow($ds);
-                $metatags[$row['name']]= array (
-                    "tag" => $row['tag'],
-                    "tagvalue" => $row['tagvalue'],
-                    "http_equiv" => $row['http_equiv']
-                );
-            }
-        }
-        return $metatags;
-    }
-
-	function userLoggedIn()
-	{
-		$userdetails= array ();
-		if ($this->isFrontend() && isset ($_SESSION['webValidated']))
-		{
-			// web user
-			$userdetails['loggedIn']= true;
-			$userdetails['id']= $_SESSION['webInternalKey'];
-			$userdetails['username']= $_SESSION['webShortname'];
-			$userdetails['usertype']= 'web'; // added by Raymond
-			return $userdetails;
-		}
-		elseif($this->isBackend() && isset ($_SESSION['mgrValidated']))
-		{
-			// manager user
-			$userdetails['loggedIn']= true;
-			$userdetails['id']= $_SESSION['mgrInternalKey'];
-			$userdetails['username']= $_SESSION['mgrShortname'];
-			$userdetails['usertype']= 'manager'; // added by Raymond
-			return $userdetails;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-    function getKeywords($id= 0) {
-        if ($id == 0) {
-            $id= $this->documentObject['id'];
-        }
-        $tblKeywords= $this->getFullTableName('site_keywords');
-        $tblKeywordXref= $this->getFullTableName('keyword_xref');
-        $sql= "SELECT keywords.keyword FROM " . $tblKeywords . " AS keywords INNER JOIN " . $tblKeywordXref . " AS xref ON keywords.id=xref.keyword_id WHERE xref.content_id = '$id'";
-        $result= $this->db->query($sql);
-        $limit= $this->db->getRecordCount($result);
-        $keywords= array ();
-        if ($limit > 0) {
-            for ($i= 0; $i < $limit; $i++) {
-                $row= $this->db->getRow($result);
-                $keywords[]= $row['keyword'];
-            }
-        }
-        return $keywords;
-    }
+	
+    function getUserData()          {$this->loadExtension('DeprecatedAPI');return getUserData();}
+	function insideManager()        {$this->loadExtension('DeprecatedAPI');return insideManager();}
+    function putChunk($chunkName)   {return $this->getChunk($chunkName);}
+    function getDocGroups()         {return $this->getUserDocGroups();}
+	function changePassword($o, $n) {return changeWebUserPassword($o, $n);}
+    function getMETATags($id= 0)    {$this->loadExtension('DeprecatedAPI');return getMETATags($id);}
+	function userLoggedIn()         {$this->loadExtension('DeprecatedAPI');return userLoggedIn();}
+    function getKeywords($id= 0)    {$this->loadExtension('DeprecatedAPI');return getKeywords($id);}
+    function mergeDocumentMETATags($template) {$this->loadExtension('DeprecatedAPI');return mergeDocumentMETATags($template);}
 
     /***************************************************************************************/
     /* End of API functions								       */
