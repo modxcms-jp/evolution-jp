1b51f6fcb4f51d22ddf531210fe061a379ed4759
yama <yamamoto@kyms.ne.jp>
2012-09-26 21:29:40 +0900

    インストーラ修正(データベース設定)

--- install/connection.js
+++ install/connection.js
@@ -1,4 +1,5 @@
 // get collation from the database server
+$(function(){
 $('#servertest').click(function()
 {
 	var url = 'connection.collation.php';
@@ -23,42 +24,49 @@ $('#servertest').click(function()
 		{
 			q: url,
 			host: $('#databasehost').val(),
-			uid: $('#databaseloginname').val(),
-			pwd: $('#databaseloginpassword').val(),
+			uid:  $('#databaseloginname').val(),
+			pwd:  $('#databaseloginpassword').val(),
 			language: language
 		};
 		
-		$.post(url, pars,function(data)
+		$.post(url, pars, function(data)
 		{
 			$('#serverstatus').html(data).fadeIn();
-			$('#setCollation').fadeIn();
+			if(0<data.indexOf('server_pass'))
+			{
+				$('#setCollation').fadeIn();
+			}
 		});
 	});
 });
 
 
 
-// database test
-$('#databasetest').click(function()
-{
-	var url = 'connection.databasetest.php';
-	
-	var pars = 
+	// database test
+	$('#databasetest').click(function()
 	{
-		'q': url,
-		'host': $('#databasehost').val(),
-		'uid': $('#databaseloginname').val(),
-		'pwd': $('#databaseloginpassword').val(),
-		'database_name': $('#database_name').val(),
-		'tableprefix': $('#tableprefix').val(),
-		'database_collation': $('#database_collation').val(),
-		'database_connection_method': $('#database_connection_method').val(),
-		'language': language,
-		'installMode': installMode
-	};
-	$.post(url, pars, function(data)
-	{
-		$('#databasestatus').html(data).fadeIn();
-		$('#AUH').fadeIn();
+		var url = 'connection.databasetest.php';
+		
+		var pars = 
+		{
+			'q': url,
+			'host': $('#databasehost').val(),
+			'uid': $('#databaseloginname').val(),
+			'pwd': $('#databaseloginpassword').val(),
+			'database_name': $('#database_name').val(),
+			'tableprefix': $('#tableprefix').val(),
+			'database_collation': $('#database_collation').val(),
+			'database_connection_method': $('#database_connection_method').val(),
+			'language': language,
+			'installMode': installMode
+		};
+		$.post(url, pars, function(data)
+		{
+			$('#databasestatus').html(data).fadeIn();
+			if(0<data.indexOf('database_pass'))
+			{
+				$('#AUH').fadeIn();
+			}
+		});
 	});
 });
