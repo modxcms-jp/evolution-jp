27b035bdaac14374cd51b724e1d2771cf600a311
yama <yamamoto@kyms.ne.jp>
2012-10-01 21:43:50 +0900

    Code fitting

--- manager/includes/extenders/manager.api.class.inc.php
+++ manager/includes/extenders/manager.api.class.inc.php
@@ -17,52 +17,65 @@ class ManagerAPI {
 		$this->action = $action; // set action directive
 	}
 	
-	function initPageViewState($id=0){
+	function initPageViewState($id=0)
+	{
 		global $_PAGE;
-		$vsid = isset($_SESSION["mgrPageViewSID"]) ? $_SESSION["mgrPageViewSID"] : '';
-		if($vsid!=$this->action) {
-			$_SESSION["mgrPageViewSDATA"] = array(); // new view state
-			$_SESSION["mgrPageViewSID"] = $id>0 ? $id:$this->action; // set id
+		$vsid = isset($_SESSION['mgrPageViewSID']) ? $_SESSION['mgrPageViewSID'] : '';
+		if($vsid!=$this->action)
+		{
+			$_SESSION['mgrPageViewSDATA'] = array(); // new view state
+			$_SESSION['mgrPageViewSID']   = ($id > 0) ? $id : $this->action; // set id
 		}
-		$_PAGE['vs'] = &$_SESSION["mgrPageViewSDATA"]; // restore viewstate
+		$_PAGE['vs'] = &$_SESSION['mgrPageViewSDATA']; // restore viewstate
 	}
 
 	// save page view state - not really necessary,
-	function savePageViewState($id=0){
-		$_SESSION["mgrPageViewSDATA"] = $_PAGE['vs'];
-		$_SESSION["mgrPageViewSID"] = $id>0 ? $id:$this->action;
+	function savePageViewState($id=0)
+	{
+		$_SESSION['mgrPageViewSDATA'] = $_PAGE['vs'];
+		$_SESSION['mgrPageViewSID']   = ($id > 0) ? $id : $this->action;
 	}
 	
 	// check for saved form
 	function hasFormValues() {
-		if(isset($_SESSION["mgrFormValueId"])) {
-			if($this->action==$_SESSION["mgrFormValueId"]) {
+		if(isset($_SESSION['mgrFormValueId']))
+		{
+			if($this->action==$_SESSION['mgrFormValueId'])
+			{
 				return true;
 			}
-			else {
+			else
+			{
 				$this->clearSavedFormValues();
 			}
 		}
 	}	
 	// saved form post from $_POST
-	function saveFormValues($id=0){
-		$_SESSION["mgrFormValues"] = $_POST;
-		$_SESSION["mgrFormValueId"] = $id>0 ? $id:$this->action;
+	function saveFormValues($id=0)
+	{
+		$_SESSION['mgrFormValues']  = $_POST;
+		$_SESSION['mgrFormValueId'] = ($id > 0) ? $id : $this->action;
 	}		
 	// load saved form values into $_POST
-	function loadFormValues(){
-		if($this->hasFormValues()) {
-			$p = $_SESSION["mgrFormValues"];
-			foreach($p as $k=>$v) $_POST[$k]=$v;
+	function loadFormValues()
+	{
+		if($this->hasFormValues())
+		{
+			$p = $_SESSION['mgrFormValues'];
+			foreach($p as $k=>$v)
+			{
+				$_POST[$k]=$v;
+			}
 			$this->clearSavedFormValues();
 			return true;
 		}
 		else return false;
 	}
 	// clear form post
-	function clearSavedFormValues(){
-		unset($_SESSION["mgrFormValues"]);
-		unset($_SESSION["mgrFormValueId"]);	
+	function clearSavedFormValues()
+	{
+		unset($_SESSION['mgrFormValues']);
+		unset($_SESSION['mgrFormValueId']);	
 	}
 	
 	function get_alias_from_title($id,$pagetitle)
