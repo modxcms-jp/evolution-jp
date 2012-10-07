3a229e46c1fb0c2c42adeb4cb4794b6b916b8659
yama <yamamoto@kyms.ne.jp>
2012-10-07 15:58:11 +0900

    Improved - Log tracking in to modx DB at FileBrowser

--- manager/media/browser/mcpuk/connectors/php/connector.php
+++ manager/media/browser/mcpuk/connectors/php/connector.php
@@ -25,61 +25,14 @@ define('IN_MANAGER_MODE', 'true');
 define('MODX_API_MODE', true);
 $self = 'manager/media/browser/mcpuk/connectors/php/connector.php';
 $base_path = str_replace($self,'',str_replace('\\','/',__FILE__));
-require_once("{$base_path}index.php");
+include_once("{$base_path}index.php");
+$mcpuk_path = "{$base_path}manager/media/browser/mcpuk/";
+
+file_put_contents("{$base_path}test.txt","{$base_path}index.php");
+$modx->db->connect();
 
 global $fckphp_config;
-require_once "config.php";
-
-error_reporting(E_ALL);
-function errorHandler ($errno, $errstr, $errfile, $errline, $errcontext) {
-	$reported=false;
-	if (strpos($errstr,"var: Deprecated.")===false)
-	{
-		global $fckphp_config;
-		if ($fckphp_config['Debug']===true && $fckphp_config['Debug_Errors']===true) {
-			$oldData=implode("",file($fckphp_config['DebugOutput']));
-			if ($fh=fopen($fckphp_config['DebugOutput'],"w")) {
-				fwrite($fh,"\n".date("d/m/Y H:i:s")."\n");
-				fwrite($fh,"PHP ERROR::: 
-						Error Number: $errno
-						Error Message: $errstr
-						Error File: $errfile
-						Error Line: $errline\n");
-				if ($fckphp_config['Debug_Trace']) fwrite($fh,"		Error Context: ".print_r($errcontext,true)."\n");
-				if ($fckphp_config['Debug_GET']) fwrite($fh,"\n\$_GET::\n".print_r($_GET,true)."\n");
-				if ($fckphp_config['Debug_POST']) fwrite($fh,"\n\$_POST::\n".print_r($_POST,true)."\n");
-				if ($fckphp_config['Debug_SERVER']) fwrite($fh,"\n\$_SERVER::\n".print_r($_SERVER,true)."\n");
-				if ($fckphp_config['Debug_SESSIONS']) fwrite($fh,"\n\$_SESSIONS::\n".print_r($_SESSION,true)."\n");
-				fwrite($fh,"\n-------------------------------------------------------\n\n\n");
-				fwrite($fh,$oldData); $oldData="";
-				fclose($fh);
-				$reported=true;
-			} 
-		}
-		
-		if (!$reported) {
-			//display error instead.
-			echo("PHP ERROR::: <br />
-					Error Number: $errno <br />
-					Error Message: $errstr <br />
-					Error File: $errfile <br />
-					Error Line: $errline <br />");
-				
-			if ($fckphp_config['Debug_Trace']) echo "Error Context: ".print_r($errcontext,true)."\n";	
-			if ($fckphp_config['Debug_GET']) echo "\$_GET::\n".print_r($_GET,true)."<br />\n";
-			if ($fckphp_config['Debug_POST']) echo "\$_POST::\n".print_r($_POST,true)."<br />\n";
-			if ($fckphp_config['Debug_SERVER']) echo "\$_SERVER::\n".print_r($_SERVER,true)."<br />\n";
-			if ($fckphp_config['Debug_SESSIONS']) echo "\$_SESSIONS::\n".print_r($_SESSION,true)."<br />\n";
-			echo "<br />\n<br />\n";
-		}
-	}
-}
-set_error_handler('errorHandler');
-
-if (!isset($_SERVER['DOCUMENT_ROOT'])) $_SERVER["DOCUMENT_ROOT"] = $fckphp_config['basedir'];
-
-if ($fckphp_config['Debug']===true && $fckphp_config['Debug_Output']) ob_start();
-outputHeaders();
+include_once("{$mcpuk_path}connectors/php/config.php");
 
 //These are the commands we may expect
 $valid_commands=$fckphp_config['Commands'];
@@ -93,73 +46,42 @@ $cwd     = (isset($_GET['CurrentFolder']) && $_GET['CurrentFolder']!='') ? $_GET
 $cwd     = str_replace('..', '', $cwd);
 $extra   = (isset($_GET['ExtraParams']) && $_GET['ExtraParams']!='')     ? $_GET['ExtraParams']   : '';
 
+if ($fckphp_config['Debug']===true)
+{
+	$msg  = '$command=' . "{$command}\n";
+	$msg .= '$type='    . $type . "\n";
+	$msg .= '$cwd='     . $cwd . "\n";
+	$msg .= '$extra='   . $extra . "\n";
+	$msg .= '$_GET='      . print_r($_GET,true) . "\n";
+	$msg .= '$_POST='     . print_r($_POST,true) . "\n";
+	$msg .= '$_SERVER='   . print_r($_SERVER,true) . "\n";
+	$msg .= '$_SESSIONS=' . print_r($_SESSION,true) . "\n";
+	$msg .= '$_COOKIE='   . print_r($_COOKIE,true) . "\n";
+	$msg .= '$_FILES='    . print_r($_FILES,true) . "\n";
+	
+	$msg = htmlspecialchars($msg,ENT_QUOTES,$modx->config['modx_charset']);
+	$msg = str_replace(' ', '&nbsp;',$msg);
+	$msg = nl2br($msg);
+	
+	$modx->logEvent(0,1,$msg,'mcpuk connector');
+}
+
 if (in_array($command,$valid_commands))
 {
 	//bit of validation
 	if (!in_array($type,$valid_resource_types))
 	{
-		echo 'Invalid resource type.';
-		if ($fckphp_config['Debug']===true  && $fckphp_config['Debug_Output']) recordOutput();
+		$modx->logEvent(0,3,'Invalid resource type.');
 		exit(0);
 	}
 	
-	require_once "Commands/{$command}.php";
+	$rs = include_once("{$mcpuk_path}connectors/php/Commands/{$command}.php");
 
 	$action=new $command($fckphp_config,$type,$cwd);
-
 	$action->run();
-	if ($fckphp_config['Debug']===true && $fckphp_config['Debug_Output']) recordOutput();
-	
-} else {
-	//No reason for me to be here.
-	echo "Invalid command.";
-	echo str_replace("\n","<br />",print_r($_GET,true));
-	if ($fckphp_config['Debug']===true  && $fckphp_config['Debug_Output']) recordOutput();
+}
+else
+{
+	$modx->logEvent(0,3,'Invalid command.(No reason for me to be here)');
 	exit(0);
-}
-
-
-function recordOutput() {
-	global $fckphp_config;
-
-	if ($fckphp_config['Debug']===true  && $fckphp_config['Debug_Output'])
-	{
-		$contents=ob_get_contents();
-		if (strlen($contents)>0)
-		{
-			$oldData=implode('',file($fckphp_config['DebugOutput']));
-			if ($fh=fopen($fckphp_config['DebugOutput'],"w")) {
-				fwrite($fh,"\n".date("d/m/Y H:i:s")."\n");
-				if ($fckphp_config['Debug_GET'])      fwrite($fh,"\n\$_GET::\n"      .print_r($_GET,true)    ."\n");
-				if ($fckphp_config['Debug_POST'])     fwrite($fh,"\n\$_POST::\n"     .print_r($_POST,true)   ."\n");
-				if ($fckphp_config['Debug_SERVER'])   fwrite($fh,"\n\$_SERVER::\n"   .print_r($_SERVER,true) ."\n");
-				if ($fckphp_config['Debug_SESSIONS']) fwrite($fh,"\n\$_SESSIONS::\n" .print_r($_SESSION,true)."\n");
-				fwrite($fh,$contents);
-				fwrite($fh,"\n-------------------------------------------------------\n\n\n");
-				fwrite($fh,$oldData); $oldData="";
-				fclose($fh);
-			}
-		}
-		ob_flush();
-	}
-}
-
-function outputHeaders() {
-
-	//Anti browser caching headers
-	//Borrowed from fatboy's implementation  (fatFCK@code247.com)
-	
-	// ensure file is never cached
-	// Date in the past
-	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-	
-	// always modified
-	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-	
-	// HTTP/1.1
-	header("Cache-Control: no-store, no-cache, must-revalidate");
-	header("Cache-Control: post-check=0, pre-check=0", false);
-	
-	// HTTP/1.0
-	header("Pragma: no-cache");
 }
