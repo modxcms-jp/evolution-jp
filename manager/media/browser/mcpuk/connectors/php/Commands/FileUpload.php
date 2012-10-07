523b8cce34df9eaa71d8bd853a74521c655dc26b
yama <yamamoto@kyms.ne.jp>
2012-10-07 16:06:12 +0900

    Improved - File upload only manager loggedin

--- manager/media/browser/mcpuk/connectors/php/Commands/FileUpload.php
+++ manager/media/browser/mcpuk/connectors/php/Commands/FileUpload.php
@@ -35,13 +35,18 @@ class FileUpload {
 	{
 		global $modx;
 		
-		define('MODX_API_MODE', true);
-		define('IN_MANAGER_MODE', 'true');
-		$self = 'manager/media/browser/mcpuk/connectors/php/Commands/FileUpload.php';
-		$base_path = str_replace($self,'',str_replace($self,'',str_replace('\\','/',__FILE__));
-		require_once("{$base_path}index.php");
+		if(!defined('IN_MANAGER_MODE'))
+		{
+			define('IN_MANAGER_MODE', 'true');
+			if(!defined('MODX_API_MODE')) define('MODX_API_MODE', true);
+			$self = 'manager/media/browser/mcpuk/connectors/php/Commands/FileUpload.php';
+			$base_path = str_replace($self,'',str_replace('\\','/',__FILE__));
+			include_once("{$base_path}index.php");
+			$modx->db->connect();
+			$modx->getSettings();
+		}
+		
 		if(!isset($_SESSION['mgrValidated'])) exit;
-		$modx->getSettings();
 		
 		$this->fckphp_config = $fckphp_config;
 		$this->type          = $type;
