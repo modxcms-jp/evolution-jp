79e8000b6b906eabdca42473a4a56780f0ed4310
yama <yamamoto@kyms.ne.jp>
2012-10-18 10:49:53 +0900

    Add Global variables

--- manager/includes/header.inc.php
+++ manager/includes/header.inc.php
@@ -1,6 +1,9 @@
 <?php
 if(!defined('IN_MANAGER_MODE') || IN_MANAGER_MODE != 'true') exit();
 global $_lang, $_style, $modx_textdir, $modx_lang_attribute;
+global $action, $SystemAlertMsgQueque, $manager_theme, $modx_charset;
+global $manager_language;
+
 if($modx->config['remember_last_tab']!=='2')
 {
 	$tab = (isset($_GET['tab'])) ? intval($_GET['tab']) : '1';
