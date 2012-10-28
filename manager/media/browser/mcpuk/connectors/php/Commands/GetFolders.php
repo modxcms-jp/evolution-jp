eefc3eb3ea04abff08d5937cb3d8ecbdb0644b17
yama <yamamoto@kyms.ne.jp>
2012-10-28 15:55:56 +0900

    ファイルブラウザ左側のフォルダー一覧ペインが機能していないため修正

--- manager/media/browser/mcpuk/connectors/php/Commands/GetFolders.php
+++ manager/media/browser/mcpuk/connectors/php/Commands/GetFolders.php
@@ -26,8 +26,8 @@ class GetFolders {
 		$this->fckphp_config=$fckphp_config;
 		$this->type=$type;
 		$this->raw_cwd=$cwd;
-		$this->actual_cwd=str_replace("//","/",($fckphp_config['UserFilesPath']."/$type/".$this->raw_cwd));
-		$this->real_cwd=str_replace("//","/",($this->fckphp_config['basedir']."/".$this->actual_cwd));
+		$this->actual_cwd=str_replace('//','/',($fckphp_config['UserFilesPath']."/{$type}/{$cwd}"));
+		$this->real_cwd=str_replace('//','/',($this->fckphp_config['basedir'].'/'.$this->actual_cwd));
 	}
 	
 	function run() {
@@ -37,49 +37,45 @@ class GetFolders {
 <Connector command="GetFolders" resourceType="<?php echo $this->type; ?>">
 	<CurrentFolder path="<?php echo $this->raw_cwd; ?>" url="<?php echo $this->actual_cwd; ?>" />
 	<Folders>
-		<?php
-			if ($dh=opendir($this->real_cwd)) {
-
-			    /**
-			     * Initiate the array to store the filenames
-			     */
-			    $files_in_folder = array();
-				
-				$files = scandir($dh);
-				if($files)
+<?php
+		/**
+		 * Initiate the array to store the filenames
+		 */
+		$files_in_folder = array();
+		
+		$files = scandir($this->real_cwd);
+		if($files)
+		{
+		    foreach ($files as $filename)
+		    {
+				if (($filename!='.')&&($filename!='..'))
 				{
-				    foreach ($files as $filename)
-				    {
-						if (($filename!='.')&&($filename!='..'))
+					if (is_dir($this->real_cwd . $filename))
+					{
+						//check if$fckphp_configured not to show this folder
+						$hide=false;
+						for($i=0;$i<sizeof($this->fckphp_config['ResourceAreas'][$this->type]['HideFolders']);$i++)
 						{
-							if (is_dir($this->real_cwd."/$filename"))
-							{
-								//check if$fckphp_configured not to show this folder
-								$hide=false;
-								for($i=0;$i<sizeof($this->fckphp_config['ResourceAreas'][$this->type]['HideFolders']);$i++)
-								{
-									$pattern = $this->fckphp_config['ResourceAreas'][$this->type]['HideFolders'][$i];
-									$hide=(preg_match("/{$pattern}/",$filename) ? true : $hide);
-								}
-								/**
-								* Dont echo the entry, push it in the array
-								*/
-								if (!$hide) array_push($files_in_folder,$filename);
-							}
+							$pattern = $this->fckphp_config['ResourceAreas'][$this->type]['HideFolders'][$i];
+							$hide=(preg_match("/{$pattern}/",$filename) ? true : $hide);
 						}
+						/**
+						* Dont echo the entry, push it in the array
+						*/
+						if (!$hide) array_push($files_in_folder,$filename);
 					}
 				}
 			}
-
-			/**
-			 * Sort the array by the way you like and show it.
-			 */
-			natcasesort($files_in_folder);
-            foreach($files_in_folder as $k=>$v)
-            {
-	           echo '<Folder name="'.$v.'" />'."\n";
-            }
-		?>
+		}
+		/**
+		 * Sort the array by the way you like and show it.
+		 */
+		natcasesort($files_in_folder);
+        foreach($files_in_folder as $k=>$v)
+        {
+           echo '<Folder name="'.$v.'" />'."\n";
+        }
+?>
 	</Folders>
 </Connector>
 		<?php
