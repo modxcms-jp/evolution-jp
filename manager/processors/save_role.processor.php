a3634f91a168fd367f68134217d20e23e565f527
yama <yamamoto@kyms.ne.jp>
2012-11-06 19:52:14 +0900

    ロール設定更新時にロール名の重複をチェックしないため修正

--- manager/processors/save_role.processor.php
+++ manager/processors/save_role.processor.php
@@ -24,15 +24,16 @@ $save_parser = (isset ($save_parser)) ? $save_parser : '0';
 // setup fields
 $fields = compact(explode(',','name,description,frames,home,view_document,new_document,save_document,publish_document,delete_document,empty_trash,action_ok,logout,help,messages,new_user,edit_user,logs,edit_parser,save_parser,edit_template,settings,credits,new_template,save_template,delete_template,edit_snippet,new_snippet,save_snippet,delete_snippet,edit_chunk,new_chunk,save_chunk,delete_chunk,empty_cache,edit_document,change_password,error_dialog,about,file_manager,save_user,delete_user,save_password,edit_role,save_role,delete_role,new_role,access_permissions,bk_manager,new_plugin,edit_plugin,save_plugin,delete_plugin,new_module,edit_module,save_module,delete_module,exec_module,view_eventlog,delete_eventlog,manage_metatags,edit_doc_metatags,new_web_user,edit_web_user,save_web_user,delete_web_user,web_access_permissions,view_unpublished,import_static,export_static,remove_locks,view_schedule'));
 
+$rs = $modx->db->select('id',$tbl_user_roles,"name='{$name}'");
+if(0<$modx->db->getRecordCount($rs))
+{
+	echo "An error occured while attempting to save the new role.";
+	exit;
+}
+
 switch ($_POST['mode'])
 {
 	case '38' :
-		$rs = $modx->db->select('id',$tbl_user_roles,"name='{$name}'");
-		if(0<$modx->db->getRecordCount($rs))
-		{
-			echo "An error occured while attempting to save the new role.";
-			exit;
-		}
 		$id = $modx->db->insert($fields, $tbl_user_roles);
 		break;
 	case '35' :
