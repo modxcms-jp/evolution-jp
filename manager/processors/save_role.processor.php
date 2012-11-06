ae9d72c6cd41700909e46286d1a979f63579b4ce
yama <yamamoto@kyms.ne.jp>
2012-11-06 15:44:29 +0900

    コード整理

--- manager/processors/save_role.processor.php
+++ manager/processors/save_role.processor.php
@@ -9,115 +9,50 @@ foreach ($_POST as $n => $v)
 {
 	$input[$n] = $modx->db->escape($v); // escape post values
 }
+$tbl_user_roles = $modx->getFullTableName('user_roles');
+
+$input = $_POST;
 extract($input);
+
+$name        = $modx->db->escape(trim($name));
+$description = $modx->db->escape(trim($description));
 
 if (!isset($name) || empty($name)) {
     echo 'Please enter a name for this role!';
     exit;
 }
 
-// setup fields
-$fields = array (
-    'name' => $name,
-    'description' => $description,
-    'frames' => $frames,
-    'home' => $home,
-    'view_document' => $view_document,
-    'new_document' => $new_document,
-    'save_document' => $save_document,
-    'publish_document' => $publish_document,
-    'delete_document' => $delete_document,
-    'empty_trash' => $empty_trash,
-    'action_ok' => $action_ok,
-    'logout' => $logout,
-    'help' => $help,
-    'messages' => $messages,
-    'new_user' => $new_user,
-    'edit_user' => $edit_user,
-    'logs' => $logs,
-    'edit_parser' => (isset ($edit_parser)) ? $edit_parser : '0',
-    'save_parser' => (isset ($save_parser)) ? $save_parser : '0',
-    'edit_template' => $edit_template,
-    'settings' => $settings,
-    'credits' => $credits,
-    'new_template' => $new_template,
-    'save_template' => $save_template,
-    'delete_template' => $delete_template,
-    'edit_snippet' => $edit_snippet,
-    'new_snippet' => $new_snippet,
-    'save_snippet' => $save_snippet,
-    'delete_snippet' => $delete_snippet,
-    'edit_chunk' => $edit_chunk,
-    'new_chunk' => $new_chunk,
-    'save_chunk' => $save_chunk,
-    'delete_chunk' => $delete_chunk,
-    'empty_cache' => $empty_cache,
-    'edit_document' => $edit_document,
-    'change_password' => $change_password,
-    'error_dialog' => $error_dialog,
-    'about' => $about,
-    'file_manager' => $file_manager,
-    'save_user' => $save_user,
-    'delete_user' => $delete_user,
-    'save_password' => $save_password,
-    'edit_role' => $edit_role,
-    'save_role' => $save_role,
-    'delete_role' => $delete_role,
-    'new_role' => $new_role,
-    'access_permissions' => $access_permissions,
-    'bk_manager' => $bk_manager,
-    'new_plugin' => $new_plugin,
-    'edit_plugin' => $edit_plugin,
-    'save_plugin' => $save_plugin,
-    'delete_plugin' => $delete_plugin,
-    'new_module' => $new_module,
-    'edit_module' => $edit_module,
-    'save_module' => $save_module,
-    'delete_module' => $delete_module,
-    'exec_module' => $exec_module,
-    'view_eventlog' => $view_eventlog,
-    'delete_eventlog' => $delete_eventlog,
-    'manage_metatags' => $manage_metatags,
-    'edit_doc_metatags' => $edit_doc_metatags,
-    'new_web_user' => $new_web_user,
-    'edit_web_user' => $edit_web_user,
-    'save_web_user' => $save_web_user,
-    'delete_web_user' => $delete_web_user,
-    'web_access_permissions' => $web_access_permissions,
-    'view_unpublished' => $view_unpublished,
-	'import_static' => $import_static,
-	'export_static' => $export_static,
-    'remove_locks' => $remove_locks,
-    'view_schedule' => $view_schedule
-);
+$edit_parser = (isset ($edit_parser)) ? $edit_parser : '0';
+$save_parser = (isset ($save_parser)) ? $save_parser : '0';
 
-$tbl_user_roles = $modx->getFullTableName("user_roles");
-switch ($_POST['mode']) {
-    case '38' :
+// setup fields
+$fields = compact(explode(',','name,description,frames,home,view_document,new_document,save_document,publish_document,delete_document,empty_trash,action_ok,logout,help,messages,new_user,edit_user,logs,edit_parser,save_parser,edit_template,settings,credits,new_template,save_template,delete_template,edit_snippet,new_snippet,save_snippet,delete_snippet,edit_chunk,new_chunk,save_chunk,delete_chunk,empty_cache,edit_document,change_password,error_dialog,about,file_manager,save_user,delete_user,save_password,edit_role,save_role,delete_role,new_role,access_permissions,bk_manager,new_plugin,edit_plugin,save_plugin,delete_plugin,new_module,edit_module,save_module,delete_module,exec_module,view_eventlog,delete_eventlog,manage_metatags,edit_doc_metatags,new_web_user,edit_web_user,save_web_user,delete_web_user,web_access_permissions,view_unpublished,import_static,export_static,remove_locks,view_schedule'));
+
+switch ($_POST['mode'])
+{
+	case '38' :
 		$rs = $modx->db->select('id',$tbl_user_roles,"name='{$name}'");
 		if(0<$modx->db->getRecordCount($rs))
 		{
 			echo "An error occured while attempting to save the new role.";
 			exit;
 		}
-		else
+		$id = $modx->db->insert($fields, $tbl_user_roles);
+		break;
+	case '35' :
+		$rs = $modx->db->update($fields, $tbl_user_roles, "id='{$id}'");
+		if (!$rs)
 		{
-			$id = $modx->db->insert($fields, $tbl_user_roles);
+			echo "An error occured while attempting to update the role. <br />" . $modx->db->getLastError();
+			exit;
 		}
 		break;
-    case '35' :
-        $rs = $modx->db->update($fields, $tbl_user_roles, "id='{$id}'");
-        if (!$rs) {
-            echo "An error occured while attempting to update the role. <br />" . $modx->db->getLastError();
-            exit;
-        }
-        break;
-    default :
-    	echo "Erm... You supposed to be here now?";
-        exit;
+	default :
+		echo "Erm... You supposed to be here now?";
+		exit;
 }
 
-if($rs)
+if(isset($rs))
 {
 	$cache_path = "{$modx->config['base_path']}assets/cache/rolePublishing.idx.php";
 	if(file_exists($cache_path))
@@ -126,5 +61,5 @@ if($rs)
 	}
 	$role[$id] = time();
 	file_put_contents($cache_path, serialize($role));
-	header("Location: index.php?a=86");
+	header('Location: index.php?a=86');
 }
