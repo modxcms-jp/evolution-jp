36b414e42530f24905d8eb6fcc30cd90445d041d
yama <yamamoto@kyms.ne.jp>
2012-11-13 08:11:54 +0900

    fix

--- manager/processors/save_role.processor.php
+++ manager/processors/save_role.processor.php
@@ -24,11 +24,26 @@ $save_parser = (isset ($save_parser)) ? $save_parser : '0';
 // setup fields
 $fields = compact(explode(',','name,description,frames,home,view_document,new_document,save_document,publish_document,delete_document,empty_trash,action_ok,logout,help,messages,new_user,edit_user,logs,edit_parser,save_parser,edit_template,settings,credits,new_template,save_template,delete_template,edit_snippet,new_snippet,save_snippet,delete_snippet,edit_chunk,new_chunk,save_chunk,delete_chunk,empty_cache,edit_document,change_password,error_dialog,about,file_manager,save_user,delete_user,save_password,edit_role,save_role,delete_role,new_role,access_permissions,bk_manager,new_plugin,edit_plugin,save_plugin,delete_plugin,new_module,edit_module,save_module,delete_module,exec_module,view_eventlog,delete_eventlog,manage_metatags,edit_doc_metatags,new_web_user,edit_web_user,save_web_user,delete_web_user,web_access_permissions,view_unpublished,import_static,export_static,remove_locks,view_schedule'));
 
-$rs = $modx->db->select('id',$tbl_user_roles,"name='{$name}'");
-if(0<$modx->db->getRecordCount($rs))
+if($_POST['mode']=='38')
 {
-	echo "An error occured while attempting to save the new role.";
-	exit;
+	$search_name = $name;
+}
+elseif($_POST['mode']=='35')
+{
+	$rs = $modx->db->select('name',$tbl_user_roles,"id='{$id}'");
+	$row = $modx->db->getRow($rs);
+	if($row['name']!==$name) $search_name = $row['name'];
+}
+else $search_name = false;
+
+if($search_name!==false)
+{
+	$rs = $modx->db->select('id',$tbl_user_roles,"name='{$search_name}'");
+	if(0<$modx->db->getRecordCount($rs))
+	{
+		echo "An error occured while attempting to save the new role.";
+		exit;
+	}
 }
 
 switch ($_POST['mode'])
