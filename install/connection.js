d2b18290bf33862a773a95e4d266aef281daa488
yama <yamamoto@kyms.ne.jp>
2012-11-20 21:19:01 +0900

    インストーラ - collation・接続時のエンコード設定などの処理を省略(utf-8に固定するため)

--- install/connection.js
+++ install/connection.js
@@ -1,24 +1,7 @@
 // get collation from the database server
 $(function(){
-$('#servertest').click(function()
-{
-	var url = 'connection.collation.php';
-	var pars =
+	$('#servertest').click(function()
 	{
-		'q': url,
-		'host': $('#database_server').val(),
-		'uid': $('#database_user').val(),
-		'pwd': $('#database_password').val(),
-		'database_collation': $('#database_collation').val(),
-		'database_connection_method': $('#database_connection_method').val(),
-		'language': language
-	};
-	
-	$.post(url, pars,function(data)
-	{
-		$('#collation').html(data);
-		
-		// get the server test status as soon as collation received
 		var url = 'connection.servertest.php';
 		var pars =
 		{
@@ -28,7 +11,6 @@ $('#servertest').click(function()
 			pwd:  $('#database_password').val(),
 			language: language
 		};
-		
 		$.post(url, pars, function(data)
 		{
 			$('#serverstatus').html(data).fadeIn();
@@ -38,15 +20,11 @@ $('#servertest').click(function()
 			}
 		});
 	});
-});
-
-
-
+	
 	// database test
 	$('#databasetest').click(function()
 	{
 		var url = 'connection.databasetest.php';
-		
 		var pars = 
 		{
 			'q': url,
@@ -55,8 +33,8 @@ $('#servertest').click(function()
 			'pwd': $('#database_password').val(),
 			'dbase': $('#dbase').val(),
 			'table_prefix': $('#table_prefix').val(),
-			'database_collation': $('#database_collation').val(),
-			'database_connection_method': $('#database_connection_method').val(),
+			'database_collation': 'utf8_general_ci',
+			'database_connection_method': 'SET CHARACTER SET',
 			'language': language,
 			'installMode': installMode
 		};
