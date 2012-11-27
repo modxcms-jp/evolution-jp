00b8af6cc2486a8a7430324571bb379b15c41636
yama <yamamoto@kyms.ne.jp>
2012-11-27 12:57:26 +0900

    バージョン更新(1.0.7J)

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.6J-r9';       // Current version number
-$modx_release_date = '2012/11/30'; // Date of release
+$modx_version = '1.0.7J';       // Current version number
+$modx_release_date = '2012/11/26'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
