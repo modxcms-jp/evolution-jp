17eed4762e7571e43893c7b1ed40ac019bf08ee6
yama <yamamoto@kyms.ne.jp>
2012-12-25 15:56:57 +0900

    Simplify generate password function

--- manager/processors/save_web_user.processor.php
+++ manager/processors/save_web_user.processor.php
@@ -453,13 +453,5 @@ function webAlert($msg) {
 
 // Generate password
 function generate_password($length = 10) {
-	$allowable_characters = 'abcdefghjkmnpqrstuvxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-	$ps_len = strlen($allowable_characters);
-	mt_srand((double) microtime() * 1000000);
-	$pass = '';
-	for ($i = 0; $i < $length; $i++)
-	{
-		$pass .= $allowable_characters[mt_rand(0, $ps_len -1)];
-	}
-	return $pass;
+	return substr(str_shuffle('abcdefghjkmnpqrstuvxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'), 0, $length);
 }
