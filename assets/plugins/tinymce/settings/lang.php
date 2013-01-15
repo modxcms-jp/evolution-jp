12eb96664aef80487209218c248664f0364b05c4
yama <yamamoto@kyms.ne.jp>
2013-01-15 19:29:55 +0900

    TinyMCE - Add Russian check

--- assets/plugins/tinymce/settings/lang.php
+++ assets/plugins/tinymce/settings/lang.php
@@ -3,6 +3,7 @@ function get_mce_lang($lang)
 {
 	switch($lang)
 	{
+		case 'russian-utf8' : $lc='ru'; break;
 		case 'japanese-utf8':
 		case 'japanese-euc' : $lc = 'ja'; break;
 		default             : $lc = 'en';
