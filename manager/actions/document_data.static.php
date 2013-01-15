f0bdde9707e974e14dc9251f7ae14137b0797a31
yama <yamamoto@kyms.ne.jp>
2013-01-15 10:50:41 +0900

    Small fix

--- manager/actions/document_data.static.php
+++ manager/actions/document_data.static.php
@@ -353,13 +353,17 @@ h3 {font-size:1em;padding-bottom:0;margin-bottom:0;}
 				<td><?php echo $content['longtitle']!='' ? $content['longtitle'] : "(<i>".$_lang['not_set']."</i>)"?></td>
 			</tr>
 			<tr><td><?php echo $_lang['resource_description']?>: </td>
-				<td><?php echo $content['description']!='' ? $content['description'] : "(<i>".$_lang['not_set']."</i>)"?></td></tr>
+				<td><?php echo $content['description']!='' ? $content['description'] : "(<i>".$_lang['not_set']."</i>)"?></td>
+			</tr>
 			<tr><td><?php echo $_lang['resource_summary']?>: </td>
-				<td><?php echo $content['introtext']!='' ? $content['introtext'] : "(<i>".$_lang['not_set']."</i>)"?></td></tr>
+				<td><?php echo $content['introtext']!='' ? $content['introtext'] : "(<i>".$_lang['not_set']."</i>)"?></td>
+			</tr>
 			<tr><td><?php echo $_lang['type']?>: </td>
-				<td><?php echo $content['type']=='reference' ? $_lang['weblink'] : $_lang['resource']?></td></tr>
+				<td><?php echo $content['type']=='reference' ? $_lang['weblink'] : $_lang['resource']?></td>
+			</tr>
 			<tr><td><?php echo $_lang['resource_alias']?>: </td>
-				<td><?php echo $content['alias']!='' ? urldecode($content['alias']) : "(<i>".$_lang['not_set']."</i>)"?></td></tr>
+				<td><?php echo $content['alias']!='' ? urldecode($content['alias']) : "(<i>".$_lang['not_set']."</i>)"?></td>
+			</tr>
 			<?php if ($modx->config['show_meta']) {?>
 			<tr><td><?php echo $_lang['keywords']?>: </td>
 				<td><?php // Keywords
@@ -375,35 +379,49 @@ h3 {font-size:1em;padding-bottom:0;margin-bottom:0;}
 				?></td></tr>
 			<?php } ?>
 			<tr><td width="200"><?php echo $_lang['page_data_created']?>: </td>
-				<td><?php echo $modx->toDateFormat($content['createdon']+$server_offset_time)?> (<b><?php echo $createdbyname?></b>)</td></tr>
+				<td><?php echo $modx->toDateFormat($content['createdon']+$server_offset_time)?> (<b><?php echo $createdbyname?></b>)</td>
+			</tr>
 <?php				if ($editedbyname != '') { ?>
 			<tr><td><?php echo $_lang['page_data_edited']?>: </td>
-				<td><?php echo $modx->toDateFormat($content['editedon']+$server_offset_time)?> (<b><?php echo $editedbyname?></b>)</td></tr>
+				<td><?php echo $modx->toDateFormat($content['editedon']+$server_offset_time)?> (<b><?php echo $editedbyname?></b>)</td>
+			</tr>
 <?php				} ?>
 			<tr><td width="200"><?php echo $_lang['page_data_status']?>: </td>
-				<td><?php echo $content['published']==0 ? '<span class="unpublishedDoc">'.$_lang['page_data_unpublished'].'</span>' : '<span class="publisheddoc">'.$_lang['page_data_published'].'</span>'?></td></tr>
+				<td><?php echo $content['published']==0 ? '<span class="unpublishedDoc">'.$_lang['page_data_unpublished'].'</span>' : '<span class="publisheddoc">'.$_lang['page_data_published'].'</span>'?></td>
+			</tr>
 			<tr><td><?php echo $_lang['page_data_publishdate']?>: </td>
-				<td><?php echo $content['pub_date']==0 ? "(<i>".$_lang['not_set']."</i>)" : $modx->toDateFormat($content['pub_date'])?></td></tr>
+				<td><?php echo $content['pub_date']==0 ? "(<i>".$_lang['not_set']."</i>)" : $modx->toDateFormat($content['pub_date'])?></td>
+			</tr>
 			<tr><td><?php echo $_lang['page_data_unpublishdate']?>: </td>
-				<td><?php echo $content['unpub_date']==0 ? "(<i>".$_lang['not_set']."</i>)" : $modx->toDateFormat($content['unpub_date'])?></td></tr>
+				<td><?php echo $content['unpub_date']==0 ? "(<i>".$_lang['not_set']."</i>)" : $modx->toDateFormat($content['unpub_date'])?></td>
+			</tr>
 			<tr><td><?php echo $_lang['page_data_cacheable']?>: </td>
-				<td><?php echo $content['cacheable']==0 ? $_lang['no'] : $_lang['yes']?></td></tr>
+				<td><?php echo $content['cacheable']==0 ? $_lang['no'] : $_lang['yes']?></td>
+			</tr>
 			<tr><td><?php echo $_lang['page_data_searchable']?>: </td>
-				<td><?php echo $content['searchable']==0 ? $_lang['no'] : $_lang['yes']?></td></tr>
+				<td><?php echo $content['searchable']==0 ? $_lang['no'] : $_lang['yes']?></td>
+			</tr>
 			<tr><td><?php echo $_lang['resource_opt_menu_index']?>: </td>
-				<td><?php echo $content['menuindex']?></td></tr>
+				<td><?php echo $content['menuindex']?></td>
+			</tr>
 			<tr><td><?php echo $_lang['resource_opt_show_menu']?>: </td>
-				<td><?php echo $content['hidemenu']==1 ? $_lang['no'] : $_lang['yes']?></td></tr>
+				<td><?php echo $content['hidemenu']==1 ? $_lang['no'] : $_lang['yes']?></td>
+			</tr>
 			<tr><td><?php echo $_lang['page_data_web_access']?>: </td>
-				<td><?php echo $content['privateweb']==0 ? $_lang['public'] : '<b style="color: #821517">'.$_lang['private'].'</b> <img src="media/style/' . $modx->config['manager_theme'] .'/images/icons/secured.gif" align="absmiddle" />'?></td></tr>
+				<td><?php echo $content['privateweb']==0 ? $_lang['public'] : '<b style="color: #821517">'.$_lang['private'].'</b> <img src="media/style/' . $modx->config['manager_theme'] .'/images/icons/secured.gif" align="absmiddle" />'?></td>
+			</tr>
 			<tr><td><?php echo $_lang['page_data_mgr_access']?>: </td>
-				<td><?php echo $content['privatemgr']==0 ? $_lang['public'] : '<b style="color: #821517">'.$_lang['private'].'</b> <img src="media/style/' . $modx->config['manager_theme'] .'/images/icons/secured.gif" align="absmiddle" />'?></td></tr>
+				<td><?php echo $content['privatemgr']==0 ? $_lang['public'] : '<b style="color: #821517">'.$_lang['private'].'</b> <img src="media/style/' . $modx->config['manager_theme'] .'/images/icons/secured.gif" align="absmiddle" />'?></td>
+			</tr>
 			<tr><td width="200"><?php echo $_lang['page_data_template']?>: </td>
-				<td><?php echo $templatename ?></td></tr>
+				<td><?php echo $templatename ?></td>
+			</tr>
 			<tr><td><?php echo $_lang['page_data_editor']?>: </td>
-				<td><?php echo $content['richtext']==0 ? $_lang['no'] : $_lang['yes']?></td></tr>
+				<td><?php echo $content['richtext']==0 ? $_lang['no'] : $_lang['yes']?></td>
+			</tr>
 			<tr><td><?php echo $_lang['page_data_folder']?>: </td>
-				<td><?php echo $content['isfolder']==0 ? $_lang['no'] : $_lang['yes']?></td></tr>
+				<td><?php echo $content['isfolder']==0 ? $_lang['no'] : $_lang['yes']?></td>
+			</tr>
 		</table>
 		</div><!-- end sectionBody -->
 	</div><!-- end tab-page -->
