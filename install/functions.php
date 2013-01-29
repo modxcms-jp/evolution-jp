d0ebe427a0e7b34a005990fd4f81eacb981aae69
yama <yamamoto@kyms.ne.jp>
2013-01-29 19:38:20 +0900

    Small fix - sessionCheck at install

--- install/functions.php
+++ install/functions.php
@@ -479,9 +479,34 @@ function ph()
 
 function sessionCheck()
 {
-	if(!isset($_SESSION['test']) || $_SESSION['test']!=1)
-		$rs = false;
-	else $rs = true;
+	global $_lang;
 	
-	return $rs;
+	if(!isset($_GET['action']) || $_GET['action']==='mode') $_SESSION['test'] = 1;
+	// session loop-back tester
+	if($_GET['action']!=='mode')
+	{
+	if(!isset($_SESSION['test']) || $_SESSION['test']!=1)
+		{
+			echo '
+<html>
+<head>
+	<title>Install Problem</title>
+	<style type="text/css">
+		*{margin:0;padding:0}
+		body{margin:150px;background:#eee;}
+		.install{padding:10px;border:3px solid #ffc565;background:#ffddb4;margin:0 auto;text-align:center;}
+		p{ margin:20px 0; }
+		a{margin-top:30px;padding:5px;}
+	</style>
+</head>
+<body>
+	<div class="install">
+		<p>' . $_lang["session_problem"] . '</p>
+		<p><a href="./">' .$_lang["session_problem_try_again"] . '</a></p>
+	</div>
+</body>
+</html>';
+		exit;
+		}
+	}
 }
--- install/index.php
+++ install/index.php
@@ -38,35 +38,7 @@ includeLang($default_language);
 
 // start session
 session_start();
-if($_GET['action']==='mode') $_SESSION['test'] = 1;
-// session loop-back tester
-if($_GET['action']!=='mode')
-{
-$rs = sessionCheck();
-if (!$rs)
-{
-	echo '
-<html>
-<head>
-	<title>Install Problem</title>
-	<style type="text/css">
-		*{margin:0;padding:0}
-		body{margin:150px;background:#eee;}
-		.install{padding:10px;border:3px solid #ffc565;background:#ffddb4;margin:0 auto;text-align:center;}
-		p{ margin:20px 0; }
-		a{margin-top:30px;padding:5px;}
-	</style>
-</head>
-<body>
-	<div class="install">
-		<p>' . $_lang["session_problem"] . '</p>
-		<p><a href="./">' .$_lang["session_problem_try_again"] . '</a></p>
-	</div>
-</body>
-</html>';
-	exit;
-	}
-}
+sessionCheck();
 
 $installmode        = getOption('installmode');
 $database_server    = getOption('database_server');
