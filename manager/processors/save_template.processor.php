c6ee47acbba33e02fd32bdff06b13849585c7121
yama <yamamoto@kyms.ne.jp>
2013-02-18 22:54:21 +0900

    Small fix

--- manager/processors/save_template.processor.php
+++ manager/processors/save_template.processor.php
@@ -5,6 +5,7 @@ if(!$modx->hasPermission('save_template')) {
 	$e->dumpError();
 }
 if(isset($_POST['id']) && preg_match('@^[0-9]+$@',$_POST['id'])) $id = $_POST['id'];
+
 $template     = $modx->db->escape($_POST['post']);
 $templatename = $modx->db->escape(trim($_POST['templatename']));
 $description  = $modx->db->escape($_POST['description']);
