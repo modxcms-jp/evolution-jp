3145edcfc7c2f306e54dda4e1fab53b157776f4f
yama <yamamoto@kyms.ne.jp>
2013-02-19 17:27:51 +0900

    Reorder charset

--- manager/includes/site_charsets.php
+++ manager/includes/site_charsets.php
@@ -1,4 +1,6 @@
 <option value="UTF-8" <?php echo $doc_encoding==="UTF-8"? "selected='selected'" : "" ; ?> >UTF-8</option>
+<option value="SJIS-win" <?php echo $doc_encoding==="SJIS-win"? "selected='selected'" : "" ; ?> >SJIS-win</option>
+<option value="eucJP-win" <?php echo $doc_encoding==="eucJP-win"? "selected='selected'" : "" ; ?> >eucJP-win</option>
 <option value="Windows-1251" <?php echo $doc_encoding==="Windows-1251"? "selected='selected'" : "" ; ?> >Windows-1251</option>
 <option value="Windows-1252" <?php echo $doc_encoding==="Windows-1252"? "selected='selected'" : "" ; ?> >Windows-1252</option>
 <option value="KOI8-R" <?php echo $doc_encoding==="KOI8-R"? "selected='selected'" : "" ; ?> >KOI8-R</option>
@@ -12,8 +14,6 @@
 <option value="ISO-8859-8" <?php echo $doc_encoding==="ISO-8859-8"? "selected='selected'" : "" ; ?> >ISO-8859-8(Hebrew)</option>
 <option value="ISO-8859-9" <?php echo $doc_encoding==="ISO-8859-9"? "selected='selected'" : "" ; ?> >ISO-8859-9(Latin 5)</option>
 <option value="ISO-8859-10" <?php echo $doc_encoding==="ISO-8859-10"? "selected='selected'" : "" ; ?> >ISO-8859-10(Latin 6)</option>
-<option value="SJIS-win" <?php echo $doc_encoding==="SJIS-win"? "selected='selected'" : "" ; ?> >SJIS-win</option>
 <option value="Shift_JIS" <?php echo $doc_encoding==="Shift_JIS"? "selected='selected'" : "" ; ?> >Shift_JIS</option>
-<option value="eucJP-win" <?php echo $doc_encoding==="eucJP-win"? "selected='selected'" : "" ; ?> >eucJP-win</option>
 <option value="EUC-JP" <?php echo $doc_encoding==="EUC-JP"? "selected='selected'" : "" ; ?> >EUC-JP</option>
 <option value="BIG-5" <?php echo $doc_encoding==="BIG-5"? "selected='selected'" : "" ; ?> >BIG-5</option>
