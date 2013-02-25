6936856687e1e401f440388246ed8fc5d733e55a
yama <yamamoto@kyms.jp>
2013-02-25 23:18:15 +0900

    default config追加

--- manager/includes/default.config.php
+++ manager/includes/default.config.php
@@ -89,6 +89,9 @@ $default_config['topmenu_security']         = 'user_manage,web_user_manage,role_
 $default_config['topmenu_user']             = 'change_user_pf,change_password,messages';
 $default_config['topmenu_tools']            = 'bk_manager,import_site,export_site,edit_settings';
 $default_config['topmenu_reports']          = 'site_schedule,eventlog_viewer,view_logging,view_sysinfo';
+$default_config['tree_pane_open_default']   = '1';
+$default_config['auto_sleep_user']          = '365';
+$default_config['doc_encoding']             = 'UTF-8';
 
 if(!isset($_GET['a']) || $_GET['a'] !=='17') return $default_config;
 
