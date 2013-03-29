330bc1c3405ed4bcfc9c76e02e111876e52d15af
yama <yamamoto@kyms.jp>
2013-03-29 13:24:04 +0900

    New feature - Set site_revision table in installer

--- install/sql/both_createtables.sql
+++ install/sql/both_createtables.sql
@@ -489,3 +489,20 @@ CREATE TABLE IF NOT EXISTS `{PREFIX}web_user_settings` (
   KEY `webuserid` (`webuser`)
 ) ENGINE=MyISAM COMMENT='Contains web user settings.';
 
+CREATE TABLE IF NOT EXISTS `{PREFIX}site_revision` (
+  `internalKey` int(10) NOT NULL auto_increment,
+  `target` varchar(32) NOT NULL default 'resource' COMMENT 'resource, template, chunk, snippet, plugin, module',
+  `id` int(10) NOT NULL default '0',
+  `revision` int(5) NOT NULL default '0',
+  `status` varchar(20) NOT NULL default 'inherit' COMMENT 'inherit,pending,draft,auto-draft,future,private',
+  `description` varchar(255) NOT NULL default '',
+  `content` text,
+  `editedon` int(20) NOT NULL default '0',
+  `editedby` int(10) NOT NULL default '0',
+  `pub_date` int(20) NOT NULL default '0',
+  `unpub_date` int(20) NOT NULL default '0',
+  `checksum` varchar(50) NOT NULL default '0',
+  PRIMARY KEY `internalKey` (`internalKey`),
+  UNIQUE INDEX `idx_revision` (`target`,`id`,`revision`),
+  KEY `status` (`status`)
+) ENGINE=MyISAM COMMENT='Contains revision data.';
