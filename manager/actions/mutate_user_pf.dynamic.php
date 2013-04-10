53ddb9e2579982849e109bfecb449b6b1359c32b
yama <yamamoto@kyms.jp>
2013-04-11 01:12:14 +0900

    Fix - Update user settings issue

--- manager/actions/mutate_user_pf.dynamic.php
+++ manager/actions/mutate_user_pf.dynamic.php
@@ -324,7 +324,7 @@ foreach($files as $file)
 	if($file!="." && $file!=".." && substr($file,0,1) != '.')
 	{
 		$themename = substr(dirname($file),strrpos(dirname($file),'/')+1);
-		$selectedtext = $themename==$manager_theme ? "selected='selected'" : "" ;
+		$selectedtext = $themename==$usersettings['manager_theme'] ? "selected='selected'" : "" ;
 		echo "<option value='$themename' $selectedtext>".ucwords(str_replace("_", " ", $themename))."</option>";
 	}
 }
