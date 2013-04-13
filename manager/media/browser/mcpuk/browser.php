d3053d2f598a1949ecbe9ab9e42d7b1ca3e68805
yama <yamamoto@kyms.jp>
2013-04-13 10:28:32 +0900

    Fix File browser

--- manager/media/browser/mcpuk/browser.php
+++ manager/media/browser/mcpuk/browser.php
@@ -4,7 +4,7 @@ define('IN_MANAGER_MODE', 'true');
 $self = 'manager/media/browser/mcpuk/browser.php';
 $base_path = str_replace($self,'',str_replace('\\','/',__FILE__));
 require_once("{$base_path}index.php");
-
+header('X-UA-Compatible: IE=EmulateIE7');
 if(!isset($_SESSION['mgrValidated']))
 {
 	die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODX Content Manager instead of accessing this file directly.");
@@ -20,26 +20,22 @@ class FBROWSER
 {
 	function seturl_js()
 	{
-			
-		if(isset($_GET['editor']) && !stristr($_GET['editor'],".."))
-			$seturl_js_filename = 'seturl_js_'  . htmlspecialchars($_GET['editor']) . '.inc';
-		else $seturl_js_filename = '';
+		$editor = isset($_GET['editor']) ? $_GET['editor'] : '';
+		$seturl_js_path = MODX_BASE_PATH . "assets/plugins/{$editor}/seturl.js";
 		
-		$seturl_js_path = MODX_BASE_PATH . 'assets/plugins/';
-		
-		if($seturl_js_filename!='' && is_file($seturl_js_path . $seturl_js_filename))
+		if(!strstr($editor,'..') && is_file($seturl_js_path))
 		{
-			$result = file_get_contents($seturl_js_path . $seturl_js_filename);
+			$result = file_get_contents($seturl_js_path);
 		}
 		else
 		{
-			switch($_GET['editor'])
+			switch($editor)
 			{
 				case 'tinymce' :
 				case 'tinymce3':
-					$editor_path = htmlspecialchars($_GET['editorpath'], ENT_QUOTES);
-					$editor_path = rtrim($editor_path, '/') . '/';
-					$result = file_get_contents('seturl_js_tinymce.inc');
+					$editor = htmlspecialchars($editor, ENT_QUOTES);
+					$editor_path = MODX_BASE_URL . "assets/plugins/{$editor}/";
+					$result = file_get_contents(dirname(__FILE__) . '/seturl_js_tinymce.inc');
 					$result = str_replace('[+editor_path+]', $editor_path, $result);
 					break;
 				default:
