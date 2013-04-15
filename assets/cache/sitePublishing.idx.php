477aecc5ccc6cf27eeff63361aef94c41f9a7cb3
Shohei Akita <fiberalph@gmail.com>
2013-04-16 03:07:56 +0900

    Revert "TEST"
    This reverts commit b644de4a5591684ea8d414eb9a92b2ed972f2801.

--- assets/cache/sitePublishing.idx.php
+++
@@ -1,8 +0,0 @@
-<?php
-$cacheRefreshTime = 0;
-$cache_type = 1;
-$site_sessionname = 'ed0mjge4pg8c4o8ogc0s40g40';
-$site_status = '1';
-$error_reporting = '1';
-$site_url = 'http://localhost/evolution-jp/';
-$base_url = '/evolution-jp/';
