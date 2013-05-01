afdcf6c6dfa269c5b3b82194dc4793b79aa63e68
soushi <soushi@modxcms-jp.com>
2013-05-01 11:35:03 +0900

    Update - version 1.0.9J

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.2J';       // Current version number
-$modx_release_date = '2013/02/25'; // Date of release
+$modx_version = '1.0.9J';       // Current version number
+$modx_release_date = '2013/03/20'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
