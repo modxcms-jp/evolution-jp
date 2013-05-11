eda1c96df6ee7d45c4ddda18b3eac87ad96a56d7
yama <yamamoto@kyms.jp>
2013-05-11 10:42:58 +0900

    Fix - MySQL server name issue

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -66,7 +66,7 @@ class DocumentParser {
 	function __construct()
 	{
         global $database_server;
-        if($database_server==='localhost') $database_server = '127.0.0.1';
+        if(substr(PHP_OS,0,3) === 'WIN' && $database_server==='localhost') $database_server = '127.0.0.1';
         
 		$this->loadExtension('DBAPI') or die('Could not load DBAPI class.'); // load DBAPI class
 		if($this->isBackend()) $this->loadExtension('ManagerAPI');
