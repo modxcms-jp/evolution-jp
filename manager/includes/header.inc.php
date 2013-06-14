cdf607300b62fc530fb1faa77b48861fcc2b35c0
yama <yamamoto@kyms.jp>
2013-06-14 23:44:47 +0900

    jquery.powertip.min.js ファイル名が違うため修正

--- manager/includes/header.inc.php
+++ manager/includes/header.inc.php
@@ -32,7 +32,7 @@ $onManagerMainFrameHeaderHTMLBlock = is_array($evtOut) ? implode("\n", $evtOut) 
     <?php echo $modx->config['manager_inline_style']; ?>
     <script src="media/script/jquery/jquery.min.js" type="text/javascript"></script>
     <script src="media/script/jquery/jquery-migrate.min.js"></script>
-    <script src="media/script/jquery/jquery.powertipmin.js" type="text/javascript"></script>
+    <script src="media/script/jquery/jquery.powertip.min.js" type="text/javascript"></script>
     <script src="media/script/jquery/jquery.alerts.js" type="text/javascript"></script>
     <script type="text/javascript" src="media/script/tabpane.js"></script>
     <script src="media/script/mootools/mootools.js" type="text/javascript"></script>
