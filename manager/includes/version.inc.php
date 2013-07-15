56773a4147426669473e1ae0eab86d2387987662
yama <yamamoto@kyms.jp>
2013-07-15 10:26:20 +0900

    Evolution 1.0.10-r3

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.10J-r2';       // Current version number
-$modx_release_date = '2013/06/18'; // Date of release
+$modx_version = '1.0.10J-r3';       // Current version number
+$modx_release_date = '2013/08/02'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
