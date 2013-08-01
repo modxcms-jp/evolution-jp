04eb783d30076aa97938290bf7a2d1ce6f35aab4
yama <yamamoto@kyms.jp>
2013-08-02 01:20:18 +0900

    Fix - ファイルブラウザ

--- manager/media/browser/mcpuk/connectors/php/config.php
+++ manager/media/browser/mcpuk/connectors/php/config.php
@@ -27,9 +27,10 @@ $base_path = str_replace($self,'',str_replace('\\','/',__FILE__));
 define('IN_MANAGER_MODE', 'true'); // set this so that user_settings will trust us.
 define('MODX_API_MODE',true);
 include_once("{$base_path}index.php");
-if(!isset($_SESSION['mgrValidated']))
-{
+if(!isset($_SESSION['mgrValidated'])) {
+	if(!isset($_SESSION['webValidated'])){
 	die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODX Content Manager instead of accessing this file directly.");
+	}
 }
 
 /**
