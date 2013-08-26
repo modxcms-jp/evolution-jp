f0329d18ecb92441b51e7e92d5eccfb1f791f737
yama <yamamoto@kyms.jp>
2013-08-26 13:05:17 +0900

    TinyMCE - Small fix

--- assets/plugins/tinymce/js/tinymce.linklist.php
+++ assets/plugins/tinymce/js/tinymce.linklist.php
@@ -36,7 +36,6 @@ define('IN_MANAGER_MODE', true);
 $self = 'assets/plugins/tinymce/js/tinymce.linklist.php';
 $base_path = str_replace($self,'',str_replace('\\','/',__FILE__));
 include_once("{$base_path}index.php");
-$modx->db->connect(); 
 
 /* only display if manager user is logged in */
 if ($modx->getLoginUserType() !== 'manager')
