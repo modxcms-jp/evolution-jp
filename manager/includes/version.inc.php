28e077b295eef8b8d3a25bb8b05f8b3e542a3a68
yama <yamamoto@kyms.jp>
2013-10-11 00:17:50 +0900

    revision up

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.10J-r4-b4';       // Current version number
+$modx_version = '1.0.12J-b1';       // Current version number
 $modx_release_date = '2013/10/11'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
