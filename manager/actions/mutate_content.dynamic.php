ff2f2b25e8dbee059b86f40c5ad7fc1b6e6a1731
yama <yamamoto@kyms.jp>
2013-10-15 09:11:41 +0900

    テンプレート変数入力フィールドの変数名欄の幅を調整

--- manager/actions/mutate_content.dynamic.php
+++ manager/actions/mutate_content.dynamic.php
@@ -758,7 +758,7 @@ if (($content['type'] == 'document' || $_REQUEST['a'] == '4') || ($content['type
 			$zindex = ($row['type'] === 'date') ? 'z-index:100;' : '';
 			if($row['type']!=='hidden')
 			{
-				echo '<tr style="height: 24px;"><td valign="top" style="width:150px;white-space:nowrap;"><span class="warning">'.$row['caption']."</span>\n".
+				echo '<tr><td valign="top" class="tvname"><span class="warning">'.$row['caption']."</span>\n".
 			     '<br /><span class="comment">'.$row['description']."</span></td>\n".
                  '<td valign="top" style="position:relative;'.$zindex.'">'."\n".
                  renderFormElement($row['type'], $row['id'], $row['default_text'], $row['elements'], $tvPBV, '', $row)."\n".
