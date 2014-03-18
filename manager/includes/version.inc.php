4594b01cab2937b4b449bfeab3099a797b59cc9f
yamamoto <yamamoto@kyms.jp>
2014-03-18 10:06:41 +0900

    バージョン更新(1.0.12J-r2)

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.12J-r1';       // Current version number
-$modx_release_date = '2013/12/31'; // Date of release
+$modx_version = '1.0.12J-r2-b1';       // Current version number
+$modx_release_date = '2014/03/20'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
