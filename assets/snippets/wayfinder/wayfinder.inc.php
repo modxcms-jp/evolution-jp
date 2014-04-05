fd8b014c162c392c848083685246aefc9c29c4d8
yamamoto <yamamoto@kyms.jp>
2014-04-05 15:36:18 +0900

    Wayfinder - Small fix

--- assets/snippets/wayfinder/wayfinder.inc.php
+++ assets/snippets/wayfinder/wayfinder.inc.php
@@ -651,20 +651,13 @@ class Wayfinder {
 		// based on version by Doze at http://forums.modx.com/thread/41066/support-comments-for-ditto?page=2#dis-post-237942
 		global $modx;
 		$template = '';
-		if(substr($tpl, 0, 5) == '@FILE')
-		{
+		if(substr($tpl, 0, 5) == '@FILE') {
 			$template = file_get_contents(substr($tpl, 6));
-		}
-		elseif(substr($tpl, 0, 5) == '@CODE')
-		{
+		} elseif (substr($tpl, 0, 5) == '@CODE') {
 			$template = substr($tpl, 6);
-		}
-		elseif ($modx->getChunk($tpl) != '')
-		{
+		} elseif ($modx->getChunk($tpl) != '') {
 			$template = $modx->getChunk($tpl);
-		}
-		else
-		{
+		} else {
 			$template = FALSE;
 		}
 		return $template;
