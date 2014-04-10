0c1d0f3a0792c5e634658c6c10f01a0beeaa2f4b
yamamoto <yamamoto@kyms.jp>
2014-04-10 13:00:06 +0900

    Reload mgrDocgroups

--- manager/includes/accesscontrol.inc.php
+++ manager/includes/accesscontrol.inc.php
@@ -193,5 +193,6 @@ else
 			echo "error replacing into active users! SQL: {$sql}\n" . $modx->db->getLastError();
 			exit;
 		}
+		$_SESSION['mgrDocgroups'] = $modx->manager->getMgrDocgroups($fields['internalKey']);
 	}
 }
