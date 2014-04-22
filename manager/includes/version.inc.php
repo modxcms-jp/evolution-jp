bcd322a28792e28c152cbbb346bc09983d3c212e
yamamoto <yamamoto@kyms.jp>
2014-04-22 12:55:47 +0900

    1.0.13J-rc2

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.13J-rc1';       // Current version number
+$modx_version = '1.0.13J-rc2';       // Current version number
 $modx_release_date = '2014/04/18'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
