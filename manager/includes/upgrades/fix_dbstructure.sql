57d9369e32a4766c462120780f61fb02f8b2153d
yamamoto <yamamoto@kyms.jp>
2014-04-23 16:31:15 +0900

    Fix

--- manager/includes/upgrades/fix_dbstructure.sql
+++ manager/includes/upgrades/fix_dbstructure.sql
@@ -127,47 +127,47 @@ REPLACE INTO `{PREFIX}system_eventnames`
 
 # 090-091
 
-ALTER TABLE `modx_site_content` ADD COLUMN `publishedon` int(20) NOT NULL DEFAULT '0' COMMENT 'Date the document was published' AFTER `deletedby`;
+ALTER TABLE `{PREFIX}site_content` ADD COLUMN `publishedon` int(20) NOT NULL DEFAULT '0' COMMENT 'Date the document was published' AFTER `deletedby`;
 
-ALTER TABLE `modx_site_content` ADD COLUMN `publishedby` int(10) NOT NULL DEFAULT '0' COMMENT 'ID of user who published the document' AFTER `publishedon`;
+ALTER TABLE `{PREFIX}site_content` ADD COLUMN `publishedby` int(10) NOT NULL DEFAULT '0' COMMENT 'ID of user who published the document' AFTER `publishedon`;
 
-ALTER TABLE `modx_site_plugins` MODIFY COLUMN `properties` text COMMENT 'Default Properties';
+ALTER TABLE `{PREFIX}site_plugins` MODIFY COLUMN `properties` text COMMENT 'Default Properties';
 
-ALTER TABLE `modx_site_snippets` MODIFY COLUMN `properties` text COMMENT 'Default Properties';
+ALTER TABLE `{PREFIX}site_snippets` MODIFY COLUMN `properties` text COMMENT 'Default Properties';
 
-ALTER TABLE `modx_site_tmplvar_templates`
+ALTER TABLE `{PREFIX}site_tmplvar_templates`
  DROP INDEX `idx_tmplvarid`,
  DROP INDEX `idx_templateid`,
  ADD PRIMARY KEY (`tmplvarid`, `templateid`);
 
-ALTER TABLE `modx_user_roles` ADD COLUMN `view_unpublished` int(1) NOT NULL DEFAULT '0' AFTER `web_access_permissions`;
+ALTER TABLE `{PREFIX}user_roles` ADD COLUMN `view_unpublished` int(1) NOT NULL DEFAULT '0' AFTER `web_access_permissions`;
 
 #091-092
 
 #092-095
 
-ALTER TABLE `modx_categories` MODIFY COLUMN `category` varchar(45) NOT NULL DEFAULT '';
+ALTER TABLE `{PREFIX}categories` MODIFY COLUMN `category` varchar(45) NOT NULL DEFAULT '';
 
-ALTER TABLE `modx_categories` MODIFY COLUMN `category` varchar(45) NOT NULL DEFAULT '';
+ALTER TABLE `{PREFIX}categories` MODIFY COLUMN `category` varchar(45) NOT NULL DEFAULT '';
 
-ALTER TABLE `modx_event_log` MODIFY COLUMN `source` varchar(50) NOT NULL DEFAULT '';
+ALTER TABLE `{PREFIX}event_log` MODIFY COLUMN `source` varchar(50) NOT NULL DEFAULT '';
 
-ALTER TABLE `modx_event_log` MODIFY COLUMN `description` text;
+ALTER TABLE `{PREFIX}event_log` MODIFY COLUMN `description` text;
 
-ALTER TABLE `modx_manager_users` MODIFY COLUMN `username` varchar(100) NOT NULL DEFAULT '';
+ALTER TABLE `{PREFIX}manager_users` MODIFY COLUMN `username` varchar(100) NOT NULL DEFAULT '';
 
-ALTER TABLE `modx_site_content` 
+ALTER TABLE `{PREFIX}site_content` 
  MODIFY COLUMN `pagetitle` varchar(255) NOT NULL default '',
  MODIFY COLUMN `alias` varchar(255) default '',
  MODIFY COLUMN `introtext` text COMMENT 'Used to provide quick summary of the document',
  MODIFY COLUMN `content` mediumtext,
  MODIFY COLUMN `menutitle` varchar(255) NOT NULL DEFAULT '' COMMENT 'Menu title';
 
-ALTER TABLE `modx_site_content` ADD COLUMN `link_attributes` varchar(255) NOT NULL DEFAULT '' COMMENT 'Link attriubtes' AFTER `alias`;
+ALTER TABLE `{PREFIX}site_content` ADD COLUMN `link_attributes` varchar(255) NOT NULL DEFAULT '' COMMENT 'Link attriubtes' AFTER `alias`;
 
-ALTER TABLE `modx_site_htmlsnippets` MODIFY COLUMN `snippet` mediumtext;
+ALTER TABLE `{PREFIX}site_htmlsnippets` MODIFY COLUMN `snippet` mediumtext;
 
-ALTER TABLE `modx_site_modules`
+ALTER TABLE `{PREFIX}site_modules`
  MODIFY COLUMN `name` varchar(50) NOT NULL DEFAULT '',
  MODIFY COLUMN `disabled` tinyint(4) NOT NULL DEFAULT '0',
  MODIFY COLUMN `icon` varchar(255) NOT NULL DEFAULT '' COMMENT 'url to module icon',
@@ -178,53 +178,53 @@ ALTER TABLE `modx_site_modules`
  MODIFY COLUMN `properties` text,
  MODIFY COLUMN `modulecode` mediumtext COMMENT 'module boot up code';
 
-ALTER TABLE `modx_site_module_access`
+ALTER TABLE `{PREFIX}site_module_access`
  MODIFY COLUMN `module` int(11) NOT NULL DEFAULT '0',
  MODIFY COLUMN `usergroup` int(11) NOT NULL DEFAULT '0';
 
-ALTER TABLE `modx_site_module_depobj`
+ALTER TABLE `{PREFIX}site_module_depobj`
  MODIFY COLUMN `module` int(11) NOT NULL DEFAULT '0',
  MODIFY COLUMN `resource` int(11) NOT NULL DEFAULT '0';
 
-ALTER TABLE `modx_site_plugins`
+ALTER TABLE `{PREFIX}site_plugins`
  MODIFY COLUMN `plugincode` mediumtext,
  MODIFY COLUMN `moduleguid` varchar(32) NOT NULL DEFAULT '' COMMENT 'GUID of module from which to import shared parameters';
 
-ALTER TABLE `modx_site_plugin_events`
+ALTER TABLE `{PREFIX}site_plugin_events`
  MODIFY COLUMN `evtid` int(10) NOT NULL DEFAULT '0';
 
-ALTER TABLE `modx_site_plugin_events` ADD COLUMN `priority` INT(10) NOT NULL default '0' COMMENT 'determines the run order of the plugin' AFTER `evtid`;
+ALTER TABLE `{PREFIX}site_plugin_events` ADD COLUMN `priority` INT(10) NOT NULL default '0' COMMENT 'determines the run order of the plugin' AFTER `evtid`;
 
-ALTER TABLE `modx_site_snippets`
+ALTER TABLE `{PREFIX}site_snippets`
  MODIFY COLUMN `snippet` mediumtext,
  MODIFY COLUMN `moduleguid` varchar(32) NOT NULL DEFAULT '' COMMENT 'GUID of module from which to import shared parameters';
 
-ALTER TABLE `modx_site_templates`
+ALTER TABLE `{PREFIX}site_templates`
  MODIFY COLUMN `icon` varchar(255) NOT NULL default '' COMMENT 'url to icon file',
  MODIFY COLUMN `content` mediumtext;
 
-ALTER TABLE `modx_site_tmplvars`
+ALTER TABLE `{PREFIX}site_tmplvars`
  MODIFY COLUMN `name` varchar(50) NOT NULL default '',
  MODIFY COLUMN `elements` text,
  MODIFY COLUMN `display` varchar(20) NOT NULL DEFAULT '' COMMENT 'Display Control',
  MODIFY COLUMN `display_params` text COMMENT 'Display Control Properties',
  MODIFY COLUMN `default_text` text;
 
-ALTER TABLE `modx_site_tmplvar_contentvalues`
+ALTER TABLE `{PREFIX}site_tmplvar_contentvalues`
  MODIFY COLUMN `tmplvarid` int(10) NOT NULL DEFAULT '0' COMMENT 'Template Variable id',
  MODIFY COLUMN `value` text;
 
-ALTER TABLE `modx_site_tmplvar_templates` MODIFY COLUMN `tmplvarid` int(10) NOT NULL DEFAULT '0' COMMENT 'Template Variable id';
+ALTER TABLE `{PREFIX}site_tmplvar_templates` MODIFY COLUMN `tmplvarid` int(10) NOT NULL DEFAULT '0' COMMENT 'Template Variable id';
 
-ALTER TABLE `modx_site_tmplvar_templates` ADD COLUMN `rank` integer(11) NOT NULL DEFAULT '0' AFTER `templateid`;
+ALTER TABLE `{PREFIX}site_tmplvar_templates` ADD COLUMN `rank` integer(11) NOT NULL DEFAULT '0' AFTER `templateid`;
 
-ALTER TABLE `modx_system_eventnames`
+ALTER TABLE `{PREFIX}system_eventnames`
  MODIFY COLUMN  `name` varchar(50) NOT NULL DEFAULT '',
  MODIFY COLUMN `service` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'System Service number';
 
-ALTER TABLE `modx_system_settings` MODIFY COLUMN `setting_value` text;
+ALTER TABLE `{PREFIX}system_settings` MODIFY COLUMN `setting_value` text;
 
-ALTER TABLE `modx_user_attributes`
+ALTER TABLE `{PREFIX}user_attributes`
  MODIFY COLUMN `country` varchar(5) NOT NULL DEFAULT '',
  MODIFY COLUMN `state` varchar(25) NOT NULL DEFAULT '',
  MODIFY COLUMN `zip` varchar(25) NOT NULL DEFAULT '',
@@ -232,27 +232,27 @@ ALTER TABLE `modx_user_attributes`
  MODIFY COLUMN `photo` varchar(255) NOT NULL DEFAULT '' COMMENT 'link to photo',
  MODIFY COLUMN `comment` varchar(255) NOT NULL DEFAULT '' COMMENT 'short comment';
 
-ALTER TABLE `modx_user_settings` MODIFY COLUMN `setting_value` text;
+ALTER TABLE `{PREFIX}user_settings` MODIFY COLUMN `setting_value` text;
 
-ALTER TABLE `modx_user_messages` MODIFY COLUMN `message` text;
+ALTER TABLE `{PREFIX}user_messages` MODIFY COLUMN `message` text;
 
-ALTER TABLE `modx_user_roles` ADD COLUMN `publish_document` int(1) NOT NULL DEFAULT '0' AFTER `save_document`;
+ALTER TABLE `{PREFIX}user_roles` ADD COLUMN `publish_document` int(1) NOT NULL DEFAULT '0' AFTER `save_document`;
 
-ALTER TABLE `modx_web_users`
+ALTER TABLE `{PREFIX}web_users`
  MODIFY COLUMN `username` varchar(100) NOT NULL DEFAULT '',
  MODIFY COLUMN `cachepwd` varchar(100) NOT NULL DEFAULT '' COMMENT 'Store new unconfirmed password' AFTER `password`;
 
-ALTER TABLE `modx_web_user_attributes`
+ALTER TABLE `{PREFIX}web_user_attributes`
  MODIFY COLUMN `country` varchar(25) NOT NULL DEFAULT '',
  MODIFY COLUMN `zip` varchar(25) NOT NULL DEFAULT '',
  MODIFY COLUMN `fax` varchar(100) NOT NULL DEFAULT '',
  MODIFY COLUMN `photo` varchar(255) NOT NULL DEFAULT '' COMMENT 'link to photo';
 
-ALTER TABLE `modx_web_user_settings` MODIFY COLUMN `setting_value` text;
+ALTER TABLE `{PREFIX}web_user_settings` MODIFY COLUMN `setting_value` text;
 
 #095-096
 
-ALTER TABLE `modx_user_roles`
+ALTER TABLE `{PREFIX}user_roles`
  ADD COLUMN `edit_chunk` int(1) NOT NULL DEFAULT '0' AFTER `delete_snippet`,
  ADD COLUMN `new_chunk` int(1) NOT NULL DEFAULT '0' AFTER `edit_chunk`,
  ADD COLUMN `save_chunk` int(1) NOT NULL DEFAULT '0' AFTER `new_chunk`,
@@ -260,7 +260,7 @@ ALTER TABLE `modx_user_roles`
  ADD COLUMN `import_static` int(1) NOT NULL DEFAULT '0' AFTER `view_unpublished`,
  ADD COLUMN `export_static` int(1) NOT NULL DEFAULT '0' AFTER `import_static`;
 
-ALTER TABLE `modx_web_user_attributes`
+ALTER TABLE `{PREFIX}web_user_attributes`
  MODIFY COLUMN `state` varchar(25) NOT NULL DEFAULT '',
  MODIFY COLUMN `zip` varchar(25) NOT NULL DEFAULT '';
 
@@ -268,95 +268,95 @@ ALTER TABLE `modx_web_user_attributes`
 
 #0961-0963
 
-ALTER TABLE `modx_user_roles` ADD COLUMN `empty_trash` int(1) NOT NULL DEFAULT '0' AFTER `delete_document`;
+ALTER TABLE `{PREFIX}user_roles` ADD COLUMN `empty_trash` int(1) NOT NULL DEFAULT '0' AFTER `delete_document`;
 
 #0963-1.0.0
 
 #1.0.3-1.0.4
 
-ALTER TABLE `modx_user_roles` ADD COLUMN `remove_locks` int(1) NOT NULL DEFAULT '0';
+ALTER TABLE `{PREFIX}user_roles` ADD COLUMN `remove_locks` int(1) NOT NULL DEFAULT '0';
 
 #1.0.4-1.0.5
 
-ALTER TABLE `modx_member_groups` ADD UNIQUE INDEX `ix_group_member` (`user_group`,`member`);
+ALTER TABLE `{PREFIX}member_groups` ADD UNIQUE INDEX `ix_group_member` (`user_group`,`member`);
 
-ALTER TABLE `modx_user_attributes` MODIFY COLUMN `comment` text;
+ALTER TABLE `{PREFIX}user_attributes` MODIFY COLUMN `comment` text;
 
-ALTER TABLE `modx_web_groups` ADD UNIQUE INDEX `ix_group_user` (`webgroup`,`webuser`);
+ALTER TABLE `{PREFIX}web_groups` ADD UNIQUE INDEX `ix_group_user` (`webgroup`,`webuser`);
 
-ALTER TABLE `modx_web_user_attributes` MODIFY COLUMN `comment` text;
+ALTER TABLE `{PREFIX}web_user_attributes` MODIFY COLUMN `comment` text;
 
 # Set the private manager group flag
 
-UPDATE modx_documentgroup_names AS dgn
-  LEFT JOIN modx_membergroup_access AS mga ON mga.documentgroup = dgn.id
-  LEFT JOIN modx_webgroup_access AS wga ON wga.documentgroup = dgn.id
+UPDATE {PREFIX}documentgroup_names AS dgn
+  LEFT JOIN {PREFIX}membergroup_access AS mga ON mga.documentgroup = dgn.id
+  LEFT JOIN {PREFIX}webgroup_access AS wga ON wga.documentgroup = dgn.id
   SET dgn.private_memgroup = (mga.membergroup IS NOT NULL),
       dgn.private_webgroup = (wga.webgroup IS NOT NULL);
 
-UPDATE `modx_site_content` SET `type`='reference', `contentType`='text/html' WHERE `type`='' AND `content` REGEXP '^https?://([-\w\.]+)+(:\d+)?/?';
+UPDATE `{PREFIX}site_content` SET `type`='reference', `contentType`='text/html' WHERE `type`='' AND `content` REGEXP '^https?://([-\w\.]+)+(:\d+)?/?';
 
-UPDATE `modx_site_content` SET `type`='document', `contentType`='text/xml' WHERE `type`='' AND `alias` REGEXP '\.(rss|xml)$';
+UPDATE `{PREFIX}site_content` SET `type`='document', `contentType`='text/xml' WHERE `type`='' AND `alias` REGEXP '\.(rss|xml)$';
 
-UPDATE `modx_site_content` SET `type`='document', `contentType`='text/javascript' WHERE `type`='' AND `alias` REGEXP '\.js$';
+UPDATE `{PREFIX}site_content` SET `type`='document', `contentType`='text/javascript' WHERE `type`='' AND `alias` REGEXP '\.js$';
 
-UPDATE `modx_site_content` SET `type`='document', `contentType`='text/css' WHERE `type`='' AND `alias` REGEXP '\.css$';
+UPDATE `{PREFIX}site_content` SET `type`='document', `contentType`='text/css' WHERE `type`='' AND `alias` REGEXP '\.css$';
 
-UPDATE `modx_site_content` SET `type`='document', `contentType`='text/html' WHERE `type`='';
+UPDATE `{PREFIX}site_content` SET `type`='document', `contentType`='text/html' WHERE `type`='';
 
 #1.0.5-1.0.6
 
-ALTER TABLE `modx_site_content` MODIFY COLUMN `template` int(10) NOT NULL default '0';
+ALTER TABLE `{PREFIX}site_content` MODIFY COLUMN `template` int(10) NOT NULL default '0';
 
-ALTER TABLE `modx_site_content` ADD INDEX `typeidx` (`type`);
+ALTER TABLE `{PREFIX}site_content` ADD INDEX `typeidx` (`type`);
 
-ALTER TABLE `modx_site_htmlsnippets` ADD COLUMN `published` int(1) NOT NULL default '1' AFTER `description`;
+ALTER TABLE `{PREFIX}site_htmlsnippets` ADD COLUMN `published` int(1) NOT NULL default '1' AFTER `description`;
 
-ALTER TABLE `modx_site_htmlsnippets` ADD COLUMN `pub_date` int(20) NOT NULL default '0' AFTER `published`;
+ALTER TABLE `{PREFIX}site_htmlsnippets` ADD COLUMN `pub_date` int(20) NOT NULL default '0' AFTER `published`;
 
-ALTER TABLE `modx_site_htmlsnippets` ADD COLUMN `unpub_date` int(20) NOT NULL default '0' AFTER `pub_date`;
+ALTER TABLE `{PREFIX}site_htmlsnippets` ADD COLUMN `unpub_date` int(20) NOT NULL default '0' AFTER `pub_date`;
 
-ALTER TABLE `modx_system_settings` DROP PRIMARY KEY;
+ALTER TABLE `{PREFIX}system_settings` DROP PRIMARY KEY;
 
-ALTER TABLE `modx_system_settings` DROP INDEX `setting_name`;
+ALTER TABLE `{PREFIX}system_settings` DROP INDEX `setting_name`;
 
-ALTER TABLE `modx_system_settings` ADD PRIMARY KEY (`setting_name`);
+ALTER TABLE `{PREFIX}system_settings` ADD PRIMARY KEY (`setting_name`);
 
-ALTER TABLE `modx_user_settings` DROP PRIMARY KEY;
+ALTER TABLE `{PREFIX}user_settings` DROP PRIMARY KEY;
 
-ALTER TABLE `modx_user_settings` ADD PRIMARY KEY (`user`, `setting_name`);
+ALTER TABLE `{PREFIX}user_settings` ADD PRIMARY KEY (`user`, `setting_name`);
 
-ALTER TABLE `modx_web_user_settings` DROP PRIMARY KEY;
+ALTER TABLE `{PREFIX}web_user_settings` DROP PRIMARY KEY;
 
-ALTER TABLE `modx_web_user_settings` ADD PRIMARY KEY (`webuser`, `setting_name`);
+ALTER TABLE `{PREFIX}web_user_settings` ADD PRIMARY KEY (`webuser`, `setting_name`);
 
-ALTER TABLE `modx_site_plugin_events` DROP PRIMARY KEY;
+ALTER TABLE `{PREFIX}site_plugin_events` DROP PRIMARY KEY;
 
-ALTER TABLE `modx_site_plugin_events` ADD PRIMARY KEY (`pluginid`, `evtid`);
+ALTER TABLE `{PREFIX}site_plugin_events` ADD PRIMARY KEY (`pluginid`, `evtid`);
 
-ALTER TABLE `modx_active_users` MODIFY COLUMN `ip` varchar(50) NOT NULL DEFAULT '';
+ALTER TABLE `{PREFIX}active_users` MODIFY COLUMN `ip` varchar(50) NOT NULL DEFAULT '';
 
-ALTER TABLE `modx_site_tmplvar_contentvalues` ADD FULLTEXT `value_ft_idx` (`value`);
+ALTER TABLE `{PREFIX}site_tmplvar_contentvalues` ADD FULLTEXT `value_ft_idx` (`value`);
 
-ALTER TABLE `modx_user_roles` ADD COLUMN `view_schedule` int(1) NOT NULL DEFAULT '0' AFTER `remove_locks`;
+ALTER TABLE `{PREFIX}user_roles` ADD COLUMN `view_schedule` int(1) NOT NULL DEFAULT '0' AFTER `remove_locks`;
 
 #1.0.8-1.0.8J-r1
 
-ALTER TABLE `modx_site_templates` ADD COLUMN `parent` int(10) NOT NULL default '0' AFTER `content`;
+ALTER TABLE `{PREFIX}site_templates` ADD COLUMN `parent` int(10) NOT NULL default '0' AFTER `content`;
 
 # end related to #MODX-1321
 
-ALTER TABLE `modx_site_content` DROP INDEX `content_ft_idx`;
+ALTER TABLE `{PREFIX}site_content` DROP INDEX `content_ft_idx`;
 
-UPDATE `modx_site_plugins` SET disabled='1' WHERE `name`='ダッシュボード・あなたの情報' OR `name`='ダッシュボード・オンライン情報';
+UPDATE `{PREFIX}site_plugins` SET disabled='1' WHERE `name`='ダッシュボード・あなたの情報' OR `name`='ダッシュボード・オンライン情報';
 
 #1.0.10-1.0.12J
 
-ALTER TABLE `modx_user_attributes`
+ALTER TABLE `{PREFIX}user_attributes`
  ADD COLUMN `street` varchar(255) NOT NULL default '' AFTER `country`,
  ADD COLUMN `city` varchar(255) NOT NULL default '' AFTER `street`;
 
-ALTER TABLE `modx_web_user_attributes`
+ALTER TABLE `{PREFIX}web_user_attributes`
  ADD COLUMN `street` varchar(255) NOT NULL default '' AFTER `country`,
  ADD COLUMN `city` varchar(255) NOT NULL default '' AFTER `street`;
 
