73629e7fd114f37b1c373d7cd58d97c301a82991
yamamoto <yamamoto@kyms.jp>
2014-04-29 21:15:54 +0900

    Fix

--- manager/processors/save_content.processor.php
+++ manager/processors/save_content.processor.php
@@ -783,7 +783,7 @@ function goNextAction($id) {
 				$header .= 'a=4';
 			elseif($form_v['type']==='reference')
 				$header .= 'a=72';
-			$header .= "pid={$parent}&r=1";
+			$header .= "&pid={$parent}&r=1";
 			break;
 		case '2':
 			$header = "Location: index.php?a=27&id={$id}&r=1&stay=2";
