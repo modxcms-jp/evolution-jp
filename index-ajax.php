e1586766b60fb7ded05e19a30609531796d065ce
yama <yamamoto@kyms.jp>
2014-06-06 23:29:45 +0900

    Small fix

--- index-ajax.php
+++ index-ajax.php
@@ -14,14 +14,20 @@ header('Cache-Control: private, must-revalidate');
 
 if(isset($_GET['q']) && $_GET['q']!=='')       $q = $_GET['q'];
 elseif(isset($_POST['q']) && $_POST['q']!=='') $q = $_POST['q'];
-else exit;
+else {
+	header('HTTP/1.1 404 Not Found');
+	exit();
+}
 
 define('MODX_API_MODE', true);
 include_once('index.php');
 $q = MODX_BASE_PATH . $q;
 $q = str_replace('\\','/',$q);
 
-if(!is_file($q) || strtolower(substr($q,-4))!=='.php') exit;
+if(!is_file($q) || strtolower(substr($q,-4))!=='.php') {
+	header('HTTP/1.1 404 Not Found');
+	exit();
+}
 
 // permission check
 $allowed = false;
@@ -33,3 +39,7 @@ foreach($allowed_dirs as $allowed_dir) {
     }
 }
 if ($allowed) include_once($q);
+else {
+	header('HTTP/1.1 404 Not Found');
+	exit();
+}
