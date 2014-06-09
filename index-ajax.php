0a11d91f145507bbefe33f34460fce27dde55f2e
yama <yamamoto@kyms.jp>
2014-06-09 13:03:44 +0900

    Small fix
    https://github.com/modxcms/evolution/issues/264
    
    https://github.com/modxcms-jp/evolution-jp/blob/stable/manager/includes/initialize.inc.php#L74-L80
    Already defined

--- index-ajax.php
+++ index-ajax.php
@@ -2,44 +2,36 @@
 // Add items to this array corresponding to which directories within assets/snippets/ can be used by this file.
 // Do not add entries unneccesarily.
 // Any PHP files in these directories can be executed by any user.
-$allowed_dirs = array('assets/snippets/ajaxSearch/');
-
-// set some settings, and address some IE issues
-@ini_set('url_rewriter.tags', '');
-@ini_set('session.use_trans_sid', 0);
-@ini_set('session.use_only_cookies',1);
-session_cache_limiter('');
-header('P3P: CP="NOI NID ADMa OUR IND UNI COM NAV"'); // header for weird cookie stuff. Blame IE.
-header('Cache-Control: private, must-revalidate');
+$allowed_dirs[] = 'assets/snippets/ajaxSearch/';
 
 if(isset($_GET['q']) && $_GET['q']!=='')       $q = $_GET['q'];
 elseif(isset($_POST['q']) && $_POST['q']!=='') $q = $_POST['q'];
-else {
-	header('HTTP/1.1 404 Not Found');
-	exit();
-}
+else force_exit();
 
-define('MODX_API_MODE', true);
-include_once('index.php');
-$q = MODX_BASE_PATH . $q;
+$base_path = str_replace('\\','/',dirname(__FILE__)) . '/';
+$q = $base_path . $q;
 $q = str_replace('\\','/',$q);
+$file_ext = strtolower(substr($q,-4));
 
-if(!is_file($q) || strtolower(substr($q,-4))!=='.php' || strpos($q,MODX_BASE_PATH.'manager/')===0) {
-	header('HTTP/1.1 404 Not Found');
-	exit();
-}
+if(!is_file($q) || $file_ext!=='.php' || strpos($q, "{$base_path}assets/snippets/")!==0)
+	force_exit();
 
 // permission check
 $allowed = false;
 foreach($allowed_dirs as $allowed_dir) {
-    if(strpos($q, MODX_BASE_PATH . $allowed_dir)===0)
+    if(strpos($q, $base_path . $allowed_dir)===0)
     {
-        $allowed = true;
-        break;
+        define('MODX_API_MODE', true);
+        include_once('index.php');
+        include_once($q);
+        exit;
     }
 }
-if ($allowed) include_once($q);
-else {
-	header('HTTP/1.1 404 Not Found');
-	exit();
+
+force_exit();
+
+// Force exit Function (404 Not Found)
+function force_exit() {
+	header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+	exit('404 Not Found');
 }
