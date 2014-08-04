399a89b68b15c6a860de9a3f97d0d56840298036
yama <yamamoto@kyms.jp>
2014-08-04 12:29:19 +0900

    Refactor - contextmenu.php

--- manager/includes/controls/contextmenu.php
+++ manager/includes/controls/contextmenu.php
@@ -1,17 +1,18 @@
 <?php
 
-global $ContextMenuCnt;
-$ContextMenuCnt = 0;
-
 class ContextMenu {
-
+	
+	var $i;
+	
 	function ContextMenu($id='',$width=120,$visible=false) {
 		global $ContextMenuCnt;
+		if(!$ContextMenuCnt) $ContextMenuCnt = 0;
 		$ContextMenuCnt++;
-		$this->html = "";
+		$this->html = '';
 		$this->visible = $visible ? $visible:false;
 		$this->width = is_numeric($width) ? intval($width):120;
 		$this->id = isset($id) ? $id : "cntxMnu{$ContextMenuCnt}"; // set id
+		$this->i = 0;
 	}
 
 	function addItem($text,$action="",$img="",$disabled=0){
@@ -27,6 +28,7 @@ class ContextMenu {
 				<img src='$img' align='absmiddle' />&nbsp;$text
 			</div>
 		";
+		$this->i++;
 	}
 
 	function addSeparator(){
