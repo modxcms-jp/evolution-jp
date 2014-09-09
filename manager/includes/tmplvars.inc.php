cf1ff80074e3807720572856a9fa99998ab93769
yama <yamamoto@kyms.jp>
2014-09-09 22:08:17 +0900

    Fix

--- manager/includes/tmplvars.inc.php
+++ manager/includes/tmplvars.inc.php
@@ -1,9 +1,9 @@
 <?php
 	function renderFormElement($field_type, $field_id, $default_text, $field_elements, $field_value, $field_style='', $row = array())
-		{global $modx;return $modx->renderFormElement($f_type,$f_id,$default_text,$f_elements,$f_value, $f_style,$row);}
+		{global $modx;$modx->loadExtension('SubParser');return $modx->sub->renderFormElement($field_type, $field_id, $default_text, $field_elements, $field_value, $field_style,$row);}
 	function ParseIntputOptions($v)
-		{global $modx;return $modx->ParseIntputOptions($v);}
+		{global $modx;$modx->loadExtension('SubParser');return $modx->sub->ParseIntputOptions($v);}
 	function splitOption($value)
-		{global $modx;return $modx->splitOption($value);}
+		{global $modx;$modx->loadExtension('SubParser');return $modx->sub->splitOption($value);}
 	function isSelected($label,$value,$item,$field_value)
-		{global $modx;return $modx->isSelected($label,$value,$item,$field_value);}
+		{global $modx;$modx->loadExtension('SubParser');return $modx->sub->isSelected($label,$value,$item,$field_value);}
