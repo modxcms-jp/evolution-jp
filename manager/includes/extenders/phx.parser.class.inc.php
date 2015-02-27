9f4b1474c5c71131f720d1e08c53f9794a4f3452
yamamoto <yamamoto@kyms.jp>
2015-02-27 16:17:39 +0900

    Small fix

--- manager/includes/extenders/phx.parser.class.inc.php
+++ manager/includes/extenders/phx.parser.class.inc.php
@@ -620,6 +620,7 @@ class PHx {
 			$char = substr($_tmp,0,1);
 			$_tmp = substr($_tmp,1);
 			$c++;
+			if(64000<$c) exit('phx parse over');
 			if($char===$delim && (substr($_tmp,0,1)===':'))
 				break;
 			else
