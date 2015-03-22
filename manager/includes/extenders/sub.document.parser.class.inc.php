7e9d112282b25db9078c98f3462aded7393e8b0b
yamamoto <yamamoto@kyms.jp>
2015-03-22 12:19:23 +0900

    $modx->addLog
    $msgが配列で渡ってきた場合は文字列化する

--- manager/includes/extenders/sub.document.parser.class.inc.php
+++ manager/includes/extenders/sub.document.parser.class.inc.php
@@ -108,6 +108,7 @@ class SubParser {
 	function addLog($title='no title',$msg='',$type=1)
 	{
 		if($title==='') $title = 'no title';
+		if(is_array($msg)) $msg = '<pre>'.print_r($msg,true).'</pre>';
 		if($msg==='')   $msg   = $_SERVER['REQUEST_URI'];
 		$this->logEvent(0, $type, $msg, $title);
 	}
