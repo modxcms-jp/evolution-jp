cfbadbe80663354e28862f9e6bad4ada293d389d
yamamoto <yamamoto@kyms.jp>
2015-03-24 17:24:45 +0900

    リストア機能 - バージョンチェックを廃止
    目視の判断に任せる

--- manager/processors/restore.processor.php
+++ manager/processors/restore.processor.php
@@ -38,11 +38,6 @@ elseif ($mode=='restore2')
 
 if(!empty($source))
 {
-	$rs = checkVersion($source);
-	if(!$rs) {
-		$modx->webAlertAndQuit($_lang['bk_different_version']);
-		exit;
-	}
 	include_once(MODX_CORE_PATH . 'mysql_dumper.class.inc.php');
 	$dumper = new Mysqldumper();
 	$dumper->import_sql($source);
@@ -50,19 +45,3 @@ if(!empty($source))
 header('Location: index.php?r=9&a=93');
 exit;
 
-
-
-function checkVersion($src) {
-	global $modx;
-	$src = substr($src,0,200);
-	$chkstr = '# MODX Version:';
-	$pos = strpos($src,$chkstr);
-	if($pos===false) return true;
-	$pos += strlen($chkstr);
-	$src = substr($src,$pos);
-	$version = substr($src,0,strpos($src,"\n"));
-	$version = trim($version);
-	if($version===$modx->config['settings_version'])
-		return true;
-	else return false;
-}
