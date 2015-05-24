5b2781e705ff6951aef7e615753e2b084260ef99
yamamoto <yamamoto@kyms.jp>
2015-05-24 20:14:33 +0900

    Start 1.0.15J-b1

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 $modx_version      = '1.0.15J-b1';   // Current version number
-$modx_release_date = '2015/03/23';       // Date of release
+$modx_release_date = '2015/05/24';       // Date of release
 $modx_branch       = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
