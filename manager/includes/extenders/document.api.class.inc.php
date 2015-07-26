08775f6deb29c179bc00ada821f16e41391dee7f
soushi <soushi@modxcms-jp.com>
2015-07-26 16:44:18 +0900

    [API:Document] テンプレート変数の修正にも対応。
    tvを設定するサンプルコードは次の通り。
    
    $rs = new Document();         //新規リソース作成
    $rs->set('pagetitle','test'); //ページタイトル設定
    $rs->setTemplate('xxx');      //テンプレート指定
    $rs->set('tv.キーワード');    //tvの変更方法(その1)
    $rs->setTV('キーワード');     //tvの変更方法(その2)
    $rs->save();                  //保存
    
    他にはgetAllTVs()で全てのtvを配列で取得、中身を変更してsetAllTVs()で一斉更新も可能です。

--- manager/includes/extenders/document.api.class.inc.php
+++ manager/includes/extenders/document.api.class.inc.php
@@ -13,9 +13,10 @@ class Document
 	const LOG_WARN = 2;
 	const LOG_ERR  = 3;
 
-	//private $id='';                  //Resource ID
+	//private $id='';                  // Resource ID
 	private $content = array();        // Site content
-	private $logLevel = self::LOG_ERR; //Output log level
+	private $tv      = array();        // Template Value
+	private $logLevel = self::LOG_ERR; // Output log level
 	private $lastLog = '';             // Last log message
 	private $modx;
 
@@ -81,21 +82,97 @@ class Document
 	}
 
 	/*
-	 * Get field
+	 * リソース値の取得
+	 *
+	 * fieldの先頭に「tv.」をつけるとTVを取得する
 	 *
 	 * @param $field Resource column name
 	 * @return string
 	 *
 	 */
 	public function get($field='content'){
-		if( !empty($field) && array_key_exists($field,$this->content) )
+		if( !empty($field) && array_key_exists($field,$this->content) ){
 			return $this->content[$field];
-		else
-			return false;
+		}
+		$field = $this->getTVName($field);
+		if( $field !== false ){			
+			return $this->getTV($field);
+		}
+		return false;
 	}
 
 	/*
-	 * Set field
+	 * TV取得(名前ベース)
+	 *
+	 * 同じ名前のTVは通常存在しない
+	 * get()と違い先頭に「tv.」があってはいけない
+	 *
+	 * @param $name TV名
+	 * @return string/false
+	 *
+	 */
+	public function getTV($name){
+		foreach( $this->tv as $k => $v ){
+			if( $v['name'] == $name ){
+				return $v['value'];
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * TV取得(idベース)
+	 *
+	 * @param $id TVID
+	 * @return string/false
+	 *
+	 */
+	public function getTVbyID($id){
+		if( !empty($id) && array_key_exists($id,$this->tv) ){
+			return $this->tv[$id]['value'];
+		}
+		return false;					  
+	}
+
+	/*
+	 * すべてのTV取得
+	 *
+	 * フォーマットは次のとおり。
+	 *    [TVID]       ... TVID
+	 *       [name]    ... TV名
+	 *       [value]   ... TV値
+	 *       [default] ... TVデフォルト値
+	 *
+	 * @param none
+	 * @return string/false
+	 *
+	 */
+	public function getAllTVs(){
+		return $this->tv;
+	}
+
+	/*
+	 * TVID取得
+	 *
+	 * 先頭に「tv.」はつけてはいけない
+	 * 
+	 * @param $name テンプレート変数名
+	 * @return int/false
+	 *
+	 */
+	public function getTVID($name){
+		foreach( $this->tv as $k => $v ){
+			if( $v['name'] == $name ){
+				return $k;
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * リソースの値設定
+	 *
+	 * fieldの先頭に「tv.」をつけるとTVに設定する
 	 *
 	 * @param $field Resource column name
 	 * @param $val   new value
@@ -104,11 +181,195 @@ class Document
 	 */
 	public function set($field='content',$val=''){
 		if( array_key_exists($field,$this->content_lists) ){
+			$tmp = $this->content['template'];
 			$this->content[$field] = $val;
+			if( $tmp != $this->content['template'] ){
+				return $this->setTemplatebyID($val);
+			}
 			return true;
+		}
+		$field = $this->getTVName($field);
+		if( $field !== false ){
+			return $this->setTV($field,$val);
 		}
 		$this->logWarn('Field not exist:'.$field);
 		return true;
+	}
+
+	/*
+	 * TV設定
+	 *
+	 * @param $name TV名
+	 * @param $val  値(無指定、もしくはnull指定でデフォルト値に戻す)
+	 * @return bool
+	 *
+	 */
+	public function setTV($name,$val=null){
+		foreach( $this->tv as $k => $v ){			
+			if( $v['name'] == $name ){
+				return $this->setTVbyID($k,$val);
+			}
+		}
+		$this->logWarn('TV not exist:'.$name);
+		return false;
+	}
+
+	/*
+	 * TV設定(idベース)
+	 *
+	 * @param $id   TVID
+	 * @param $val  値(無指定、もしくはnull指定でデフォルト値に戻す)
+	 * @return bool
+	 *
+	 */
+	public function setTVbyID($id,$val=null){
+		if( !empty($id) && array_key_exists($id,$this->tv) ){
+			if( is_null($val) ){
+				$this->tv[$id]['value'] = $this->tv[$id]['default'];
+			}else{
+				$this->tv[$id]['value'] = $val;
+			}
+			return true;
+		}
+		$this->logWarn('TV not exist:'.$name);
+		return false;
+	}
+
+	/*
+	 * すべてのTVを一括設定
+	 *
+	 * フォーマットは次の通り
+	 *    [TVID]       ... TVID
+	 *       [value]   ... TV値
+	 *
+	 * ※getAllTVs()のフォーマットに合わせてます
+	 * ※配列にnameやdefaultが含まれていても無視します
+	 * ※valueにnullを設定するとデフォルト値を採用
+	 *
+	 * @param $tv TV設定
+	 * @return bool
+	 *
+	 */
+	public function setAllTVs($tv){
+		if( !is_array($tv) ){ return false; }
+		foreach( $this->tv as $k => $v){
+			if( isset($tv[$k]) ){
+				$this->setTVbyID($k,$tv[$k]['value']);
+			}
+		}
+		return true;
+	}
+
+	/*
+	 * テンプレートの指定
+	 *
+	 * テンプレートを名前で指定する。
+	 *
+	 * ※tidに合わせて$this->tvを変更
+	 * ※無効なテンプレートIDの場合、tidは0になる
+	 *
+	 * @param $name テンプレート名
+	 * @return bool
+	 *
+	 */
+	public function setTemplate($name){
+		$rs  = $this->modx->db->select('id','[+prefix+]site_templates',"templatename= '" . $this->modx->db->escape($name) . "'");
+		if( $row = $this->modx->db->getRow($rs) ){
+			return $this->setTemplatebyID($row['id']);
+		}
+		$this->logWarn('無効なテンプレート名を指定しています。');
+		
+		return false;
+	}
+
+	/*
+	 * テンプレートの指定(idベース)
+	 *
+	 * ※tidに合わせて$this->tvを変更
+	 * ※無効なテンプレートIDの場合、tidは0になる
+	 *
+	 * @param $tid テンプレートID
+	 * @return bool
+	 *
+	 */
+	public function setTemplatebyID($tid){
+		if( !$this->isInt($tid,0) ){
+			return false;
+		}
+		if( $tid != 0 ){
+			$rs  = $this->modx->db->select('id','[+prefix+]site_templates',"id= $tid");
+			if( !($row = $this->modx->db->getRow($rs)) ){
+				$this->logWarn('無効なテンプレートIDを指定しています。');
+				$tid = 0;
+			}
+		}
+		$this->content['template'] = $tid;
+		//tv読み直し
+		$this->tv = array();
+		if( $this->DocumentExist($this->content['id']) ){
+			//tv読み込み(値付)
+			$sql = <<< SQL_QUERY
+SELECT tv.id
+      ,tv.name
+      ,IFNULL(tvc.value,tv.default_text) AS value
+      ,tv.default_text
+FROM [+prefix+]site_tmplvars AS tv
+  LEFT JOIN [+prefix+]site_tmplvar_templates AS tvt
+    ON tvt.tmplvarid = tv.id
+  LEFT JOIN [+prefix+]site_tmplvar_contentvalues AS tvc
+    ON tvc.tmplvarid = tv.id AND tvc.contentid = {$this->content['id']}
+WHERE tvt.templateid = {$this->content['template']}
+
+SQL_QUERY;
+
+			$sql = str_replace('[+prefix+]',$this->modx->db->config['table_prefix'],$sql);
+			$rs  = $this->modx->db->query($sql);
+			while( $row = $this->modx->db->getRow($rs) ){
+				$this->tv[$row['id']]['name']    = $row['name'];
+				$this->tv[$row['id']]['value']   = $row['value'];
+				$this->tv[$row['id']]['default'] = $row['default_text'];
+			}
+		}else{
+			//tv読み込み(値無)
+			$sql = <<< SQL_QUERY
+SELECT tv.id
+	  ,tv.name
+	  ,tv.default_text
+FROM [+prefix+]site_tmplvars AS tv
+  LEFT JOIN [+prefix+]site_tmplvar_templates AS tvt 
+    ON tvt.tmplvarid = tv.id
+  LEFT JOIN [+prefix+]site_templates AS st
+	ON st.id = tvt.templateid
+WHERE st.id = {$this->content['template']}
+SQL_QUERY;
+
+			$sql = str_replace('[+prefix+]',$this->modx->db->config['table_prefix'],$sql);
+			$rs  = $this->modx->db->query($sql);
+			while( $row = $this->modx->db->getRow($rs) ){
+				$this->tv[$row['id']]['name']    = $row['name'];
+				$this->tv[$row['id']]['value']   = $row['default_text'];
+				$this->tv[$row['id']]['default'] = $row['default_text'];
+			}
+		}	
+		return true;
+	}
+
+	/*
+	 * リソースの存在確認
+	 *
+	 * @param $id リソースID
+	 * @return bool  
+	 *
+	 */
+	public function DocumentExist($id){
+		if( !$this->isInt($id,1) ){
+			return false;
+		}
+		$rs  = $this->modx->db->select('id','[+prefix+]site_content',"id = $id");
+		if( $row = $this->modx->db->getRow($rs) ){
+			return true;
+		}
+		return false;
 	}
 
 	/*
@@ -120,20 +381,28 @@ class Document
 	 */
 	public function load($id){
 		$this->content = $this->content_lists;
-		if( $this->isInt($id,1) ){
+		$this->tv = array();
+		if( !$this->isInt($id,1) ){
+			return false;
+		}else{
 			$rs  = $this->modx->db->select('*','[+prefix+]site_content','id='.$id);
 			$row = $this->modx->db->getRow($rs);
-			if( !empty($row) ){
-				$this->content = $row;
-				return true;
+			if( empty($row) ){
+				$this->logerr('リソースの読み込みに失敗しました。');
+				return false;
 			}
-
+			$this->content = $row;
+			//tv読み込み
+			$this->setTemplatebyID($this->content['template']);			
 		}
-		return false;
+		return true;
 	}
   
 	/*
-	 * save resource
+	 * リソースの保存
+	 *
+	 * fieldを指定すれば特定のレコードだけ保存する
+	 * (先頭に「tv.」をつけるとTVが対象)
 	 *
 	 * @param $fields     Save target fields(blank or * = all)
 	 * @param $clearCache Clear cache
@@ -141,43 +410,105 @@ class Document
 	 *
 	 */
 	public function save($fields='*',$clearCache=true){
-		$c = array(); //insert/update content
+		$c  = array(); //新規/更新対象content
+		$tv = array(); //新規/更新対象tv
 		if( empty($fields) || $fields == '*' ){
-			foreach( $this->content as $key => $val )
-			{
-				if( !is_null($this->content[$key]) )
+			foreach( $this->content as $key => $val ){
+				if( !is_null($this->content[$key]) ){
 					$c[$key] = $val;
+				}
 			}
+			$tv = $this->tv;
 		}else{
 			if( !is_array($fields) )
 				$fields = explode(',',$fields);
 			foreach( $fields as $val ){
-				if( isset($this->content[$val]) && !is_null($this->content[$val]) )
+				if( isset($this->content[$val]) && !is_null($this->content[$val]) ){
 					$c[$val] = $this->content[$val];
-				else
-					$this->logWarn('Fields not exist:'.$val);
+				}else{
+					$tmp = $this->getTVName($val);
+					if( $tmp !== false ){
+						$tmp = $this->getTVID($tmp);
+						$tv[$tmp] = $this->tv[$tmp];
+					}else{
+						$this->logWarn('Fields not exist:'.$val);
+					}
+
+				}
 			}
 		}
 
-		// date control
+		// 日付調整
 		foreach( $this->content_type_date as $val ){
 			if( isset($c[$val]) && $c[$val] == 'now()' )
 				$c[$val] = time();
 		}
 
-		unset($c['id']);
-		$c = $this->modx->db->escape($c);
-
+		//idは途中エラー時はfalseに変化
 		if( $this->isInt($this->content['id'],1) ){
-			//update
 			$id = $this->content['id'];
-			if( !$this->modx->db->update($c,'[+prefix+]site_content','id='.$id) )
-				$id = false;
 		}else{
-			//insert
-			$id = $this->modx->db->insert($c,'[+prefix+]site_content');
+			$id = 0; //新規
 		}
+		
+		//content登録
+		unset($c['id']);
+		if( !empty($c) ){
+			$c = $this->modx->db->escape($c);
 
+			if( $id != 0 ){
+				//update
+				if( !$this->modx->db->update($c,'[+prefix+]site_content','id='.$id) )
+					$id = false;
+			}else{
+				//insert
+				$id = $this->modx->db->insert($c,'[+prefix+]site_content');
+			}
+		}
+		
+		//TVの登録
+		if( $id === false ){
+			$this->logerr('contentの保存に失敗しているため、tvの保存は行いません。');
+		}elseif( $id == 0 ){
+			$this->logerr('新規リソースの場合は最初にリソースを保存する必要があります。');
+			$id = false;
+		}else{
+			$errflag = false;
+
+			$tmp='';
+			foreach( $tv as $k => $v ){
+				if( $v['value'] === $v['default'] ){
+					//デフォルト時は削除
+					if( $this->isInt($k,1) ){
+						$this->modx->db->delete('[+prefix+]site_tmplvar_contentvalues',
+												"tmplvarid = $k AND contentid = $id");
+					}
+				}else{
+					$rs  = $this->modx->db->select('id','[+prefix+]site_tmplvar_contentvalues',"tmplvarid = $k AND contentid = $id");
+					if( $row = $this->modx->db->getRow($rs) ){
+						$rs = $this->modx->db->update(array( 'value' => $this->modx->db->escape($v['value']) ),
+													  '[+prefix+]site_tmplvar_contentvalues',
+													  "tmplvarid = $k AND contentid = $id");
+						if( !$rs ){
+							$errflag = true;
+						}
+					}else{
+						$rs = $this->modx->db->insert(array( 'tmplvarid' => $k ,
+															 'contentid' => $id ,
+															 'value' => $this->modx->db->escape($v['value'])
+						                                   ),
+													  '[+prefix+]site_tmplvar_contentvalues');
+						if( !$rs ){
+							$errflag = true;
+						}
+					}
+				}
+			}
+		}
+		if( $errflag ){
+			$id = false;
+		}
+		
 		if( $id !== false && $clearCache )
 			$this->modx->clearCache();
             
@@ -240,7 +571,7 @@ class Document
 	private function logging($level,$msg=''){
 		$this->lastLog = $msg;
 		if( $this->logLevel <= $level )
-			$this->modx->logEvent(4,$level,$msg,'Document Object');
+			$this->modx->logEvent(4,$level,$msg,'Document Object API');
 	}
 	
 	private function loginfo($msg=''){
@@ -254,7 +585,30 @@ class Document
 	private function logerr($msg=''){
 		$this->logging(self::LOG_ERR,$msg);   
 	}
-  
+
+	/*
+	 * TV名を返す
+	 *
+	 * 先頭に「tv.」がある場合は削除される
+	 * TV名ではない場合はfalseを返す
+	 *
+	 * @param $name 文字列
+	 * @return string/false
+	 *
+	 */
+	private function getTVName($name){
+		$pos = strpos($name,'tv.');
+		if( $pos === 0 ){
+			$name = substr($name,3);
+		}
+		foreach( $this->tv as $k => $v ){
+			if( $v['name'] == $name ){
+				return $name;
+			}
+		}
+		return false;
+	}
+									  
 	//--- Sub method (This method might be good to be another share class.)
 	/*
 	 * Number check
