f1f813349cbb18480c7d01325a55d71b86ec04ff
yamamoto <yamamoto@kyms.jp>
2015-09-06 15:00:56 +0900

    Fix

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -299,7 +299,7 @@ class DocumentParser {
             if(isset($qs['id'])) unset($qs['id']);
             if(0 < count($qs)) $this->qs_hash = '_' . md5(join('&',$qs));
             else $this->qs_hash = '';
-            $userID = $modx->getLoginUserID('web');
+            $userID = $this->getLoginUserID('web');
             if($userID) $this->qs_hash = md5($this->qs_hash."^{$userID}^");
         }
         
