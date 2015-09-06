7ef7ce57a6cb1a24421e5ff2955088b6623640d1
yamamoto <yamamoto@kyms.jp>
2015-09-06 13:46:47 +0900

    Minor fix

--- assets/templates/help/01Help_Overview.php
+++ assets/templates/help/01Help_Overview.php
@@ -15,7 +15,7 @@ if(!defined('IN_MANAGER_MODE') || IN_MANAGER_MODE != 'true') exit();
 <ul>
 <li><a href="http://www.php.net/copyright.php" target="_blank">Copyright&copy; 2001-2012 The PHP Group</a></li>
 <li><a href="http://www.mysql.com" target="_blank">&copy; 2012, Oracle Corporation and/or its affiliates</a></li>
-<li><a href="http://www.destroydrop.com" target="_blank">&copy;2002-2003 Geir Landr&ouml;</a></li>
+<li><a href="http://www.destroydrop.com/javascripts/tree/" target="_blank">&copy;2002-2003 Geir Landr&ouml;</a></li>
 <li><a href="http://www.everaldo.com" target="_blank">Copyright (c)  2006-2007  Everaldo Coelho.</a></li>
 </ul>
 <?php echo $_lang['credits_shouts_msg']; ?>
