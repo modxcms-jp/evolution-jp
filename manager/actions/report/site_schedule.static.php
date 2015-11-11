8c1d50e6bd20bc6d51451974e372493f88497e4c
soushi <soushi@modxcms-jp.com>
2015-11-11 14:34:13 +0900

    公開状態一覧ページのテーブルカラムの表示順を統一。

--- manager/actions/report/site_schedule.static.php
+++ manager/actions/report/site_schedule.static.php
@@ -31,8 +31,8 @@ if($total<1) {
   <table border="0" cellpadding="2" cellspacing="0"  class="sortabletable sortable-onload-3 rowstyle-even" id="table-1" width="100%">
     <thead>
       <tr bgcolor="#CCCCCC">
-        <th class="sortable"><b><?php echo $_lang['resource'];?></b></th>
         <th class="sortable"><b><?php echo $_lang['id'];?></b></th>
+        <th class="sortable"><b><?php echo $_lang['resource'];?></b></th>
         <th class="sortable"><b><?php echo $_lang['publish_date'];?></b></th>
       </tr>
     </thead>
@@ -41,8 +41,8 @@ if($total<1) {
 	while ($row = $modx->db->getRow($rs)) {
 ?>
     <tr>
-      <td><a href="index.php?a=3&id=<?php echo $row['id'] ;?>"><?php echo $row['pagetitle']?></a></td>
 	  <td><?php echo $row['id'] ;?></td>
+      <td><a href="index.php?a=3&id=<?php echo $row['id'] ;?>"><?php echo $row['pagetitle']?></a></td>
       <td><?php echo $modx->toDateFormat($row['pub_date']+$server_offset_time)?></td>
     </tr>
 <?php
@@ -72,8 +72,8 @@ if($total<1) {
   <table border="0" cellpadding="2" cellspacing="0"  class="sortabletable sortable-onload-3 rowstyle-even" id="table-2" width="100%">
     <thead>
       <tr bgcolor="#CCCCCC">
-        <th class="sortable"><b><?php echo $_lang['resource'];?></b></th>
         <th class="sortable"><b><?php echo $_lang['id'];?></b></th>
+        <th class="sortable"><b><?php echo $_lang['resource'];?></b></th>
         <th class="sortable"><b><?php echo $_lang['unpublish_date'];?></b></th>
       </tr>
     </thead>
@@ -82,8 +82,8 @@ if($total<1) {
 	while ($row = $modx->db->getRow($rs)) {
 ?>
     <tr>
-      <td><a href="index.php?a=3&id=<?php echo $row['id'] ;?>"><?php echo $row['pagetitle'] ;?></a></td>
 	  <td><?php echo $row['id'] ;?></td>
+      <td><a href="index.php?a=3&id=<?php echo $row['id'] ;?>"><?php echo $row['pagetitle'] ;?></a></td>
       <td><?php echo $modx->toDateFormat($row['unpub_date']+$server_offset_time) ;?></td>
     </tr>
 <?php
