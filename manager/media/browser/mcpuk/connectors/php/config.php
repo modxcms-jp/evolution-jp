9101fc06593b7283b81fbdf8049b711212a9767e
yamamoto <yamamoto@kyms.jp>
2016-01-18 11:23:28 +0900

    upload_maxsizeが空の場合の応急処置
    とりあえず5000000をセット

--- manager/media/browser/mcpuk/connectors/php/config.php
+++ manager/media/browser/mcpuk/connectors/php/config.php
@@ -122,6 +122,7 @@ $fckphp_config['auth']['Handler']['SharedKey']="->Shared_K3y-F0R*5enD1NG^auth3nt
 //First area options are commented
 
 //File Area
+if(empty($upload_maxsize)) $upload_maxsize = 5000000;
 $fckphp_config['ResourceAreas']['files'] =array(
 	'AllowedExtensions'	=>	$upload_files,
 	'AllowedMIME'		=>	array(),
