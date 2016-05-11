8810c02475799436005388271feb3455c1b9ddbe
yamamoto <yamamoto@kyms.jp>
2016-05-11 09:44:28 +0900

    Update version 1.0.16J-rc1

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.0.15J';    // Current version number
-$modx_release_date = '2016/02/12'; // Date of release
+$modx_version      = '1.0.16J-rc1';    // Current version number
+$modx_release_date = '2016/05/11'; // Date of release
 $modx_branch       = 'Evolution';  // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
