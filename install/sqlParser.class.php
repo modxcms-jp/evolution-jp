3f2b74475d8d36a93f73564159ad5cc117de3349
yamamoto <yamamoto@kyms.jp>
2016-07-07 22:44:42 +0900

    Refactor

--- install/sqlParser.class.php
+++ install/sqlParser.class.php
@@ -11,23 +11,23 @@ class SqlParser {
 
 	function __construct() {
 		$this->base_path = str_replace('\\','/', dirname(getcwd())).'/';
-		$this->prefix = 'modx_';
-		$this->adminname = 'admin';
-		$this->adminpass = 'password';
-		$this->adminemail = 'example@example.com';
-		$this->connection_charset = 'utf8';
+		$this->prefix               = 'modx_';
+		$this->adminname            = 'admin';
+		$this->adminpass            = 'password';
+		$this->adminemail           = 'example@example.com';
+		$this->connection_charset   = 'utf8';
 		$this->connection_collation = 'utf8_general_ci';
-		$this->showSqlErrors = true;
-		$this->managerlanguage = 'english';
+		$this->showSqlErrors        = true;
+		$this->managerlanguage      = 'english';
 	}
-
+	
 	function intoDB($filename) {
 	    global $mysqli;
 		
 		// check to make sure file exists
 		$path = "{$this->base_path}install/sql/{$filename}";
 		if (!is_file($path)) {
-			$this->mysqlErrors[] = array("error" => "File '$path' not found");
+			$this->mysqlErrors[] = array("error" => "File '{$path}' not found");
 			$this->installFailed = true ;
 			return false;
 		}
@@ -36,8 +36,8 @@ class SqlParser {
 		
 		$idata = str_replace("\r", '', $idata);
 		
-		$dbVersion = $mysqli->server_info;
 		$dbVersion = (float) $mysqli->server_info;
+		
 		if(version_compare($dbVersion,'4.1.0', '>='))
 		{
 			$char_collate = "DEFAULT CHARSET={$this->connection_charset} COLLATE {$this->connection_collation}";
@@ -48,21 +48,19 @@ class SqlParser {
 		$ph = array();
 		$ph['PREFIX']            = $this->prefix;
 		$ph['ADMINNAME']         = $this->adminname;
-		$ph['ADMINFULLNAME']     = substr($this->adminemail,0,strpos($this->adminemail,'@'));
-		$ph['ADMINEMAIL']        = $this->adminemail;
 		$ph['ADMINPASS']         = genHash($this->adminpass, '1');
+		$ph['ADMINEMAIL']        = $this->adminemail;
+		$ph['ADMINFULLNAME']     = substr($this->adminemail,0,strpos($this->adminemail,'@'));
 		$ph['MANAGERLANGUAGE']   = $this->managerlanguage;
 		$ph['DATE_NOW']          = time();
 		$idata = parse($idata,$ph,'{','}');
 		
 		$sql_array = preg_split('@;[ \t]*\n@', $idata);
 		
-		$num = 0;
-		foreach($sql_array as $sql_entry)
+		foreach($sql_array as $sql)
 		{
-			$sql_do = trim($sql_entry, "\r\n; ");
-			$num++;
-			if ($sql_do) $mysqli->query($sql_do);
+			$sql = trim($sql, "\r\n; ");
+			if ($sql) $mysqli->query($sql);
 			if($mysqli->error)
 			{
 				// Ignore duplicate and drop errors - Raymond
@@ -72,7 +70,7 @@ class SqlParser {
 					if ($errno == 1060 || $errno == 1061 || $errno == 1091 || $errno == 1054) continue;
 				}
 				// End Ignore duplicate
-				$this->mysqlErrors[] = array("error" => $mysqli->error, "sql" => $sql_do);
+				$this->mysqlErrors[] = array("error" => $mysqli->error, "sql" => $sql);
 				$this->installFailed = true;
 			}
 		}
