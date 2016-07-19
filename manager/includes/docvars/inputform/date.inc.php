d416c36f57f07fac60446cf5cae40ccfaadd71c8
yamamoto <yamamoto@kyms.jp>
2016-07-20 00:11:34 +0900

    Fix

--- manager/includes/docvars/inputform/date.inc.php
+++ manager/includes/docvars/inputform/date.inc.php
@@ -2,7 +2,29 @@
 <a onclick="document.forms['mutate'].elements['[+id+]'].value='';document.forms['mutate'].elements['[+id+]'].onblur(); return true;"
    style="cursor:pointer; cursor:hand"><img src="[+cal_nodate+]" border="0" alt="No date"></a>
 <script type="text/javascript">
-	window.addEvent('domready', function() {
-		new DatePicker($('[+id+]'), {'yearOffset' : [+yearOffset+], 'format' : '[+datetime_format+]'});
+	jQuery(function() {
+        var start = new Date();
+        start.setHours(0);
+        start.setMinutes(0);
+        
+        var options = {
+            language      : 'ja',
+            timepicker    : true,
+            todayButton   : new Date(),
+            keyboardNav   : false,
+            startDate     : start,
+            autoClose     : true,
+            toggleSelected: false,
+            clearButton   : true,
+            minutesStep   : 5,
+            dateFormat    : 'yyyy/mm/dd',
+            onSelect      : function (fd, d, picker) {
+                documentDirty = true;
+            },
+            navTitles: {
+               days: 'yyyy/mm'
+            }
+        };
+		jQuery('#[+id+]').datepicker(options);
 	});
 </script>
