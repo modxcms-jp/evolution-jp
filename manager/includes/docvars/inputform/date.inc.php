a3d1564eb313aaa740dba5f46d31de08fba1e200
yamamoto <yamamoto@kyms.jp>
2016-07-20 00:43:44 +0900

    Fix - DatePicker
    デートピッカータイプのテンプレート変数で動作しなかったので修正

--- manager/includes/docvars/inputform/date.inc.php
+++ manager/includes/docvars/inputform/date.inc.php
@@ -1,30 +1,3 @@
 <input id="[+id+]" name="[+name+]" class="[+class+]" type="text" value="[+value+]" style="[+style+]" onblur="documentDirty=true;" />
 <a onclick="document.forms['mutate'].elements['[+id+]'].value='';document.forms['mutate'].elements['[+id+]'].onblur(); return true;"
    style="cursor:pointer; cursor:hand"><img src="[+cal_nodate+]" border="0" alt="No date"></a>
-<script type="text/javascript">
-	jQuery(function() {
-        var start = new Date();
-        start.setHours(0);
-        start.setMinutes(0);
-        
-        var options = {
-            language      : 'ja',
-            timepicker    : true,
-            todayButton   : new Date(),
-            keyboardNav   : false,
-            startDate     : start,
-            autoClose     : true,
-            toggleSelected: false,
-            clearButton   : true,
-            minutesStep   : 5,
-            dateFormat    : 'yyyy/mm/dd',
-            onSelect      : function (fd, d, picker) {
-                documentDirty = true;
-            },
-            navTitles: {
-               days: 'yyyy/mm'
-            }
-        };
-		jQuery('#[+id+]').datepicker(options);
-	});
-</script>
