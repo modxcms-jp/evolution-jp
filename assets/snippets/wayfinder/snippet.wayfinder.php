0a6529a35a6d2958f74244d73b48b0213b9229d9
yamamoto <yamamoto@kyms.jp>
2016-08-27 15:23:42 +0900

    Wayfinder - Refactor

--- assets/snippets/wayfinder/snippet.wayfinder.php
+++ assets/snippets/wayfinder/snippet.wayfinder.php
@@ -1,48 +1,49 @@
 <?php
+/**
+ * Wayfinder
+ *
+ * Completely template-driven and highly flexible menu builder
+ *
+ * @category 	snippet
+ * @version 	2.1
+ * @license 	http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
+ * @internal	@properties
+ * @internal	@modx_category Navigation
+ * @internal    @installset base, sample
+ * @documentation Official docs https://rtfm.modx.com/extras/evo/wayfinder
+ * @documentation Almost complete guide, Cheatsheet http://sottwell.com/links/wayfinder.html
+ * @documentation Almost complete guide Direct-Link https://drive.google.com/file/d/0B5y4Q9am5QDTOVpIa3Y0VVpjcFU/edit?usp=sharing
+ * @reportissues https://github.com/modxcms/evolution
+ * @author      Kyle Jaebker http://muddydogpaws.com
+ * @author      Ryan Thrash http://thrash.me
+ * @author      and many others since 2006
+ * @lastupdate  23/05/2016
+ */
 if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
-/*
-::::::::::::::::::::::::::::::::::::::::
- Snippet name: Wayfinder
- Short Desc: builds site navigation
- Version: 2.0.7
- Authors: 
-	Kyle Jaebker (muddydogpaws.com)
-	Ryan Thrash (vertexworks.com)
- Date: February 27, 2006
-::::::::::::::::::::::::::::::::::::::::
-Description:
-    Totally refactored from original DropMenu nav builder to make it easier to
-    create custom navigation by using chunks as output templates. By using templates,
-    many of the paramaters are no longer needed for flexible output including tables,
-    unordered- or ordered-lists (ULs or OLs), definition lists (DLs) or in any other
-    format you desire.
-::::::::::::::::::::::::::::::::::::::::
-Example Usage:
-    [[Wayfinder? &startId=`0`]]
-::::::::::::::::::::::::::::::::::::::::
-*/
 
 $wf_base_path = $modx->config['base_path'] . 'assets/snippets/wayfinder/';
-$config_path = "{$wf_base_path}configs/";
+$conf_path = "{$wf_base_path}configs/";
 
 //Include a custom config file if specified
-@include("{$config_path}default.config.php");
+@include("{$conf_path}default.config.php");
 
 $config = (!isset($config)) ? 'default' : trim($config);
+$config = ltrim($config,'/');
 
-if(substr($config, 0, 6) == '@CHUNK')                           eval('?>' . $modx->getChunk(trim(substr($config, 7))));
-elseif(substr($config, 0, 5) == '@FILE')                        include($modx->config['base_path'] . trim(substr($config, 6)));
-elseif(is_file("{$config_path}{$config}.config.php"))           include("{$config_path}{$config}.config.php");
-elseif(is_file("{$config_path}{$config}"))                      include("{$config_path}{$config}");
-elseif(is_file($modx->config['base_path'].ltrim($config, '/'))) include($modx->config['base_path'] . ltrim($config, '/'));
+if(substr($config, 0, 6) == '@CHUNK')               eval('?>' . $modx->getChunk(trim(substr($config, 7))));
+elseif(substr($config, 0, 5) == '@FILE')            include($modx->config['base_path'] . trim(substr($config, 6)));
+elseif($config!='default'&&is_file("{$conf_path}{$config}.config.php"))
+                                                    include("{$conf_path}{$config}.config.php");
+elseif(is_file("{$conf_path}{$config}"))            include("{$conf_path}{$config}");
+elseif(is_file($modx->config['base_path'].$config)) include($modx->config['base_path'] . $config);
 
-include_once($wf_base_path . 'wayfinder.inc.php');
+include_once("{$wf_base_path}wayfinder.inc.php");
 
 if (class_exists('Wayfinder')) $wf = new Wayfinder();
 else                           return 'error: Wayfinder class not found';
 
 $wf->_config = array(
-	'id' => isset($startId) ? $startId : $modx->documentIdentifier,
+	'id' => isset($startId) ? intval($startId) : $modx->documentIdentifier,
 	'level' => isset($level) ? intval($level) : 0,
 	'includeDocs' => isset($includeDocs) ? $includeDocs : 0,
 	'excludeDocs' => isset($excludeDocs) ? $excludeDocs : 0,
@@ -105,11 +106,10 @@ if ($wf->_config['debug']) {
 	$output .= $wf->renderDebugOutput();
 }
 
-//Ouput Results
+//Output Results
 if ($wf->_config['ph']) {
     $modx->setPlaceholder($wf->_config['ph'],$output);
     return;
 } else {
     return $output;
 }
-?>
