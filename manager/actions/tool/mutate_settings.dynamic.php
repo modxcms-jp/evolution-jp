5bd6d3af7dd7f1a3b448634edd31df41c7c42a3b
yamamoto <yamamoto@kyms.jp>
2016-10-03 22:42:56 +0900

    Fix

--- manager/actions/tool/mutate_settings.dynamic.php
+++ manager/actions/tool/mutate_settings.dynamic.php
@@ -657,7 +657,7 @@ if(is_array($evtOut)) echo implode("",$evtOut);
 	<?php echo wrap_label('error',form_radio('send_errormail','3', $send_errormail=='3'));?><br />
 	<?php echo wrap_label('error + warning',form_radio('send_errormail','2', $send_errormail=='2'));?><br />
 	<?php echo wrap_label('error + warning + information',form_radio('send_errormail','1', $send_errormail=='1'));?><br />
-<?php echo $modx->parseTextSimple($_lang['mutate_settings.dynamic.php8'],'emailsender=' . $modx->config['emailsender']);?></td>
+<?php echo $modx->parseTextSimple($_lang['mutate_settings.dynamic.php8'],array('emailsender'=>$modx->config['emailsender']));?></td>
 </tr>
 <?php
 // Check for GD before allowing captcha to be enabled
