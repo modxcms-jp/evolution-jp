18d70e7048999536faf24ecb234b36ef4256b105
yamamoto <yamamoto@kyms.jp>
2016-10-06 21:39:46 +0900

    Fix
    http://forum.modx.jp/viewtopic.php?p=9156#p9156

--- install/connection.databasetest.php
+++ install/connection.databasetest.php
@@ -1,9 +1,4 @@
 <?php
-$host = $_POST['host'];
-$uid  = $_POST['uid'];
-$pwd  = $_POST['pwd'];
-$table_prefix = trim($_POST['table_prefix']);
-
 $self = 'install/connection.databasetest.php';
 $base_path = str_replace($self,'',str_replace('\\','/',__FILE__));
 include_once("{$base_path}manager/includes/document.parser.class.inc.php");
@@ -11,8 +6,8 @@ $modx = new DocumentParser;
 
 require_once("{$base_path}manager/includes/default.config.php");
 require_once("{$base_path}install/functions.php");
-$language = getOption('install_language');
-includeLang($language);
+
+includeLang(getOption('install_language'));
 
 $output = $_lang['status_checking_database'];
 
@@ -20,71 +15,71 @@ $modx->db->hostname = $_SESSION['database_server'];
 $modx->db->username = $_SESSION['database_user'];
 $modx->db->password = $_SESSION['database_password'];
 $modx->db->connect();
-if(!$modx->db->isConnected()) $output .= span_fail($_lang['status_failed']);
-else
-{
-	$dbase                      = trim($_POST['dbase'],'`');
-	$table_prefix = trim($table_prefix,'_').'_';
-	$database_collation         = $_POST['database_collation'];
-	$database_connection_method = $_POST['database_connection_method'];
-	$database_charset = substr($database_collation,0,strpos($database_collation,'_'));
+
+if(!$modx->db->isConnected()) {
+	$bgcolor = '#ffe6eb';
+	$output .= span_fail($bgcolor,$_lang['status_failed']);
+} else {
+	$bgcolor = '#e6ffeb';
+	$db_name              = trim($_POST['dbase'],'`');
+	$table_prefix         = trim($_POST['table_prefix']);
+	if($table_prefix !== '') $table_prefix = rtrim($table_prefix,'_').'_';
+	$db_collation         = trim($_POST['database_collation']);
+	$db_connection_method = trim($_POST['database_connection_method']);
+	$db_charset           = substr($db_collation,0,strpos($db_collation,'_'));
 	
-	if(get_magic_quotes_gpc())
-	{
-		$dbase                      = stripslashes($dbase);
-		$table_prefix               = stripslashes($table_prefix);
-		$database_collation         = stripslashes($database_collation);
-		$database_connection_method = stripslashes($database_connection_method);
-		$database_charset           = stripslashes($database_charset);
+	if(get_magic_quotes_gpc()) {
+		$db_name              = stripslashes($db_name);
+		$table_prefix         = stripslashes($table_prefix);
+		$db_collation         = stripslashes($db_collation);
+		$db_connection_method = stripslashes($db_connection_method);
+		$db_charset           = stripslashes($db_charset);
 	}
-	$dbase                      = $modx->db->escape($dbase);
-	$table_prefix               = $modx->db->escape($table_prefix);
-	$database_collation         = $modx->db->escape($database_collation);
-	$database_connection_method = $modx->db->escape($database_connection_method);
-	$database_charset           = $modx->db->escape($database_charset);
+	$db_name              = $modx->db->escape($db_name);
+	$table_prefix         = $modx->db->escape($table_prefix);
+	$db_collation         = $modx->db->escape($db_collation);
+	$db_connection_method = $modx->db->escape($db_connection_method);
+	$db_charset           = $modx->db->escape($db_charset);
 
 	$pass = false;
-	$rs = $modx->db->select_db($dbase);
-	if (!$rs)
-	{
-		$query = "CREATE DATABASE `{$dbase}` CHARACTER SET '{$database_charset}' COLLATE {$database_collation}";
-		if(!@ $modx->db->query($query)) $output .= span_fail($query.$_lang['status_failed_could_not_create_database']);
-		else
-		{
-			$output .= span_pass($_lang['status_passed_database_created']);
-			$pass = true;
-		}
-	}
-	else {
-		$modx->db->dbname       = $dbase;
+	
+	if ($modx->db->select_db($db_name)) {
+		
+		$modx->db->dbname       = $db_name;
 		$modx->db->table_prefix = $table_prefix;
-		$rs = $modx->db->table_exists('[+prefix+]site_content');
-		if($rs) $rs = $modx->db->select('COUNT(id)','[+prefix+]site_content');
-    	if($rs) {
-    		$output .= span_fail($_lang['status_failed_table_prefix_already_in_use']);
-    	}
+		
+		if($modx->db->table_exists('[+prefix+]site_content') && $modx->db->select('COUNT(id)','[+prefix+]site_content')) {
+			$output .= span_fail($bgcolor,$_lang['status_failed_table_prefix_already_in_use']);
+		}
     	else {
-    		$output .= span_pass($_lang['status_passed']);
+    		$output .= span_pass($bgcolor,$_lang['status_passed']);
     		$pass = true;
     	}
 	}
-	if($pass === true)
-	{
-		$_SESSION['dbase']                      = $dbase;
+	else {
+		$query = "CREATE DATABASE `{$db_name}` CHARACTER SET '{$db_charset}' COLLATE {$db_collation}";
+		if(!@ $modx->db->query($query)) $output .= span_fail($bgcolor,$query.$_lang['status_failed_could_not_create_database']);
+		else {
+			$output .= span_pass($bgcolor,$_lang['status_passed_database_created']);
+			$pass = true;
+		}
+	}
+	if($pass === true) {
+		$_SESSION['dbase']                      = $db_name;
 		$_SESSION['table_prefix']               = $table_prefix;
-		$_SESSION['database_collation']         = $database_collation;
-		$_SESSION['database_connection_method'] = $database_connection_method;
-		$_SESSION['database_charset']           = $database_charset;
+		$_SESSION['database_collation']         = $db_collation;
+		$_SESSION['database_connection_method'] = $db_connection_method;
+		$_SESSION['database_charset']           = $db_charset;
 	}
 }
 
 echo $output;
 
-function span_pass($str) {
-	return '<span id="database_pass" style="color:#388000;">' . $str . '</span>';
+function span_pass($bgcolor,$str) {
+	return sprintf('<span id="database_pass" style="background: %s;padding:8px;border-radius:5px;color:#388000;">%s</span>',$bgcolor,$str);
 }
 
-function span_fail($str) {
-	return '<span id="database_fail" style="color:#FF0000;">' . $str . '</span>';
+function span_fail($bgcolor,$str) {
+	return sprintf('<span id="database_fail" style="background: %s;padding:8px;border-radius:5px;color:#FF0000;">%s</span>',$bgcolor,$str);
 }
 
