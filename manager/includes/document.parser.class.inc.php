4b03bad3fc22c50c5774257da811dbf69f70a287
yamamoto <yamamoto@kyms.jp>
2016-12-06 16:31:56 +0900

    Refactor

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -1903,7 +1903,7 @@ class DocumentParser {
             if(isset($_['token'])) unset($_['token']);
         }
         if(strpos($key,'[')!==false)
-            $value = eval("return isset($key) ? $key : '';");
+            $value = $key ? eval("return {$key};") : '';
         elseif(0<eval("return count({$key});"))
             $value = eval("return print_r({$key},true);");
         else $value = '';
