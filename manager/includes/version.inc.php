442ce3db04d101941efeea38726bd05628d9afbf
yamamoto <yamamoto@kyms.jp>
2016-12-14 15:05:03 +0900

    Version 1.0.18J

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.0.18J-rc1';    // Current version number
-$modx_release_date = '2016/11/24'; // Date of release
+$modx_version      = '1.0.18J';    // Current version number
+$modx_release_date = '2016/12/14'; // Date of release
 $modx_branch       = 'Evolution';  // Codebase name
 $modx_full_appname = "MODX {$modx_branch} {$modx_version} ({$modx_release_date})";
