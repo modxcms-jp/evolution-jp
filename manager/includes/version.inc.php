667a2091d5d644f0db4a9d736b051b46ea35a176
yamamoto <yamamoto@kyms.jp>
2017-01-26 09:15:46 +0900

    version 1.0.19J

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 $modx_version      = '1.0.19J';    // Current version number
-$modx_release_date = '2017/01/11'; // Date of release
+$modx_release_date = '2017/01/26'; // Date of release
 $modx_branch       = 'Evolution';  // Codebase name
 $modx_full_appname = "MODX {$modx_branch} {$modx_version} ({$modx_release_date})";
