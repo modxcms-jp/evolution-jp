2507612611be05e78e3d703436b021569c2942b4
yamamoto <yamamoto@kyms.jp>
2017-04-09 15:44:31 +0900

    Improved - summary modifier
    精度を改善

--- manager/includes/extenders/modifiers/mdf_summary.inc.php
+++ manager/includes/extenders/modifiers/mdf_summary.inc.php
@@ -3,41 +3,32 @@ if(strpos($opt,',')) list($limit,$delim) = explode(',', $opt);
 elseif(preg_match('/^[1-9][0-9]*$/',$opt)) {$limit=$opt;$delim='';}
 else {$limit=124;$delim='';}
 
-if($delim==='') $delim = $modx->config['manager_language']==='japanese-utf8' ? '。' : '.';
 $limit = intval($limit);
 
 $content = $modx->filter->parseDocumentSource($value);
 
 $content = strip_tags($content);
+$content = str_replace('&nbsp;', ' ', $content);
+$content = str_replace('　', ' ', $content);
+$content = str_replace( "\xc2\xa0", " ", $content );
+$content = str_replace(array("\r\n","\r","\n","\t",' '),' ',$content);
+$content = str_replace(array('。 ','、 ',' ・','。・','…'),array('。','、','・','。','・・'),$content);
+$content = preg_replace('@\s+@',' ',$content);
 
-$content = str_replace(array("\r\n","\r","\n","\t",'&nbsp;'),' ',$content);
-if(preg_match('/\s+/',$content))
-    $content = preg_replace('/\s+/',' ',$content);
-$content = trim($content);
+$strlen = $modx->filter->strlen($content);
+$limit = $strlen<$limit ? $strlen : $limit;
+$p = strpos($content,'。')!==false ? '[。！？]+' : '[\.\!\?\s]+';
+if(!preg_match('@('.$p.')@u', $content)) return $modx->filter->substr($content, 0, $limit);
 
-$pos = $modx->filter->strpos($content, $delim);
+$_ = preg_split('@('.$p.')@u', $content, -1, PREG_SPLIT_DELIM_CAPTURE);
+$content='';
+foreach($_ as $i=>$v) {
+    $isToken = preg_match('@('.$p.')@u', $v) ? 1 : 0;
 
-if($pos!==false && $pos<$limit) {
-    $_ = explode($delim, $content);
-    $text = '';
-    foreach($_ as $v) {
-        if($limit <= $modx->filter->strlen($text.$v.$delim)) break;
-        $text .= $v.$delim;
-    }
-    if($text) $content = $text;
-}
-
-if($limit<$modx->filter->strlen($content) && strpos($content,' ')!==false) {
-    $_ = explode(' ', $content);
-    $text = '';
-    foreach($_ as $v) {
-        if($limit <= $modx->filter->strlen($text.$v.' ')) break;
-        $text .= $v . ' ';
-    }
-    if($text!=='') $content = $text;
+    if($modx->filter->strlen($content.$v)<$limit || $isToken) $content .= $v;
+    else                                       break;
 }
 
 if($limit < $modx->filter->strlen($content)) $content = $modx->filter->substr($content, 0, $limit);
-if($modx->filter->substr($content,-1)==$delim) $content = rtrim($content,$delim) . $delim;
 
 return $content;
