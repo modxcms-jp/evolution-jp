1ee94ff89fb936aed3c6b2b92101ba718f5a84fc
yama <yamamoto@kyms.jp>
2019-01-27 23:01:31 +0900

    Refactor

--- assets/snippets/ditto/classes/filter.class.inc.php
+++ assets/snippets/ditto/classes/filter.class.inc.php
@@ -1,196 +1,126 @@
 <?php
 
 /*
- * Title: Filter Class
- * Purpose:
- *  	The Filter class contains all functions relating to filtering,
- * 		the removing of documents from the result set
+ * The Filter class contains all functions relating to filtering,
+ * the removing of documents from the result set
 */
 
 class filter {
-	var $array_key, $filtertype, $filterValue, $flip_mode, $filterArgs;
-
-// ---------------------------------------------------
-// Function: execute
-// Filter documents via either a custom filter or basic filter
-// ---------------------------------------------------
-	public function execute($doc, $filter)
-	{
-		global $modx;
-		foreach ($filter['basic'] as $currentFilter)
-		{
-			$this->flip_mode = 0;
-
-			if (!is_array($currentFilter) || !$currentFilter) {
+	public function execute($docs, $filter) {
+		foreach ($filter['basic'] as $current_filter) {
+			if (!is_array($current_filter) || !$current_filter) {
 				continue;
 			}
-
-			$this->array_key = $currentFilter['source'];
-			
-			if (substr($currentFilter['mode'],0,1)==='!' && substr($currentFilter['mode'],0,2)!=='!!') {
-				$this->flip_mode = 1;
-				$currentFilter['mode'] = substr($currentFilter['mode'],1);
-			}
-			
-			switch($currentFilter['value'])
-			{
-				case '>':
-				case '>=':
-				case '<':
-				case '<=':
-				case '!=':
-				case '<>':
-				case '==':
-				case '=~':
-				case '!~':
-					$t = $currentFilter['value'];
-					$currentFilter['value'] = $currentFilter['mode'];
-					$currentFilter['mode'] = $t;
-					unset($t);
-					break;
-			}
-			
-			if(substr($currentFilter['value'],0,5) === '@EVAL')
-			{
-				$eval_code = trim(substr($currentFilter['value'],6));
-				$eval_code = trim($eval_code,';') . ';';
-				if(strpos($eval_code,'return')===false)
-				{
-					$eval_code = 'return ' . $eval_code;
-				}
-				$this->filterValue = eval($eval_code);
-			}
-			else
-			{
-				$this->filterValue = $currentFilter['value'];
-			}
-			if(strpos($this->filterValue,'[+') !== false)
-			{
-				$this->filterValue = $modx->mergePlaceholderContent($this->filterValue);
-			}
-			if (!isset($currentFilter['mode'])) {
-				$currentFilter['mode'] = '!=';
-			}
-			$this->filtertype = $this->get_operator_name($currentFilter['mode']);
-
-			$this->filterValue = trim($this->filterValue);
-			if ($modx->get_docfield_type($this->array_key)==='datetime') {
-				if (!preg_match('@^[0-9]+$@',$this->filterValue)) {
-					$this->filterValue = strtotime($this->filterValue);
-				}
-			}
-
-			$doc = array_filter($doc, array($this, 'basicFilter'));
+			$param = $this->_getParams($current_filter);
+			$docs = $this->_basicFilter($docs, $param);
 		}
-
-		foreach ($filter['custom'] as $currentFilter)
-		{
-			$doc = array_filter($doc, $currentFilter);
+		foreach ($filter['custom'] as $current_filter) {
+			$docs = array_filter($docs, $current_filter);
 		}
-
-		return $doc;
+		return $docs;
 	}
-	
-// ---------------------------------------------------
-// Function: basicFilter
-// Do basic comparison filtering
-// ---------------------------------------------------
-	
-	private function basicFilter ($doc) {
+
+	private function _basicFilter($docs, $param) {
+		foreach($docs as $i=>$doc) {
+			$unset = $this->isTrue($doc, $param);
+			if($param['flip_mode']) {
+				$unset = $unset ? 0 : 1;
+			}
+			if($unset) {
+				unset($docs[$i]);
+			}
+		}
+		return $docs;
+	}
+
+	private function _getParams($param) {
 		global $modx;
 
-		$unset = 1;
+		$rs = array();
+		$rs['op'] = isset($param['mode']) ? $param['mode'] : '!=';
 
-		switch ($this->filtertype) {
-			case '!=' :
-				if (!isset ($doc[$this->array_key]))
-					$unset = 0;
-				elseif($doc[$this->array_key] != $this->filterValue)
-					$unset = 0;
-				break;
-			case '==' :
-				if ($doc[$this->array_key] == $this->filterValue)
-					$unset = 0;
-				break;
-			case '<' :
-				if ($doc[$this->array_key] < $this->filterValue)
-					$unset = 0;
-				break;
-			case '>' :
-				if ($doc[$this->array_key] > $this->filterValue)
-					$unset = 0;
-				break;
-			case '>=' :
-				if ($doc[$this->array_key] >= $this->filterValue)
-					$unset = 0;
-				break;
-			case '<=' :
-				if ($doc[$this->array_key] <= $this->filterValue)
-					$unset = 0;
-				break;
-				
-			// Cases 7 & 8 created by MODx Testing Team Member ZAP
-			case '=~':
-				if (strpos($doc[$this->array_key], $this->filterValue)===false)
-					$unset = 0;
-				break;
-			case '!~':
-				if (strpos($doc[$this->array_key], $this->filterValue)!==false)
-					$unset = 0;
-				break;
-			
-			// Cases 9-11 created by highlander
-			// case insenstive version of #7 - exclude records that do not contain the text of the criterion
-			case 9 :
-				if (stripos($doc[$this->array_key], $this->filterValue)===false)
-					$unset = 0;
-				break;
-			case 10 : // case insenstive version of #8 - exclude records that do contain the text of the criterion
-				if (stripos($doc[$this->array_key], $this->filterValue)!==false)
-					$unset = 0;
-				break;
-			case 11 : // checks leading character of the field
-				$firstChr = strtoupper(substr($doc[$this->array_key], 0, 1));
-				if ($firstChr!=$this->filterValue)
-					$unset = 0;
-				break;
-			case 'regex':
-				if (preg_match($doc[$this->array_key], $this->filterValue)===false)
-					$unset = 0;
-				break;
+		if (in_array($param['value'], array('>','>=','<','<=','!=','<>','==','=~','!~') )) {
+			list($param['value'], $rs['op']) = array($rs['op'], $param['value']);
 		}
-		if($this->flip_mode) $unset = $unset ? 0 : 1;
-		return $unset;
+		
+		if(substr($param['value'],0,5) === '@EVAL') {
+			$eval_code = trim(substr($param['value'],6));
+			$eval_code = trim($eval_code,';') . ';';
+			if(strpos($eval_code,'return')===false) {
+				$eval_code = 'return ' . $eval_code;
+			}
+			$rs['creteria'] = eval($eval_code);
+		} else {
+			$rs['creteria'] = $param['value'];
+		}
+
+		if(strpos($rs['creteria'],'[+') !== false) {
+			$rs['creteria'] = $modx->mergePlaceholderContent($rs['creteria']);
+		}
+
+		$rs['op'] = $this->_get_operator_name($rs['op']);
+
+		$rs['creteria'] = trim($rs['creteria']);
+
+		if ($this->get_docfield_type($param['source'])==='datetime') {
+			if (!preg_match('@^[0-9]+$@',$rs['creteria'])) {
+				$rs['creteria'] = strtotime($rs['creteria']);
+			}
+		}
+
+		$rs['field_name'] = $param['source'];
+
+		if (substr($rs['op'],0,1)==='!' && substr($rs['op'],0,2)!=='!!') {
+			$rs['flip_mode'] = 1;
+			$rs['op'] = substr($rs['op'],1);
+		} else {
+			$rs['flip_mode'] = 0;
+		}
+
+		return $rs;
+	}
+	
+	private function isTrue($doc, $param) {
+		$field_name = $param['field_name'];
+		$doc_value = isset($doc[$field_name]) ? $doc[$field_name] : false;
+		$creteria = $param['creteria'];
+
+		switch ($param['op']) {
+			case '!=' : return ($doc_value != $creteria || $doc_value===false);
+			case '==' : return ($doc_value == $creteria);
+			case '<'  : return ($doc_value <  $creteria);
+			case '>'  : return ($doc_value >  $creteria);
+			case '>=' : return ($doc_value >= $creteria);
+			case '<=' : return ($doc_value <= $creteria);
+			case '=~' : return (strpos($doc_value, $creteria)===false);
+			case '!~' : return (strpos($doc_value, $creteria)!==false);
+			case 9    : return (stripos($doc_value, $creteria)===false);
+			case 10   : return (stripos($doc_value, $creteria)!==false);
+			case 'regex': return (preg_match($doc_value, $creteria)===false);
+			case 11   :
+				$firstChr = strtoupper(substr($doc_value, 0, 1));
+				return ($firstChr!=$creteria);
+		}
+		return false;
 	}
 
-	private function get_operator_name($operator_name) {
-		if (in_array($operator_name, array(1,'<>','ne'))) {
-			return '!=';
-		}
-		if (in_array($operator_name, array(2,'eq','ne'))) {
-			return '==';
-		}
-		if (in_array($operator_name, array(3,'lt','ne'))) {
-			return '<';
-		}
-		if (in_array($operator_name, array(4,'gt'))) {
-			return '>';
-		}
-		if (in_array($operator_name, array(5,'gte','ge'))) {
-			return '>=';
-		}
-		if (in_array($operator_name, array(6,'lte','le'))) {
-			return '<=';
-		}
-		if (in_array($operator_name, array(7,'find','search','strpos'))) {
-			return '=~';
-		}
-		if ($operator_name==8) {
-			return '!~';
-		}
-		if ($operator_name=='preg') {
-			return 'regex';
-		}
+	private function _get_operator_name($operator_name) {
+		if (in_array($operator_name, array(1,'<>','ne')))  return '!=';
+		if (in_array($operator_name, array(2,'eq','ne')))  return '==';
+		if (in_array($operator_name, array(3,'lt','ne')))  return '<';
+		if (in_array($operator_name, array(6,'lte','le'))) return '<=';
+		if (in_array($operator_name, array(4,'gt')))       return '>';
+		if (in_array($operator_name, array(5,'gte','ge'))) return '>=';
+		if ($operator_name==8)                             return '!~';
+		if ($operator_name=='preg')                        return 'regex';
+		if (in_array($operator_name, array(7,'find','search','strpos'))) return '=~';
 		return $operator_name;
 	}
+
+    private function get_docfield_type($field_name='') {
+        if(in_array($field_name, explode(',','published,pub_date,unpub_date,createdon,editedon,publishedon,deletedon'))) {
+            return 'datetime';
+        };
+        return false;
+    }
 }
