deb4187861552386d6c5ac8765647777db7d23e8
yama <yamamoto@kyms.jp>
2019-04-01 09:11:10 +0900

    Minor fix - topicPath

--- assets/snippets/topicpath/topicpath.class.inc.php
+++ assets/snippets/topicpath/topicpath.class.inc.php
@@ -56,7 +56,7 @@ class TopicPath
 				$tpl['Separator']       = "\n";
 				break;
 			case 'bootstrap':
-				$tpl['Outer']           = '<ol class="breadcrumb">[+topics+]</ol>';
+				$tpl['Outer']           = '<ol class="topicpath">[+topics+]</ol>';
 				$tpl['HomeTopic']       = '<li class="home"><a href="[+url+]">[+title+]</a></li>';
 				$tpl['CurrentTopic']    = '<li class="active">[+title+]</li>';
 				$tpl['ReferenceTopic']  = '<li>[+title+]</li>';
