27d4804f211d235dcea0b42cd2ac0d460b241c64
yama <yamamoto@kyms.jp>
2019-05-03 08:18:36 +0900

    Replace __DIR__

--- manager/includes/initialize.inc.php
+++ manager/includes/initialize.inc.php
@@ -14,8 +14,7 @@ $init->fix_ssl();
 if (!defined('MODX_BASE_PATH')) define('MODX_BASE_PATH', $init->get_base_path());
 if (!defined('MODX_BASE_URL'))  define('MODX_BASE_URL', $init->get_base_url(MODX_BASE_PATH));
 if (!defined('MODX_SITE_URL'))  define('MODX_SITE_URL', $init->get_site_url(MODX_BASE_URL));
-if (!defined('MODX_CORE_PATH')) define('MODX_CORE_PATH', str_replace('\\','/',dirname(__FILE__)).'/');
-
+if (!defined('MODX_CORE_PATH')) define('MODX_CORE_PATH', str_replace('\\','/', __DIR__).'/');
 
 if (!defined('MODX_MANAGER_PATH')) define('MODX_MANAGER_PATH', MODX_BASE_PATH.'manager/');
 if (!defined('MODX_MANAGER_URL'))  define('MODX_MANAGER_URL', MODX_SITE_URL.'manager/');
