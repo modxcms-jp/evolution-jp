31e894a6aab420f3ab8b8074d00d156f085be0eb
yama <yamamoto@kyms.jp>
2019-05-06 09:40:29 +0900

    Code cleanup $modx->outputContent()

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -627,9 +627,12 @@ class DocumentParser {
 
         $this->documentOutput = $this->cleanUpMODXTags($this->documentOutput);
         
-        if(strpos($this->documentOutput,'[~')!==false) $this->documentOutput = $this->rewriteUrls($this->documentOutput);
-        if(strpos($this->documentOutput,'<!---->')!==false)
-            $this->documentOutput = str_replace('<!---->','',$this->documentOutput);
+        if(strpos($this->documentOutput,'[~')!==false) {
+            $this->documentOutput = $this->rewriteUrls($this->documentOutput);
+        }
+        if(strpos($this->documentOutput,'<!---->')!==false) {
+            $this->documentOutput = str_replace('<!---->', '', $this->documentOutput);
+        }
         
         // send out content-type and content-disposition headers
         if (IN_PARSER_MODE == 'true')
