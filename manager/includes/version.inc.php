d342ca218146bf16e8233a91cc50ad3a70af8fa0
yama <yamamoto@kyms.jp>
2019-05-07 22:57:22 +0900

    1.0.22J-rc4

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.0.22J-rc2';    // Current version number
-$modx_release_date = '2019/05/04'; // Date of release
+$modx_version      = '1.0.22J-rc4';    // Current version number
+$modx_release_date = '2019/05/07'; // Date of release
 $modx_branch       = 'Evolution';  // Codebase name
 $modx_full_appname = "MODX {$modx_branch} {$modx_version} ({$modx_release_date})";
