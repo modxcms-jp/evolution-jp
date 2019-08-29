99ebfa7672b02670c72bf9a9c5bd55d6947908db
yama <yamamoto@kyms.jp>
2019-08-29 23:41:09 +0900

    Refactor mutate_content.functions.inc.php

--- manager/actions/document/mutate_content.functions.inc.php
+++ manager/actions/document/mutate_content.functions.inc.php
@@ -14,7 +14,7 @@ function fieldPagetitle() {
 
 function fieldLongtitle() {
     return renderTr(
-        lang('longtitle')
+        lang('long_title')
         , input_text_tag(
             array(
                 'name'=>'longtitle',
@@ -1566,10 +1566,6 @@ function getActionButtons($id) {
     return preg_replace('@\[\+[^]]+\+]@', '', parseText(file_get_tpl('action_buttons.tpl'),$ph));
 }
 
-function config($key, $default=null) {
-    return evo()->conf_var($key, $default);
-}
-
 function doc($key) {
     global $docObject;
     if(str_contains($key,'|hsc')) {
@@ -1583,82 +1579,6 @@ function doc($key) {
     return evo()->array_get($docObject, $key);
 }
 
-function lang($key) {
-    global $_lang;
-    return $_lang[$key];
-}
-
-function style($key) {
-    global $_style;
-    return $_style[$key];
-}
-
-if (!function_exists('str_contains')) {
-    function str_contains($str,$needle) {
-        return strpos($str,$needle)!==false;
-    }
-}
-
-function hsc($string) {
-    return evo()->hsc($string);
-}
-
-function parseText($tpl,$ph) {
-    foreach($ph as $k=>$v) {
-        $k = "[+{$k}+]";
-        $tpl = str_replace($k,$v,$tpl);
-    }
-    return $tpl;
-}
-
-function evo() {
-    global $modx;
-    return $modx;
-}
-
-function db() {
-    return evo()->db;
-}
-
-function html_tag($tag_name, $attrib=array(), $content=null) {
-    return evo()->html_tag($tag_name, $attrib, $content);
-}
-
-function input_text_tag($props=array()) {
-    $props['type'] = 'text';
-    $props['maxlength'] = evo()->array_get($props,'maxlength',255);
-    $props['class']     = evo()->array_get($props,'class','inputBox');
-    foreach($props as $k=>$v) {
-        if($v===false) {
-            unset($props[$k]);
-        }
-    }
-    return html_tag('input', $props);
-
-}
-
-function textarea_tag($props=array(), $content) {
-    $props['class'] = evo()->array_get($props,'class','inputBox');
-    return html_tag('textarea', $props, $content);
-}
-
-function select_tag($props=array(), $options) {
-    $props['class'] = evo()->array_get($props,'class','inputBox');
-    if(is_array($options)) {
-        $options = implode("\n", $options);
-    }
-    return html_tag('select', $props, $options);
-}
-
-function img_tag($src,$props=array()) {
-    $props['src'] = $src;
-    return html_tag('img', $props);
-}
-
-function alert() {
-    global $e;
-    return $e;
-}
 
 function file_get_tpl ($path) {
     if(is_file(MODX_BASE_PATH . config('custom_tpl_dir') . $path)) {
