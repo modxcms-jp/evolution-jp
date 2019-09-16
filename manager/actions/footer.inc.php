9be9399804becab4f34038553583864cfc1fc697
yama <yamamoto@kyms.jp>
2019-09-17 08:48:50 +0900

    Fix for PHP7

--- manager/actions/footer.inc.php
+++ manager/actions/footer.inc.php
@@ -1,7 +1,7 @@
 <?php
 if(!isset($modx) || !$modx->isLoggedin()) exit;
 // display system alert window if messages are available
-if (count($modx->SystemAlertMsgQueque)>0) {
+if (is_array($modx->SystemAlertMsgQueque) && count($modx->SystemAlertMsgQueque)>0) {
 	echo $modx->manager->sysAlert($modx->SystemAlertMsgQueque);
 }
 if(in_array($modx->manager->action,array(85,27,4,72,131,132,133,74,13,11,12,77,78,87,88)))
