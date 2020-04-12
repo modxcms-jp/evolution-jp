b32cce87f2cbbc23c0514ded6b0e6b5a1dce43bf
yama <yamamoto@kyms.jp>
2020-04-12 15:03:40 +0900

    Fix ユーザ管理画面

--- manager/processors/permission/save_user.processor.php
+++ manager/processors/permission/save_user.processor.php
@@ -64,7 +64,7 @@ if (in_array(post('mode'), array('12','74'))) {
     }
 
     // check if the email address already exists
-    if (userid_byemail(post('email')) != post('userid')) {
+    if (userid_byemail(post('email'))) {
         webAlert('Email is already in use!');
         exit;
     }
