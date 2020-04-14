ae8282fbc9f92fa015bb1c86c7ee89045fe1db88
yama <yamamoto@kyms.jp>
2020-04-14 22:31:03 +0900

    Refactor - manager()->getUploadMaxsize()

--- manager/includes/extenders/ex_managerapi.php
+++ manager/includes/extenders/ex_managerapi.php
@@ -880,19 +880,11 @@ class ManagerAPI {
     }
 
     function getUploadMaxsize() {
-        $upload_max_filesize = ini_get('upload_max_filesize');
-        $post_max_size       = ini_get('post_max_size');
-        $memory_limit        = ini_get('memory_limit');
-        if(version_compare($upload_max_filesize, $post_max_size,'<')) {
-            $limit_size = $upload_max_filesize;
-        } else {
-            $limit_size = $post_max_size;
-        }
-
-        if(version_compare($memory_limit, $limit_size,'<')) {
-            $limit_size = $memory_limit;
-        }
-        return $limit_size;
+        return max(
+            ini_get('upload_max_filesize')
+            , ini_get('post_max_size')
+            , ini_get('memory_limit')
+        );
     }
 
     function getTplModule() {
