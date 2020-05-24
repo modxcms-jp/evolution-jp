51b35e8e4877b4e57b96f62b1b1b5e0795a5ec6f
yama <yamamoto@kyms.jp>
2020-05-25 08:14:52 +0900

    Refactor tab_profile.php

--- manager/actions/permission/mutate_user/tpl/form/tab_profile.php
+++ manager/actions/permission/mutate_user/tpl/form/tab_profile.php
@@ -2,42 +2,42 @@
 <table class="settings">
     <tr>
         <th><?php echo lang('user_full_name'); ?>:</th>
-        <td><input type="text" name="fullname" class="inputBox" value="<?php echo htmlspecialchars($user['fullname']); ?>" /></td>
+        <td><input type="text" name="fullname" class="inputBox" value="<?php echo hsc(user('fullname','')); ?>" /></td>
     </tr>
     <tr>
         <th><?php echo lang('user_phone'); ?>:</th>
-        <td><input type="text" name="phone" class="inputBox" value="<?php echo htmlspecialchars($user['phone']); ?>" /></td>
+        <td><input type="text" name="phone" class="inputBox" value="<?php echo hsc(user('phone')); ?>" /></td>
     </tr>
     <tr>
         <th><?php echo lang('user_mobile'); ?>:</th>
-        <td><input type="text" name="mobilephone" class="inputBox" value="<?php echo htmlspecialchars($user['mobilephone']); ?>" /></td>
+        <td><input type="text" name="mobilephone" class="inputBox" value="<?php echo hsc(user('mobilephone')); ?>" /></td>
     </tr>
     <tr>
         <th><?php echo lang('user_fax'); ?>:</th>
-        <td><input type="text" name="fax" class="inputBox" value="<?php echo htmlspecialchars($user['fax']); ?>" /></td>
+        <td><input type="text" name="fax" class="inputBox" value="<?php echo hsc(user('fax')); ?>" /></td>
     </tr>
     <tr>
         <th><?php echo lang('user_street'); ?>:</th>
-        <td><input type="text" name="street" class="inputBox" value="<?php echo htmlspecialchars($user['street']); ?>" onchange="documentDirty=true;" /></td>
+        <td><input type="text" name="street" class="inputBox" value="<?php echo hsc(user('street')); ?>" onchange="documentDirty=true;" /></td>
     </tr>
     <tr>
         <th><?php echo lang('user_city'); ?>:</th>
-        <td><input type="text" name="city" class="inputBox" value="<?php echo htmlspecialchars($user['city']); ?>" onchange="documentDirty=true;" /></td>
+        <td><input type="text" name="city" class="inputBox" value="<?php echo hsc(user('city')); ?>" onchange="documentDirty=true;" /></td>
     </tr>
 
     <tr>
         <th><?php echo lang('user_state'); ?>:</th>
-        <td><input type="text" name="state" class="inputBox" value="<?php echo htmlspecialchars($user['state']); ?>" /></td>
+        <td><input type="text" name="state" class="inputBox" value="<?php echo hsc(user('state')); ?>" /></td>
     </tr>
     <tr>
         <th><?php echo lang('user_zip'); ?>:</th>
-        <td><input type="text" name="zip" class="inputBox" value="<?php echo htmlspecialchars($user['zip']); ?>" /></td>
+        <td><input type="text" name="zip" class="inputBox" value="<?php echo hsc(user('zip')); ?>" /></td>
     </tr>
     <tr>
         <th><?php echo lang('user_country'); ?>:</th>
         <td>
             <select size="1" name="country" class="inputBox">
-                <?php $chosenCountry = isset($_POST['country']) ? $_POST['country'] : $user['country']; ?>
+                <?php $chosenCountry = postv('country', user('country')); ?>
                 <option value="" <?php echo selected(empty($chosenCountry)); ?> >&nbsp;</option>
                 <?php
                 foreach ($_country_lang as $key => $country)
@@ -51,39 +51,42 @@
     <tr>
         <th><?php echo lang('user_dob'); ?>:</th>
         <td>
-            <input type="text" id="dob" name="dob" class="DatePicker" value="<?php echo ($user['dob'] ? $modx->toDateFormat($user['dob'],'dateOnly'):""); ?>" onblur="documentDirty=true;">
+            <input type="text" id="dob" name="dob" class="DatePicker" value="<?php echo (user('dob') ? evo()->toDateFormat(user('dob'),'dateOnly'):'0'); ?>" onblur="documentDirty=true;">
             <a onclick="document.userform.dob.value=''; return true;" style="cursor:pointer; cursor:hand"><img align="absmiddle" src="media/style/<?php echo $modx->config['manager_theme']; ?>/images/icons/cal_nodate.gif"  border="0" alt="<?php echo lang('remove_date'); ?>"></a>
         </td>
     </tr>
     <tr>
         <th><?php echo lang('user_gender'); ?>:</th>
         <td><select name="gender" class="inputBox">
-                <option value=""></option>
-                <option value="1" <?php echo selected($user['gender']=='1'); ?>><?php echo lang('user_male'); ?></option>
-                <option value="2" <?php echo selected($user['gender']=='2'); ?>><?php echo lang('user_female'); ?></option>
-                <option value="3" <?php echo selected($user['gender']=='3'); ?>><?php echo lang('user_other'); ?></option>
+                <option value="0"></option>
+                <option value="1" <?php echo selected(user('gender')==1); ?>><?php echo lang('user_male'); ?></option>
+                <option value="2" <?php echo selected(user('gender')==2); ?>><?php echo lang('user_female'); ?></option>
+                <option value="3" <?php echo selected(user('gender')==3); ?>><?php echo lang('user_other'); ?></option>
             </select>
         </td>
     </tr>
     <tr>
         <th valign="top"><?php echo lang('comment'); ?>:</th>
         <td>
-            <textarea type="text" name="comment" class="inputBox"  rows="5"><?php echo htmlspecialchars($user['comment']); ?></textarea>
+            <textarea type="text" name="comment" class="inputBox"  rows="5"><?php echo hsc(user('comment')); ?></textarea>
         </td>
     </tr>
     <tr>
         <th><?php echo lang('user_photo') ?></th>
-        <td><input type="text" maxlength="255" style="width: 150px;" name="photo" value="<?php echo htmlspecialchars($user['photo']); ?>" />
+        <td><input type="text" maxlength="255" style="width: 150px;" name="photo" value="<?php echo hsc(user('photo')); ?>" />
             <input type="button" value="<?php echo lang('insert'); ?>" onclick="BrowseServer();" />
             <div><?php echo lang('user_photo_message'); ?></div>
             <div>
                 <?php
-                if(isset($_POST['photo']))         $photo = $_POST['photo'];
-                elseif(!empty($user['photo'])) $photo = $user['photo'];
-                else                               $photo = $modx->config['base_url'] . 'manager/' . style('tx');
+                if(isset($_POST['photo'])) {
+                    $photo = $_POST['photo'];
+                } elseif(user('photo')) {
+                    $photo = user('photo');
+                } else {
+                    $photo = $modx->config['base_url'] . 'manager/' . style('tx');
+                }
 
-                if(substr($photo,0,1)!=='/' && !preg_match('@^https?://@',$photo))
-                {
+                if(strpos($photo, '/') !== 0 && !preg_match('@^https?://@',$photo)) {
                     $photo = $modx->config['base_url'] . $photo;
                 }
                 ?>
