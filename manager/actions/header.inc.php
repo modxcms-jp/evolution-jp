f5aca6994d409e9685be3eeb65b4db6c368ea43b
yama <yamamoto@kyms.jp>
2020-06-06 07:54:15 +0900

    Code cleanup header.inc.php

--- manager/actions/header.inc.php
+++ manager/actions/header.inc.php
@@ -1,58 +1,68 @@
 <?php
 global $modx;
-if(!isset($modx) || !$modx->isLoggedin()) exit;
+if (!isset($modx) || !$modx->isLoggedin()) {
+    exit;
+}
 global $_lang, $_style, $modx_textdir, $modx_lang_attribute;
 global $manager_theme, $modx_charset;
-global $manager_language,$modx_version;
+global $manager_language, $modx_version;
 
-if($modx->config['remember_last_tab']!=='2')
-{
-	if(70300 <= PHP_VERSION_ID) {
-		setcookie(
-			'webfxtab_childPane'
-			, getv('tab', 1)
-			, [
-				'expires' => time()+3600,
-				'path' => MODX_BASE_URL,
-				'secure' => true,
-				'httponly' => true,
-				'samesite' => 'Lax',
-			]
-		);
-	} else {
-		setcookie(
-			'webfxtab_childPane'
-			, getv('tab', 1)
-			, time()+3600
-			, MODX_BASE_URL . '; SameSite=Lax'
-			, ''
-			, init::is_ssl() ? true : false
-			, true
-		);
-	}
+if ($modx->config['remember_last_tab'] !== '2') {
+    if (70300 <= PHP_VERSION_ID) {
+        setcookie(
+            'webfxtab_childPane'
+            , getv('tab', 1)
+            , [
+                'expires' => time() + 3600,
+                'path' => MODX_BASE_URL,
+                'secure' => true,
+                'httponly' => true,
+                'samesite' => 'Lax',
+            ]
+        );
+    } else {
+        setcookie(
+            'webfxtab_childPane'
+            , getv('tab', 1)
+            , time() + 3600
+            , MODX_BASE_URL . '; SameSite=Lax'
+            , ''
+            , init::is_ssl() ? true : false
+            , true
+        );
+    }
 }
 $mxla = $modx_lang_attribute ? $modx_lang_attribute : 'en';
 
 $bodyid = (isset($_GET['f'])) ? $_GET['f'] : 'mainpane';
-$textdir = $modx_textdir==='rtl' ? 'rtl' : 'ltr';
+$textdir = $modx_textdir === 'rtl' ? 'rtl' : 'ltr';
 
 // invoke OnManagerRegClientStartupHTMLBlock event
 $evtOut = $modx->invokeEvent('OnManagerMainFrameHeaderHTMLBlock');
-if(!isset($modx->config['tree_pane_open_default'])) $modx->config['tree_pane_open_default'] = 1;
-if(!isset($modx->config['mgr_jquery_path']))  $modx->config['mgr_jquery_path'] = 'media/script/jquery/jquery.min.js';
-if(!isset($modx->config['mgr_date_picker_path']))   $modx->config['mgr_date_picker_path']   = 'media/script/air-datepicker/datepicker.inc.php';
+if (!isset($modx->config['tree_pane_open_default'])) {
+    $modx->config['tree_pane_open_default'] = 1;
+}
+if (!isset($modx->config['mgr_jquery_path'])) {
+    $modx->config['mgr_jquery_path'] = 'media/script/jquery/jquery.min.js';
+}
+if (!isset($modx->config['mgr_date_picker_path'])) {
+    $modx->config['mgr_date_picker_path'] = 'media/script/air-datepicker/datepicker.inc.php';
+}
 
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo  $mxla;?>" dir="<?php echo  $textdir;?>">
+<html lang="<?php echo $mxla; ?>" dir="<?php echo $textdir; ?>">
 <head>
     <title>MODX</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $modx->config['modx_charset']; ?>" />
-    <link rel="stylesheet" type="text/css" href="media/style/<?php echo $modx->config['manager_theme']; ?>/style.css?<?php echo $modx_version;?>" />
-    <link rel="stylesheet" type="text/css" href="media/script/jquery/jquery.powertip.css" />
-    <link rel="stylesheet" href="media/script/jquery/jquery.alerts.css" type="text/css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $modx->config['modx_charset']; ?>"/>
+    <link rel="stylesheet" type="text/css"
+          href="media/style/<?php echo $modx->config['manager_theme']; ?>/style.css?<?php echo $modx_version; ?>"/>
+    <link rel="stylesheet" type="text/css" href="media/script/jquery/jquery.powertip.css"/>
+    <link rel="stylesheet" href="media/script/jquery/jquery.alerts.css" type="text/css"/>
     <!-- OnManagerMainFrameHeaderHTMLBlock -->
-    <?php if(is_array($evtOut)) echo implode("\n", $evtOut); ?>
+    <?php if (is_array($evtOut)) {
+        echo implode("\n", $evtOut);
+    } ?>
     <?php echo $modx->config['manager_inline_style']; ?>
     <?php echo sprintf('<script src="%s" type="text/javascript"></script>', $modx->config['mgr_jquery_path']); ?>
     <script src="media/script/jquery/jquery.powertip.min.js" type="text/javascript"></script>
@@ -60,85 +70,87 @@ if(!isset($modx->config['mgr_date_picker_path']))   $modx->config['mgr_date_pick
     <script src="media/script/mootools/mootools.js" type="text/javascript"></script>
     <script type="text/javascript" src="media/script/tabpane.js"></script>
     <script type="text/javascript">
-		var treeopen = <?php echo $modx->config['tree_pane_open_default'];?>;
-		if(treeopen==0 && top.mainMenu) top.mainMenu.hideTreeFrame();
-		
-		var documentDirty=false;
-		var dontShowWorker = false;
-		var baseurl = '<?php echo MODX_BASE_URL; ?>';
-		var $j = jQuery.noConflict();
-		
+        var treeopen = <?php echo $modx->config['tree_pane_open_default'];?>;
+        if (treeopen == 0 && top.mainMenu) top.mainMenu.hideTreeFrame();
+
+        var documentDirty = false;
+        var dontShowWorker = false;
+        var baseurl = '<?php echo MODX_BASE_URL; ?>';
+        var $j = jQuery.noConflict();
+
         // set tree to default action.
         if (parent.tree) parent.tree.ca = "open";
 
-		// call the updateMail function, updates mail notification in top navigation
-		if (top.mainMenu && top.mainMenu.updateMail) top.mainMenu.updateMail(true);
-		
-		jQuery(function(){
-			var action = <?php echo $modx->manager->action;?>;
-			switch(action)
-			{
-				case 27:
-				case 17:
-				case 4:
-				case 87:
-				case 88:
-				case 11:
-				case 12:
-				case 28:
-				case 38:
-				case 35:
-				case 16:
-				case 19:
-				case 22:
-				case 23:
-				case 77:
-				case 78:
-				case 107:
-				case 108:
-				case 113:
-				case 100:
-				case 101:
-				case 102:
-				case 300:
-				case 301:
-					jQuery('input,textarea,select:not(#field_template,#which_editor,#stay)')
-						.change(
-							function() {
-								documentDirty=true;
-							}
-						);
-					gotosave=false;
-				break;
-			}
-			<?php if(isset($_REQUEST['r']))
-				echo sprintf("doRefresh(%s);\n",$_REQUEST['r']);
-			?>
-			jQuery('.tooltip').powerTip({'fadeInTime':'0','placement':'e'});
-		});
-		
-		jQuery(function(){
-			jQuery('#preLoader').hide();
-			jQuery('input.DatePicker').attr('autocomplete','off');
-		});
-        
-		jQuery(window).on('beforeunload', function(){
-			if(documentDirty) return '<?php echo addslashes($_lang['warning_not_saved']);?>';
-				jQuery('#actions').fadeOut(100);
-				jQuery('input,textarea,select').addClass('readonly');
-				jQuery('#preLoader').show();
-			if(!dontShowWorker && top.mainMenu) top.mainMenu.work();
-		});
-        
+        // call the updateMail function, updates mail notification in top navigation
+        if (top.mainMenu && top.mainMenu.updateMail) top.mainMenu.updateMail(true);
+
+        jQuery(function () {
+            var action = <?php echo $modx->manager->action;?>;
+            switch (action) {
+                case 27:
+                case 17:
+                case 4:
+                case 87:
+                case 88:
+                case 11:
+                case 12:
+                case 28:
+                case 38:
+                case 35:
+                case 16:
+                case 19:
+                case 22:
+                case 23:
+                case 77:
+                case 78:
+                case 107:
+                case 108:
+                case 113:
+                case 100:
+                case 101:
+                case 102:
+                case 300:
+                case 301:
+                    jQuery('input,textarea,select:not(#field_template,#which_editor,#stay)')
+                        .change(
+                            function () {
+                                documentDirty = true;
+                            }
+                        );
+                    gotosave = false;
+                    break;
+            }
+            <?php if (isset($_REQUEST['r'])) {
+            echo sprintf("doRefresh(%s);\n", $_REQUEST['r']);
+        }
+            ?>
+            jQuery('.tooltip').powerTip({'fadeInTime': '0', 'placement': 'e'});
+        });
+
+        jQuery(function () {
+            jQuery('#preLoader').hide();
+            jQuery('input.DatePicker').attr('autocomplete', 'off');
+        });
+
+        jQuery(window).on('beforeunload', function () {
+            if (documentDirty) return '<?php echo addslashes($_lang['warning_not_saved']);?>';
+            jQuery('#actions').fadeOut(100);
+            jQuery('input,textarea,select').addClass('readonly');
+            jQuery('#preLoader').show();
+            if (!dontShowWorker && top.mainMenu) top.mainMenu.work();
+        });
+
         function doRefresh(r) {
-            try{
+            try {
                 rr = r;
                 top.mainMenu.reloadPane(rr);
-            } catch(oException) {
-                vv = window.setTimeout('doRefresh(' + r + ')',200);
+            } catch (oException) {
+                vv = window.setTimeout('doRefresh(' + r + ')', 200);
             }
         }
     </script>
 </head>
-<body id="<?php echo $bodyid;?>" ondragstart="return false"<?php echo $modx_textdir==='rtl' ? ' class="rtl"':''?>>
-<div id="preLoader"><div class="preLoaderText"><?php echo $_style['ajax_loader']; ?></div></div>
+<body id="<?php echo $bodyid; ?>" ondragstart="return false"<?php echo $modx_textdir === 'rtl' ? ' class="rtl"' : '' ?>>
+<div id="preLoader">
+    <div class="preLoaderText"><?php echo $_style['ajax_loader']; ?></div>
+</div>
