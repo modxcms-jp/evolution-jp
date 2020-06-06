14c297343868fccc8dd5e0500e0a02cccb412e1a
yama <yamamoto@kyms.jp>
2020-06-06 14:22:24 +0900

    code cleanup

--- manager/actions/element/ajax/mutate_templates.php
+++ manager/actions/element/ajax/mutate_templates.php
@@ -3,27 +3,33 @@ if (!isset($modx) || !$modx->isLoggedin()) {
     exit;
 }
 
-if (preg_match('@^[1-9][0-9]*$@', $_REQUEST['id'])) {
-    return get_resources_byajax($_REQUEST['id']);
+if (preg_match('@^[1-9][0-9]*$@', anyv('id'))) {
+    return get_resources_byajax(anyv('id'));
 }
 
 function get_resources_byajax($id) {
-    global $modx, $_lang;
+    global $_lang;
 
-    $rs = db()->select('pagetitle,id', $modx->getFullTableName('site_content'), "template='{$id}'");
+    $rs = db()->select(
+        'pagetitle,id'
+        , '[+prefix+]site_content'
+        , "template='" . $id . "'"
+    );
     $total = db()->getRecordCount($rs);
-    if ($modx->config['limit_by_container'] < $total) {
-        $result = $_lang['a16_many_resources'];
-    } elseif ($total === 0) {
-        $result = $_lang['a16_no_resource'];
-    } else {
-        $tpl = '<a href="index.php?a=27&id=[+id+]">[+pagetitle+]([+id+])</a>';
-        $items = array();
-        while ($ph = db()->getRow($rs)) {
-            $items[] = $modx->parseText($tpl, $ph);
-        }
-        $result = join(', ', $items);
+    if (evo()->config['limit_by_container'] < $total) {
+        return sprintf('<p>%s</p>', $_lang['a16_many_resources']);
     }
-    return "<p>{$result}</p>";
+    if ($total === 0) {
+        return sprintf('<p>%s</p>', $_lang['a16_no_resource']);
+    }
+
+    $items = array();
+    while ($ph = db()->getRow($rs)) {
+        $items[] = evo()->parseText(
+            '<a href="index.php?a=27&id=[+id+]">[+pagetitle+]([+id+])</a>'
+            , $ph
+        );
+    }
+    return sprintf('<p>%s</p>', implode(', ', $items));
 }
 
