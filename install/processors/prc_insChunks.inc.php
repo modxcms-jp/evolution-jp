90b865b03d8c81f47318f4988e3cf9d2ecbc47ac
yama <yamamoto@kyms.jp>
2020-06-06 16:02:35 +0900

    Code cleanup install/processors/prc_insChunks.inc.php

--- install/processors/prc_insChunks.inc.php
+++ install/processors/prc_insChunks.inc.php
@@ -4,37 +4,38 @@ if (!sessionv('chunk') && !sessionv('installdata')) {
 }
 
 echo '<h3>' . lang('chunks') . ':</h3>';
-foreach ($tplChunks as $i=>$tplInfo) {
-    if(!sessionv('installdata') || !in_array('sample', $tplInfo['installset'])) {
+foreach ($tplChunks as $i => $tplInfo) {
+    if (!sessionv('installdata') || !in_array('sample', $tplInfo['installset'])) {
         $installSample = false;
     } else {
         $installSample = true;
     }
 
-    if(!in_array($i, sessionv('chunk')) && !$installSample) {
+    if (!in_array($i, sessionv('chunk')) && !$installSample) {
         continue;
     }
 
     $name = db()->escape($tplInfo['name']);
 
     if (!is_file($tplInfo['tpl_file_path'])) {
-        echo ng($name, sprintf("%s '%s' %s", lang('unable_install_chunk'), $tplInfo['tpl_file_path'], lang('not_found')));
+        echo ng($name,
+            sprintf("%s '%s' %s", lang('unable_install_chunk'), $tplInfo['tpl_file_path'], lang('not_found')));
         continue;
     }
 
     $field = array(
         'description' => $tplInfo['description'],
-        'snippet'     => preg_replace("@^.*?/\*\*.*?\*/\s+@s", '', file_get_contents($tplInfo['tpl_file_path']), 1),
-        'category'    => getCreateDbCategory($tplInfo['category'])
+        'snippet' => preg_replace("@^.*?/\*\*.*?\*/\s+@s", '', file_get_contents($tplInfo['tpl_file_path']), 1),
+        'category' => getCreateDbCategory($tplInfo['category'])
     );
 
     if (db()->getObject('site_htmlsnippets', "name='" . $name . "'")) {
-        if($tplInfo['overwrite'] === 'false') {
-            $rs =true;
+        if ($tplInfo['overwrite'] === 'false') {
+            $rs = true;
             $i = 0;
-            while($rs === true) {
+            while ($rs === true) {
                 $newname = $tplInfo['name'] . '-' . str_replace('.', '_', $modx_version);
-                if(0<$i) {
+                if (0 < $i) {
                     sprintf('%s(%s)', $newname, $i);
                 }
                 $newname = db()->escape($newname);
