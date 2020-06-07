f4aaa435bb699ea9d21bf2c8e688d49a59c2ed71
yama <yamamoto@kyms.jp>
2020-06-07 15:40:12 +0900

    Code cleanup install/index.php

--- install/index.php
+++ install/index.php
@@ -3,14 +3,12 @@
  * MODX Installer
  */
 
-// set error reporting
 error_reporting(E_ALL & ~E_NOTICE);
-ini_set('display_errors',1);
-
+ini_set('display_errors', 1);
 header("Content-Type: text/html; charset=utf-8");
 
 define('MODX_API_MODE', true);
-define('MODX_BASE_PATH', str_replace('\\','/', dirname(__DIR__)).'/');
+define('MODX_BASE_PATH', str_replace('\\', '/', dirname(__DIR__)) . '/');
 define('MODX_SETUP_PATH', MODX_BASE_PATH . 'install/');
 
 include_once(MODX_BASE_PATH . 'manager/includes/document.parser.class.inc.php');
@@ -18,29 +16,29 @@ $modx = new DocumentParser;
 
 require_once(MODX_BASE_PATH . 'manager/includes/version.inc.php');
 $cmsName = 'MODX';
-$cmsVersion = $modx_branch.' '.$modx_version;
+$cmsVersion = $modx_branch . ' ' . $modx_version;
 $moduleRelease = $modx_release_date;
 
 require_once(MODX_BASE_PATH . 'manager/includes/default.config.php');
 require_once(MODX_SETUP_PATH . 'functions.php');
 
-$rs = install_sessionCheck();
-if(!$rs) {
-    $ph = includeLang(lang_name());
-    $tpl = file_get_contents(MODX_SETUP_PATH . 'tpl/session_problem.tpl');
-    echo $modx->parseText($tpl,$ph);
+if (!install_sessionCheck()) {
+    echo $modx->parseText(
+        file_get_contents(MODX_SETUP_PATH . 'tpl/session_problem.tpl')
+        , includeLang(lang_name())
+    );
     exit;
 }
 
-sessionv('*prevAction', sessionv('currentAction',''));
+sessionv('*prevAction', sessionv('currentAction', ''));
 $action = anyv('action', 'mode');
 sessionv('*currentAction', $action);
 
-if($action==='mode') {
+if ($action === 'mode') {
     sessionv('*is_upgradeable', isUpGradeable());
 }
 
-if(sessionv('database_server')) {
+if (sessionv('database_server')) {
     db()->prop('*dbname', sessionv('dbase'));
     db()->prop('*table_prefix', sessionv('table_prefix', 'modx_'));
     db()->prop('*connection_method', sessionv('database_connection_method'));
@@ -54,18 +52,23 @@ if(sessionv('database_server')) {
 
 $_lang = includeLang(lang_name());
 
-$errors= 0;
+$errors = 0;
 
 $ph = ph();
-$ph = array_merge($ph,$_lang);
+$ph = array_merge($ph, $_lang);
 $ph['install_language'] = lang_name();
 
 ob_start();
-if (!@include(MODX_SETUP_PATH . "actions/" . $action . ".php")) {
-    die ('Invalid install action attempted. [action=' . $action . ']');
+if (!@include(sprintf('%sactions/%s.php', MODX_SETUP_PATH, $action))) {
+    exit (sprintf(
+        'Invalid install action attempted. [action=%s]'
+        , $action
+    ));
 }
 $ph['content'] = ob_get_contents();
 ob_end_clean();
 
-$tpl = file_get_contents(MODX_SETUP_PATH . 'tpl/template.tpl');
-echo $modx->parseText($tpl,$ph);
+echo evo()->parseText(
+    file_get_contents(MODX_SETUP_PATH . 'tpl/template.tpl')
+    , $ph
+);
