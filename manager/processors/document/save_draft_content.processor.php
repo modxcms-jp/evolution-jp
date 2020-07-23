29d2ca53d7b0a20c8a1352c9e895345119033a76
yama <yamamoto@kyms.jp>
2020-07-23 19:56:37 +0900

    refactor save_draft_content.processor.php

--- manager/processors/document/save_draft_content.processor.php
+++ manager/processors/document/save_draft_content.processor.php
@@ -4,40 +4,49 @@ if (!isset($modx) || !$modx->isLoggedin()) {
     exit;
 }
 
-if (!$modx->hasPermission('save_document')) {
-    $e->setError(3);
-    $e->dumpError();
+if (!hasPermission('save_document')) {
+    alert()->setError(3);
+    alert()->dumpError();
 }
 
-if (isset($_POST['id']) && preg_match('@^[1-9][0-9]*$@', $_POST['id'])) {
-    $docid = $_POST['id'];
-} else {
-    $e->setError(2);
-    $e->dumpError();
+if (! preg_match('@^[1-9][0-9]*$@', postv('id'))) {
+    alert()->setError(2);
+    alert()->dumpError();
+    return;
 }
 
-$modx->manager->saveFormValues(4);
+manager()->saveFormValues(4);
 
-$modx->loadExtension('REVISION');
-$modx->loadExtension('DocAPI');
+evo()->loadExtension('REVISION');
+evo()->loadExtension('DocAPI');
 
-$fields = $modx->doc->fixTvNest($_POST);
-$fields = $modx->doc->fixPubStatus($fields);
+$fields = $modx->doc->fixPubStatus(
+    $modx->doc->fixTvNest($_POST)
+);
 
 if (postv('stay') === 'save_standby') {
-    $rs = $modx->revision->save($docid, $fields, 'standby');
+    $rs = $modx->revision->save(postv('id'), $fields, 'standby');
 } else {
-    $rs = $modx->revision->save($docid, $fields, 'draft');
+    $rs = $modx->revision->save(postv('id'), $fields, 'draft');
 }
 
-$i = postv('stay');
-if ($i === 'new') {
-    $header = sprintf('Location: index.php?a=131&id=%s&r=1', $docid);
-} elseif ($i === 'stay') {
-    $header = sprintf('Location: index.php?a=131&id=%s&stay=stay', $docid);
-} elseif ($i === 'save_draft' || $i === 'save_standby') {
-    $header = sprintf('Location: index.php?a=133&id=%s&r=1', $docid);
-} else {
-    $header = sprintf('Location: index.php?a=3&id=%s&r=1', $docid);
+if (postv('stay') === 'new') {
+    header(
+        sprintf('Location: index.php?a=131&id=%s&r=1', postv('id'))
+    );
+    return;
 }
-header($header);
+if (postv('stay') === 'stay') {
+    header(
+        sprintf('Location: index.php?a=131&id=%s&stay=stay', postv('id'))
+    );
+    return;
+}
+if (in_array(postv('stay'), array('save_draft', 'save_standby'))) {
+    header(
+        sprintf('Location: index.php?a=133&id=%s&r=1', postv('id'))
+    );
+    return;
+}
+
+header(sprintf('Location: index.php?a=3&id=%s&r=1', postv('id')));
