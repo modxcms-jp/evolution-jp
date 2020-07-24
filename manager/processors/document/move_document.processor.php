9ddab10064b76b4a084220657ae37913372a8c9f
yama <yamamoto@kyms.jp>
2020-07-24 23:59:09 +0900

    refactor

--- manager/processors/document/move_document.processor.php
+++ manager/processors/document/move_document.processor.php
@@ -3,115 +3,153 @@ if (!isset($modx) || !$modx->isLoggedin()) {
     exit;
 }
 if (!$modx->hasPermission('move_document')) {
-    $e->setError(3);
-    $e->dumpError();
+    alert()->setError(3);
+    alert()->dumpError();
 }
 if (!$modx->hasPermission('edit_document')) {
-    $e->setError(3);
-    $e->dumpError();
+    alert()->setError(3);
+    alert()->dumpError();
 }
 
-if ($_REQUEST['id'] == $_REQUEST['new_parent']) {
-    $e->setError(600);
-    $e->dumpError();
+if (anyv('id') == anyv('new_parent')) {
+    alert()->setError(600);
+    alert()->dumpError();
 }
-if ($_REQUEST['id'] == '') {
-    $e->setError(601);
-    $e->dumpError();
+if (anyv('id') == '') {
+    alert()->setError(601);
+    alert()->dumpError();
 }
-if ($_REQUEST['new_parent'] == '') {
+if (anyv('new_parent') == '') {
     echo '<script type="text/javascript">parent.tree.ca = "open";</script>';
-    $e->setError(602);
-    $e->dumpError();
+    alert()->setError(602);
+    alert()->dumpError();
 }
 
-$tbl_site_content = $modx->getFullTableName('site_content');
-$doc_id = $_REQUEST['id'];
-if (strpos($doc_id, ',')) {
-    $doc_ids = explode(',', $doc_id);
-    $doc_id = substr($doc_id, 0, strpos($doc_id, ','));
+if (strpos(anyv('id'), ',')===false) {
+    $doc_ids[] = anyv('id');
+    $doc_id = anyv('id');
 } else {
-    $doc_ids[] = $doc_id;
+    $doc_ids = explode(',', anyv('id'));
+    $doc_id = $doc_ids[0];
 }
 
-$rs = $modx->db->select('parent', $tbl_site_content, "id='{$doc_id}'");
+$rs = db()->select(
+        'parent'
+        , '[+prefix+]site_content'
+        , sprintf("id='%s'", $doc_id)
+);
 if (!$rs) {
     exit("An error occured while attempting to find the resource's current parent.");
 }
-$current_parent = $modx->db->getValue($rs);
-$new_parent = (int)$_REQUEST['new_parent'];
+$current_parent = db()->getValue($rs);
+$new_parent = (int)anyv('new_parent');
 
 // check user has permission to move resource to chosen location
-if ($modx->config['use_udperms'] == 1 && $current_parent != $new_parent) {
-    if (!$modx->checkPermissions($new_parent)) {
+if (evo()->config['use_udperms'] == 1 && $current_parent != $new_parent) {
+    if (!evo()->checkPermissions($new_parent)) {
         include_once(MODX_MANAGER_PATH . 'actions/header.inc.php');
         ?>
         <script type="text/javascript">parent.tree.ca = '';</script>
-        <div class="sectionHeader"><?php echo $_lang['access_permissions']; ?></div>
+        <div class="sectionHeader"><?php echo lang('access_permissions'); ?></div>
         <div class="sectionBody">
-            <p><?php echo $_lang['access_permission_parent_denied']; ?></p>
+            <p><?php echo lang('access_permission_parent_denied'); ?></p>
         </div>
         <?php
         include_once(MODX_MANAGER_PATH . 'actions/footer.inc.php');
         exit;
     }
 }
-$children = allChildren($doc_id);
-$alert = '';
+
 if ($current_parent == $new_parent) {
-    $alert = $_lang["move_resource_new_parent"];
-} elseif (in_array($new_parent, $children)) {
-    $alert = $_lang["move_resource_cant_myself"];
-} else {
-    $rs = $modx->db->update('isfolder=1', $tbl_site_content, "id='{$new_parent}'");
-    if (!$rs) {
-        $alert = "An error occured while attempting to change the new parent to a folder.";
-    }
-
-    // increase menu index
-    if ($modx->config['auto_menuindex'] === null || $modx->config['auto_menuindex']) {
-        $menuindex = $modx->db->getValue($modx->db->select('max(menuindex)', $tbl_site_content,
-                "parent='{$new_parent}'")) + 1;
-    } else {
-        $menuindex = 0;
-    }
-
-    $user_id = $modx->getLoginUserID();
-    if (is_array($doc_ids)) {
-        foreach ($doc_ids as $v) {
-            update_parentid($v, $new_parent, $user_id, $menuindex);
-            $menuindex++;
-        }
-    }
-
-    // finished moving the resource, now check to see if the old_parent should no longer be a folder.
-    $rs = $modx->db->select('count(*) as count', $tbl_site_content, "parent='{$current_parent}'");
-    if (!$rs) {
-        $alert = "An error occured while attempting to find the old parents' children.";
-    }
-
-    $row = $modx->db->getRow($rs);
-
-    if (!$row['count']) {
-        $rs = $modx->db->update('isfolder=0', '[+prefix+]site_content', "id='{$current_parent}'");
-        if (!$rs) {
-            $alert = 'An error occured while attempting to change the old parent to a regular resource.';
-        }
-    }
-}
-
-if ($alert) {
-    $modx->webAlertAndQuit(
-        $alert
-        , "javascript:parent.tree.ca='open';window.location.href='index.php?a=51&id={$doc_id}';"
+    alertAndQuit(
+        lang('move_resource_new_parent')
+        , $doc_id
     );
     exit;
 }
 
-$modx->clearCache();
+$children = allChildren($doc_id);
+if (in_array($new_parent, $children)) {
+    alertAndQuit(
+        lang('move_resource_cant_myself')
+        , $doc_id
+    );
+    exit;
+}
+
+$rs = db()->update(
+        'isfolder=1'
+        , '[+prefix+]site_content'
+        , "id='".$new_parent."'"
+);
+if (!$rs) {
+    alertAndQuit(
+        'An error occured while attempting to change the new parent to a folder.'
+        , $doc_id
+    );
+    exit;
+}
+
+// increase menu index
+if (evo()->config('auto_menuindex') === null || evo()->config('auto_menuindex')) {
+    $menuindex = db()->getValue(
+            db()->select(
+                'max(menuindex)'
+                , '[+prefix+]site_content'
+                , "parent='".$new_parent."'"
+            )
+        ) + 1;
+} else {
+    $menuindex = 0;
+}
+
+$user_id = evo()->getLoginUserID();
+if (is_array($doc_ids)) {
+    foreach ($doc_ids as $v) {
+        update_parentid($v, $new_parent, $user_id, $menuindex);
+        $menuindex++;
+    }
+}
+
+// finished moving the resource, now check to see if the old_parent should no longer be a folder.
+$rs = db()->select(
+        'count(*) as count'
+        , '[+prefix+]site_content'
+        , "parent='".$current_parent."'"
+);
+if (!$rs) {
+    alertAndQuit(
+        "An error occured while attempting to find the old parents' children."
+        , $doc_id
+    );
+    exit;
+}
+
+$row = db()->getRow($rs);
+if (! $row['count']) {
+    $rs = db()->update(
+        'isfolder=0'
+        , '[+prefix+]site_content'
+        , sprintf("id='%s'", $current_parent)
+    );
+    if (!$rs) {
+        alertAndQuit(
+            'An error occured while attempting to change the old parent to a regular resource.'
+            , $doc_id
+        );
+        exit;
+    }
+}
+
+evo()->clearCache();
 
 if ($new_parent !== 0) {
-    header("Location: index.php?a=120&id={$current_parent}&r=1");
+    header(
+        sprintf(
+            'Location: index.php?a=120&id=%s&r=1'
+            , $current_parent
+        )
+    );
     exit;
 }
 
@@ -120,17 +158,26 @@ header("Location: index.php?a=2&r=1");
 exit;
 
 
+function alertAndQuit($string, $docid) {
+    evo()->webAlertAndQuit(
+        $string
+        , sprintf(
+            "javascript:parent.tree.ca='open';window.location.href='index.php?a=51&id=%s';"
+            , $docid
+        )
+    );
+    exit;
+}
+
 function allChildren($docid) {
-    global $modx;
-    $tbl_site_content = $modx->getFullTableName('site_content');
     $children = array();
-    $rs = $modx->db->select('id', $tbl_site_content, "parent='{$docid}'");
+    $rs = db()->select('id', '[+prefix+]site_content', "parent='{$docid}'");
     if (!$rs) {
         exit("An error occured while attempting to find all of the resource's children.");
     }
 
-    if ($numChildren = $modx->db->getRecordCount($rs)) {
-        while ($child = $modx->db->getRow($rs)) {
+    if ($numChildren = db()->getRecordCount($rs)) {
+        while ($child = db()->getRow($rs)) {
             $children[] = $child['id'];
             $nextgen = allChildren($child['id']);
             foreach ($nextgen as $k => $v) {
@@ -142,25 +189,28 @@ function allChildren($docid) {
 }
 
 function update_parentid($doc_id, $new_parent, $user_id, $menuindex) {
-    global $modx, $_lang;
-    $tbl_site_content = $modx->getFullTableName('site_content');
-    if (!$modx->config['allow_duplicate_alias']) {
-        $rs = $modx->db->select("IF(alias='', id, alias) AS alias", $tbl_site_content, "id='{$doc_id}'");
-        $alias = $modx->db->getValue($rs);
-        $rs = $modx->db->select('id', $tbl_site_content,
+    if (!evo()->config('allow_duplicate_alias')) {
+        $rs = db()->select("IF(alias='', id, alias) AS alias", '[+prefix+]site_content', "id='{$doc_id}'");
+        $alias = db()->getValue($rs);
+        $rs = db()->select('id', '[+prefix+]site_content',
             "parent='{$new_parent}' AND (alias='{$alias}' OR (alias='' AND id='{$alias}'))");
-        $find = $modx->db->getRecordcount($rs);
+        $find = db()->getRecordcount($rs);
         if (0 < $find) {
-            $target_id = $modx->db->getValue($rs);
+            $target_id = db()->getValue($rs);
             $url = "javascript:parent.tree.ca='open';window.location.href='index.php?a=27&id={$doc_id}';";
-            $modx->webAlertAndQuit(sprintf($_lang["duplicate_alias_found"], $target_id, $alias), $url);
+            evo()->webAlertAndQuit(sprintf(lang('duplicate_alias_found'), $target_id, $alias), $url);
             exit;
         }
     }
-    $field['parent'] = $new_parent;
-    $field['editedby'] = $user_id;
-    $field['menuindex'] = $menuindex;
-    $rs = $modx->db->update($field, $tbl_site_content, "id='{$doc_id}'");
+    $rs = db()->update(
+        array(
+            'parent' => $new_parent,
+            'editedby' => $user_id,
+            'menuindex' => $menuindex
+        )
+        , '[+prefix+]site_content'
+        , sprintf("id='%s'", $doc_id)
+    );
     if (!$rs) {
         exit("An error occured while attempting to move the resource to the new parent.");
     }
