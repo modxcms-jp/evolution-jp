0076a0d5428b715e4e3de911df0956ecfd80aaf9
yama <yamamoto@kyms.jp>
2020-12-12 21:34:37 +0900

    version 1.0.24J

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.0.23J';    // Current version number
-$modx_release_date = '2020-06-06'; // Date of release
+$modx_version      = '1.0.24J';    // Current version number
+$modx_release_date = '2020-12-12'; // Date of release
 $modx_branch       = 'Evolution';  // Codebase name
 $modx_full_appname = sprintf('MODX %s %s (%s)', $modx_branch, $modx_version, $modx_release_date);
