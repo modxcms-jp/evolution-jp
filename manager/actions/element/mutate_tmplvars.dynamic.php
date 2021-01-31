ad68a153d58772a8683179d23cbd64bb9ba82e06
yama <yamamoto@kyms.jp>
2021-02-01 07:37:37 +0900

    code clenup

--- manager/actions/element/mutate_tmplvars.dynamic.php
+++ manager/actions/element/mutate_tmplvars.dynamic.php
@@ -80,14 +80,14 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
 ?>
 <script language="JavaScript">
     function duplicaterecord() {
-        if (confirm("<?=$_lang['confirm_duplicate_record'] ?>") == true) {
+        if (confirm("<?= $_lang['confirm_duplicate_record'] ?>") == true) {
             documentDirty = false;
-            document.location.href = "index.php?id=<?=$_REQUEST['id']; ?>&a=304";
+            document.location.href = "index.php?id=<?= $_REQUEST['id']; ?>&a=304";
         }
     }
 
     function deletedocument() {
-        if (confirm("<?=$_lang['confirm_delete_tmplvars']; ?>") == true) {
+        if (confirm("<?= $_lang['confirm_delete_tmplvars']; ?>") == true) {
             documentDirty = false;
             document.location.href = "index.php?id=" + document.mutate.id.value + "&a=303";
         }
@@ -101,7 +101,7 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
     widgetParams['hyperlink'] = '&text=Display Text;string; &title=Title;string; &linkclass=Class;string &linkstyle=Style;string &target=Target;string &linkattrib=Attributes;string';
     widgetParams['htmltag'] = '&tagoutput=Content;textarea;[+value+] &tagname=Tag Name;string;div &tagid=Tag ID;string &tagclass=Class;string &tagstyle=Style;string &tagattrib=Attributes;string';
     widgetParams['datagrid'] = '&cdelim=Column Delimiter;list;%2C,tab,||,:: &cwrap=Column Wrapper;string;" &enc=Src Encode;list;utf-8,sjis-win,sjis,eucjp-win,euc-jp,jis,auto &detecthead=Detect Header;list;first line,none;first line &cols=Column Names;string &cwidth=Column Widths;string &calign=Column Alignments;string &ccolor=Column Colors;string &ctype=Column Types;string &cpad=Cell Padding;string; &cspace=Cell Spacing;string; &psize=Page Size;int;100 &ploc=Pager Location;list;top-right,top-left,bottom-left,bottom-right,both-right,both-left; &pclass=Pager Class;string &pstyle=Pager Style;string &head=Header Text;string &foot=Footer Text;string &tblc=Grid Class;string &tbls=Grid Style;string &itmc=Item Class;string; &itms=Item Style;string &aitmc=Alt Item Class;string &aitms=Alt Item Style;string &chdrc=Column Header Class;string &chdrs=Column Header Style;string;&egmsg=Empty message;string;No records found;';
-    widgetParams['richtext'] = '&w=Width;string;100% &h=Height;string;300px &edt=Editor;list;<?=$RTEditors; ?>';
+    widgetParams['richtext'] = '&w=Width;string;100% &h=Height;string;300px &edt=Editor;list;<?= $RTEditors; ?>';
     widgetParams['image'] = '&imgoutput=Src;textarea;[+value+] &alttext=Alternate Text;string &align=Align;list;none,baseline,top,middle,bottom,texttop,absmiddle,absbottom,left,right &name=Name;string &imgclass=Class;string &id=ID;string &imgstyle=Style;string &imgattrib=Other Attribs;string';
     widgetParams['custom_widget'] = '&output=Output;textarea;[+value+]';
 
@@ -140,7 +140,7 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
         dp = (widgetParams[df]) ? widgetParams[df].split("&") : "";
         if (!dp) tr.style.display = 'none';
         else {
-            t = '<table width="400" style="margin-bottom:3px;background-color:#EEEEEE" cellpadding="2" cellspacing="1"><thead><tr><td width="50%"><?=$_lang['parameter']; ?></td><td width="50%"><?=$_lang['value']; ?></td></tr></thead>';
+            t = '<table width="400" style="margin-bottom:3px;background-color:#EEEEEE" cellpadding="2" cellspacing="1"><thead><tr><td width="50%"><?= $_lang['parameter']; ?></td><td width="50%"><?= $_lang['value']; ?></td></tr></thead>';
             for (p = 0; p < dp.length; p++) {
                 dp[p] = (dp[p] + '').replace(/^\s|\s$/, ""); // trim
                 ar = dp[p].split("=");
@@ -270,12 +270,12 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
         echo implode("", $evtOut);
     }
     ?>
-    <input type="hidden" name="id" value="<?=$content['id']; ?>">
+    <input type="hidden" name="id" value="<?= $content['id']; ?>">
     <input type="hidden" name="a" value="302">
-    <input type="hidden" name="mode" value="<?=$_GET['a']; ?>">
+    <input type="hidden" name="mode" value="<?= $_GET['a']; ?>">
     <input type="hidden" name="params" value="<?=htmlspecialchars($content['display_params']); ?>">
 
-    <h1><?=$_lang['tmplvars_title'];
+    <h1><?= $_lang['tmplvars_title'];
         if ($id) {
             echo "(ID:{$id})";
         } ?></h1>
@@ -286,15 +286,15 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
                 <li id="Button1" class="mutate">
                     <a href="#"
                         onclick="documentDirty=false;jQuery('#mutate').submit();jQuery('#Button1').hide();jQuery('input,textarea,select').addClass('readonly');">
-                        <img src="<?=$_style["icons_save"] ?>"/> <?=$_lang['update'] ?>
+                        <img src="<?= $_style["icons_save"] ?>"/> <?= $_lang['update'] ?>
                     </a><span class="and"> + </span>
                     <select id="stay" name="stay">
                         <option id="stay1"
-                                value="1" <?=$_REQUEST['stay'] == '1' ? ' selected=""' : '' ?> ><?=$_lang['stay_new'] ?></option>
+                                value="1" <?= $_REQUEST['stay'] == '1' ? ' selected=""' : '' ?> ><?= $_lang['stay_new'] ?></option>
                         <option id="stay2"
-                                value="2" <?=$_REQUEST['stay'] == '2' ? ' selected="selected"' : '' ?> ><?=$_lang['stay'] ?></option>
+                                value="2" <?= $_REQUEST['stay'] == '2' ? ' selected="selected"' : '' ?> ><?= $_lang['stay'] ?></option>
                         <option id="stay3"
-                                value="" <?=$_REQUEST['stay'] == '' ? ' selected=""' : '' ?> ><?=$_lang['close'] ?></option>
+                                value="" <?= $_REQUEST['stay'] == '' ? ' selected=""' : '' ?> ><?= $_lang['close'] ?></option>
                     </select>
                 </li>
             <?php endif; ?>
@@ -330,10 +330,10 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
     <div class="sectionBody">
         <div class="tab-pane" id="tmplvarsPane">
             <div class="tab-page" id="tabGeneral">
-                <h2 class="tab"><?=$_lang['settings_general']; ?></h2>
+                <h2 class="tab"><?= $_lang['settings_general']; ?></h2>
                 <table>
                     <tr>
-                        <th align="left"><?=$_lang['tmplvars_name']; ?></th>
+                        <th align="left"><?= $_lang['tmplvars_name']; ?></th>
                         <td align="left"><span style="font-family:'Courier New', Courier, mono">[*</span><input
                                     name="name" type="text" maxlength="50"
                                     value="<?=htmlspecialchars($content['name']); ?>" class="inputBox"
@@ -341,14 +341,14 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
                                     style="font-family:'Courier New', Courier, mono">*]</span></td>
                     </tr>
                     <tr>
-                        <th align="left"><?=$_lang['tmplvars_caption']; ?></th>
+                        <th align="left"><?= $_lang['tmplvars_caption']; ?></th>
                         <td align="left"><input name="caption" type="text" maxlength="80"
                                                 value="<?=htmlspecialchars($content['caption']); ?>"
                                                 class="inputBox" style="width:300px;"></td>
                     </tr>
 
                     <tr>
-                        <th align="left"><?=$_lang['tmplvars_type']; ?></th>
+                        <th align="left"><?= $_lang['tmplvars_type']; ?></th>
                         <td align="left">
                             <select id="type" name="type" size="1" class="inputBox" style="width:300px;">
                                 <?php
@@ -427,23 +427,23 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
                         $display = '';
                     }
                     ?>
-                    <tr id="inputoption" <?=$display; ?>>
-                        <th align="left" valign="top"><?=$_lang['tmplvars_elements']; ?></th>
-                        <td align="left" nowrap="nowrap"><?=$form_elements . $tooltip_input_option; ?></td>
+                    <tr id="inputoption" <?= $display; ?>>
+                        <th align="left" valign="top"><?= $_lang['tmplvars_elements']; ?></th>
+                        <td align="left" nowrap="nowrap"><?= $form_elements . $tooltip_input_option; ?></td>
                     </tr>
                     <tr>
-                        <th align="left" valign="top"><?=$_lang['tmplvars_default']; ?></th>
+                        <th align="left" valign="top"><?= $_lang['tmplvars_default']; ?></th>
                         <td
                             align="left" nowrap="nowrap"
                         ><textarea
                             name="default_text" type="text"
                             class="inputBox phptextarea" rows="5"
                             style="width:400px;"
-                        ><?=hsc($content['default_text']);?></textarea><?=$tooltip_tv_binding; ?>
+                        ><?=hsc($content['default_text']);?></textarea><?= $tooltip_tv_binding; ?>
                         </td>
                     </tr>
                     <tr>
-                        <th align="left"><?=$_lang['tmplvars_widget']; ?></th>
+                        <th align="left"><?= $_lang['tmplvars_widget']; ?></th>
                         <td align="left">
                             <select name="display" size="1" class="inputBox" style="width:400px;"
                                     onchange="showParameters(this);">
@@ -478,11 +478,11 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
                         </td>
                     </tr>
                     <tr id="displayparamrow">
-                        <td valign="top" align="left"><?=$_lang['tmplvars_widget_prop']; ?>
+                        <td valign="top" align="left"><?= $_lang['tmplvars_widget_prop']; ?>
                             <div style="padding-top:8px;"><a href="javascript://"
                                                              onclick="resetParameters(); return false"><img
-                                            src="<?=$_style['icons_refresh']; ?>"
-                                            alt="<?=$_lang['tmplvars_reset_params']; ?>"></a></div>
+                                            src="<?= $_style['icons_refresh']; ?>"
+                                            alt="<?= $_lang['tmplvars_reset_params']; ?>"></a></div>
                         </td>
                         <td align="left" id="displayparams">&nbsp;</td>
                     </tr>
@@ -491,8 +491,8 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
 
             <!-- Template Permission -->
             <div class="tab-page" id="tabPerm">
-                <h2 class="tab"><?=$_lang['tmplvar_tmpl_access']; ?></h2>
-                <p><?=$_lang['tmplvar_tmpl_access_msg']; ?></p>
+                <h2 class="tab"><?= $_lang['tmplvar_tmpl_access']; ?></h2>
+                <p><?= $_lang['tmplvar_tmpl_access_msg']; ?></p>
                 <style type="text/css">
                     label {
                         display: block;
@@ -534,13 +534,13 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
             </div>
 
             <div class="tab-page" id="tabInfo">
-                <h2 class="tab"><?=$_lang['settings_properties']; ?></h2>
+                <h2 class="tab"><?= $_lang['settings_properties']; ?></h2>
                 <table>
                     <tr>
-                        <th align="left"><?=$_lang['existing_category']; ?></th>
+                        <th align="left"><?= $_lang['existing_category']; ?></th>
                         <td align="left">
                             <select name="categoryid" style="width:300px;">
-                                <option value="0"><?=$_lang["no_category"]; ?></option>
+                                <option value="0"><?= $_lang["no_category"]; ?></option>
                                 <?php
                                 $ds = $modx->manager->getCategories();
                                 if ($ds) {
@@ -549,20 +549,20 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
                                     }
                                 }
                                 ?>
-                                <option value="-1">&gt;&gt; <?=$_lang["new_category"]; ?></option>
+                                <option value="-1">&gt;&gt; <?= $_lang["new_category"]; ?></option>
                             </select>
                         </td>
                     </tr>
                     <tr id="newcategry" style="display:none;">
                         <th align="left" valign="top"
-                            style="padding-top:5px;"><?=$_lang['new_category']; ?></th>
+                            style="padding-top:5px;"><?= $_lang['new_category']; ?></th>
                         <td align="left" valign="top" style="padding-top:5px;"><input name="newcategory" type="text"
                                                                                       maxlength="45" value=""
                                                                                       class="inputBox"
                                                                                       style="width:300px;"></td>
                     </tr>
                     <tr>
-                        <th align="left"><?=$_lang['tmplvars_description']; ?></th>
+                        <th align="left"><?= $_lang['tmplvars_description']; ?></th>
                         <td align="left"><textarea name="description"
                                                    style="padding:0;height:4em;"><?=htmlspecialchars($content['description']); ?></textarea>
                         </td>
@@ -570,15 +570,15 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
                     <?php if ($modx->hasPermission('save_template') == 1) { ?>
                         <tr>
                             <td align="left" colspan="2"><label><input name="locked" value="on"
-                                                                       type="checkbox" <?=$content['locked'] == 1 ? "checked='checked'" : ""; ?>
+                                                                       type="checkbox" <?= $content['locked'] == 1 ? "checked='checked'" : ""; ?>
                                                                        class="inputBox"/>
-                                    <b><?=$_lang['lock_tmplvars']; ?></b> <span
-                                            class="comment"><?=$_lang['lock_tmplvars_msg']; ?></span></label>
+                                    <b><?= $_lang['lock_tmplvars']; ?></b> <span
+                                            class="comment"><?= $_lang['lock_tmplvars_msg']; ?></span></label>
                             </td>
                         </tr>
                     <?php } ?>
                     <tr>
-                        <th align="left"><?=$_lang['tmplvars_rank']; ?></th>
+                        <th align="left"><?= $_lang['tmplvars_rank']; ?></th>
                         <td align="left"><input name="rank" type="text" maxlength="4"
                                                 value="<?=(isset($content['rank'])) ? $content['rank'] : 0; ?>"
                                                 class="inputBox" style="width:300px;"></td>
@@ -599,7 +599,7 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
                 if ($modx->hasPermission('access_permissions')) {
                     ?>
                     <div class="tab-page" id="tabAccess">
-                    <h2 class="tab"><?=$_lang['access_permissions']; ?></h2>
+                    <h2 class="tab"><?= $_lang['access_permissions']; ?></h2>
                     <script type="text/javascript">
                         function makePublic(b) {
                             var notPublic = false;
@@ -620,7 +620,7 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
                             }
                         }
                     </script>
-                    <p><?=$_lang['tmplvar_access_msg']; ?></p>
+                    <p><?= $_lang['tmplvar_access_msg']; ?></p>
                     <?php
                 }
                 $chk = '';
@@ -653,7 +653,7 @@ $tooltip_input_option = $modx->parseText($tooltip_tpl, $ph);
             ?>
             <div class="tab-page" id="tabHelp">
                 <h2 class="tab">ヘルプ</h2>
-                <?=$_lang['tmplvars_msg']; ?>
+                <?= $_lang['tmplvars_msg']; ?>
             </div>
             <?php
             // invoke OnTVFormRender event
