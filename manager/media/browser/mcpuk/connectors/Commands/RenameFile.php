c64362bc8a9b9533ab4527cb15792f4460acf991
yama <yamamoto@kyms.jp>
2021-04-29 21:00:57 +0900

    refactor - file browser

--- manager/media/browser/mcpuk/connectors/Commands/RenameFile.php
+++ manager/media/browser/mcpuk/connectors/Commands/RenameFile.php
@@ -16,74 +16,100 @@
  * File Authors:
  * 		Grant French (grant@mcpuk.net)
  */
-class RenameFile {
+class RenameFile
+{
     public $fckphp_config;
     public $type;
     public $cwd;
     public $actual_cwd;
 
-	function __construct($fckphp_config,$type,$cwd) {
-		$this->fckphp_config=$fckphp_config;
-		$this->type=$type;
-		$this->raw_cwd=$cwd;
-		$this->actual_cwd=str_replace('//','/',($fckphp_config['UserFilesPath']."/$type/".$this->raw_cwd));
-		$this->real_cwd=str_replace('//','/',($this->fckphp_config['basedir']."/".$this->actual_cwd));
-		$this->filename=str_replace(array('../','/'),'',getv('FileName'));
-		$this->newname=str_replace(array('../','/'),'',$this->checkName(getv('NewName')));
-	}
-	
-	function checkName($name) {
-		$newName="";
-		for ($i=0, $iMax = strlen($name); $i< $iMax; $i++) {
-			if (in_array($name[$i],$this->fckphp_config['FileNameAllowedChars'])) $newName.=$name[$i];
-		}
-		return $newName;
-	}
-	
-	function run() {
-		$result1=false;
-		$result2=true;
-		
-		if ($this->newname!='') {
-		
-			if ($this->nameValid($this->newname)) {
-				//Remove thumbnail if it exists
-				$result2=true;
-				$thumb=$this->real_cwd.'/.thumb/'.$this->filename;
-				if (file_exists($thumb)) $result2=unlink($thumb);
-				
-				$result1=rename($this->real_cwd.'/'.$this->filename,$this->real_cwd.'/'.$this->newname);
-			}
-			else $result1=false;
-		}
-		
-		header ("content-type: text/xml");
-		echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
-		?>
-<Connector command="RenameFile" resourceType="<?php echo $this->type; ?>">
-	<CurrentFolder path="<?php echo $this->raw_cwd; ?>" url="<?php echo $this->actual_cwd; ?>" />
-	<?php
-		if ($result1&&$result2) $err_no=0;
-		else                    $err_no=502;
-	?>
-	<Error number="<?php echo "".$err_no; ?>" />
-</Connector>
-		<?php
-	}
-	
-	function nameValid($fname) {
-		$type_config=$this->fckphp_config['ResourceAreas'][$this->type];
-		
-		$lastdot=strrpos($fname,".");
-			
-		if ($lastdot!==false) {
-			$ext=substr($fname,($lastdot+1));
-			$fname=substr($fname,0,$lastdot);
-			
-			if (in_array(strtolower($ext),$type_config['AllowedExtensions'])) 
-				return true;
-			else
-				return false;
-		}
-	}
+    function __construct($fckphp_config, $type, $cwd)
+    {
+        $this->fckphp_config = $fckphp_config;
+        $this->type = $type;
+        $this->raw_cwd = $cwd;
+        $this->actual_cwd = str_replace(
+            '//',
+            '/',
+            sprintf(
+                "%s/%s/%s",
+                $fckphp_config['UserFilesPath'],
+                $type,
+                $this->raw_cwd
+            )
+        );
+        $this->real_cwd = str_replace(
+            '//',
+            '/',
+            $this->fckphp_config['basedir'] . "/" . $this->actual_cwd
+        );
+        $this->filename = str_replace(
+            array(
+                '../', '/'
+            ),
+            '',
+            getv('FileName')
+        );
+        $this->newname = str_replace(
+            array('../', '/'),
+            '',
+            $this->checkName(getv('NewName'))
+        );
+    }
+
+    function checkName($name)
+    {
+        return unescape($name);
+        $newName = "";
+        for ($i = 0, $iMax = strlen($name); $i < $iMax; $i++) {
+            if (in_array($name[$i], $this->fckphp_config['FileNameAllowedChars'])) {
+                $newName .= $name[$i];
+            }
+        }
+        return $newName;
+    }
+
+    function run()
+    {
+        $result1 = false;
+        $result2 = true;
+
+        if ($this->newname != '') {
+            $result2 = true;
+            $thumb = $this->real_cwd . '/.thumb/' . $this->newname;
+            if (file_exists($thumb)) {
+                $result2 = unlink($thumb);
+            }
+            $result1 = rename(
+                $this->real_cwd . '/' . unescape($this->filename),
+                $this->real_cwd . '/' . $this->newname
+            );
+        }
+        header("content-type: text/xml");
+        echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
+?>
+        <Connector command="RenameFile" resourceType="<?php echo $this->type; ?>">
+            <CurrentFolder path="<?php echo $this->raw_cwd; ?>" url="<?php echo $this->actual_cwd; ?>" />
+            <?php
+            if ($result1 && $result2) $err_no = 0;
+            else                    $err_no = 502;
+            ?>
+            <Error number="<?php echo "" . $err_no; ?>" />
+        </Connector>
+<?php
+    }
+
+    function nameValid($fname)
+    {
+        $lastdot = strrpos($fname, '.');
+        if ($lastdot === false) {
+            return false;
+        }
+        $ext = substr($fname, ($lastdot + 1));
+        $extensions = $this->fckphp_config['ResourceAreas'][$this->type]['AllowedExtensions'];
+        if (!in_array(strtolower($ext), $extensions)) {
+            return false;
+        }
+        return true;
+    }
 }
