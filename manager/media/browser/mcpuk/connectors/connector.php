dd184aeff1832215f6c619ceab2009eb9aba2206
yama <yamamoto@kyms.jp>
2021-04-29 17:04:42 +0900

    ファイルブラウザで日本語を扱う http://forum.modx.jp/viewtopic.php?f=34&t=1945

--- manager/media/browser/mcpuk/connectors/connector.php
+++ manager/media/browser/mcpuk/connectors/connector.php
@@ -50,6 +50,7 @@ $type    = (isset($_GET['Type']) && $_GET['Type']!='')                   ? $_GET
 $type    = strtolower($type);
 $cwd     = (isset($_GET['CurrentFolder']) && $_GET['CurrentFolder']!='') ? $_GET['CurrentFolder'] : '/';
 $cwd     = str_replace('..', '', $cwd);
+$cwd = unescape($cwd);
 $extra   = (isset($_GET['ExtraParams']) && $_GET['ExtraParams']!='')     ? $_GET['ExtraParams']   : '';
 
 if ($fckphp_config['Debug']===true) {
@@ -105,4 +106,56 @@ function outputHeaders() {
 	
 	// HTTP/1.0
 	header("Pragma: no-cache");
+}
+
+function unescape($source, $iconv_to = 'UTF-8') {
+    $decodedStr = '';
+    $pos = 0;
+    $len = strlen ($source);
+    while ($pos < $len) {
+        $charAt = substr ($source, $pos, 1);
+        if ($charAt == '%') {
+            $pos++;
+            $charAt = substr ($source, $pos, 1);
+            if ($charAt == 'u') {
+                // we got a unicode character
+                $pos++;
+                $unicodeHexVal = substr ($source, $pos, 4);
+                $unicode = hexdec ($unicodeHexVal);
+                $decodedStr .= code2utf($unicode);
+                $pos += 4;
+            }
+            else {
+                // we have an escaped ascii character
+                $hexVal = substr ($source, $pos, 2);
+                $decodedStr .= chr (hexdec ($hexVal));
+                $pos += 2;
+            }
+        }
+        else {
+            $decodedStr .= $charAt;
+            $pos++;
+        }
+    }
+
+    if ($iconv_to != "UTF-8") {
+        $decodedStr = iconv("UTF-8", $iconv_to, $decodedStr);
+    }
+   
+    return $decodedStr;
+}
+
+/**
+* Function coverts number of utf char into that character.
+* Function taken from: http://sk2.php.net/manual/en/function.utf8-encode.php#49336
+*
+* @param int $num
+* @return utf8char
+*/
+function code2utf($num){
+    if($num<128)return chr($num);
+    if($num<2048)return chr(($num>>6)+192).chr(($num&63)+128);
+    if($num<65536)return chr(($num>>12)+224).chr((($num>>6)&63)+128).chr(($num&63)+128);
+    if($num<2097152)return chr(($num>>18)+240).chr((($num>>12)&63)+128).chr((($num>>6)&63)+128) .chr(($num&63)+128);
+    return '';
 }
