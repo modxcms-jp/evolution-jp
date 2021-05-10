9fc7a09057dcd0ff5cbe50a961dfd81a07728c4f
yama <yamamoto@kyms.jp>
2021-05-10 18:22:36 +0900

    versio 1.0.25J

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.0.24J';    // Current version number
-$modx_release_date = '2021-04-30'; // Date of release
+$modx_version      = '1.0.25J';    // Current version number
+$modx_release_date = '2021-05-10'; // Date of release
 $modx_branch       = 'Evolution';  // Codebase name
 $modx_full_appname = sprintf('MODX %s %s (%s)', $modx_branch, $modx_version, $modx_release_date);
