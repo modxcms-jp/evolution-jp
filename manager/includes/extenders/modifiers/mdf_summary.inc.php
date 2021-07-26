865b56560fa53bfca3494c8c3d3f79439583f724
yama <yamamoto@kyms.jp>
2021-07-26 09:32:42 +0900

    refactor mdf_summary.inc.php

--- manager/includes/extenders/modifiers/mdf_summary.inc.php
+++ manager/includes/extenders/modifiers/mdf_summary.inc.php
@@ -1,38 +1,52 @@
 <?php
-if(strpos($opt,',')) list($limit,$delim) = explode(',', $opt);
-elseif(preg_match('/^[1-9][0-9]*$/',$opt)) {$limit=$opt;$delim='';}
-else {$limit=124;$delim='';}
+if (!preg_match('/^[1-9][0-9]*$/', $opt)) {
+    $limit = 124;
+} else {
+    $limit = $opt;
+}
 
-$limit = intval($limit);
-
-$content = $modx->filter->parseDocumentSource($value);
-
-$content = $this->strip_tags($content);
-$content = str_replace('&nbsp;', ' ', $content);
-$content = str_replace('　', ' ', $content);
-$content = str_replace( "\xc2\xa0", " ", $content );
-$content = str_replace(array("\r\n","\r","\n","\t",' '),' ',$content);
-$content = str_replace(array('。 ','、 ',' ・','。・','…'),array('。','、','・','。','・・'),$content);
+$content = str_replace(
+    array('。 ', '、 ', ' ・', '。・', '…'),
+    array('。',  '、',  '・',  '。', '・・'),
+    str_replace(
+        array('&nbsp;', '　', "\xc2\xa0", "\r", "\n", "\t", ' '),
+        ' ',
+        evo()->filter->strip_tags(
+            evo()->filter->parseDocumentSource($value)
+        )
+    )
+);
 $content = preg_replace('@\s+@',' ',$content);
 
 $content_org = $content;
 
-$strlen = $modx->filter->strlen($content);
+$strlen = evo()->filter->strlen($content);
 $limit = $strlen<$limit ? $strlen : $limit;
 $p = strpos($content,'。')!==false ? '[。！？]+' : '[\.\!\?\s]+';
-if(!preg_match('@('.$p.')@u', $content)) return $modx->filter->substr($content, 0, $limit);
+if(!preg_match('@('.$p.')@u', $content)) {
+    return evo()->filter->substr($content, 0, $limit);
+}
 
 $_ = preg_split('@('.$p.')@u', $content, -1, PREG_SPLIT_DELIM_CAPTURE);
 $content='';
 foreach($_ as $i=>$v) {
-    $isToken = preg_match('@('.$p.')@u', $v) ? 1 : 0;
-    
-    if($modx->filter->strlen($content.$v)<$limit || $isToken) $content .= $v;
-    else                                       break;
+    if(evo()->filter->strlen($content.$v)<$limit) {
+        $content .= $v;
+        continue;
+    }
+    if(preg_match('@('.$p.')@u', $v)) {
+        $content .= $v;
+        continue;
+    }
+    break;
 }
 
-if($limit < $modx->filter->strlen($content)) $content = $modx->filter->substr($content, 0, $limit);
+if($limit < evo()->filter->strlen($content)) {
+    $content = evo()->filter->substr($content, 0, $limit);
+}
 
-if(trim($content)=='') $content = $modx->filter->substr($content_org, 0, $limit-4).' ...';
+if(trim($content)=='') {
+    $content = evo()->filter->substr($content_org, 0, $limit - 4) . ' ...';
+}
 
 return $content;
