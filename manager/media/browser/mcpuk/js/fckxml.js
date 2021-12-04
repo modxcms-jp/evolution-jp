978061b17336d29b5de3a249f53ca17eee681dcd
yamamoto <yamamoto@kyms.jp>
2021-12-04 18:09:50 +0900

    Code cleanup

--- manager/media/browser/mcpuk/js/fckxml.js
+++ manager/media/browser/mcpuk/js/fckxml.js
@@ -18,87 +18,77 @@
  * 		Frederico Caldeira Knabben (fredck@fckeditor.net)
  */
 
-var FCKXml = function()
-{}
+var FCKXml = function () { }
 
 function escapeHTML(text) {
-text=text.replace('\n','');
-text=text.replace('&','&amp;');
-text=text.replace('<','&lt;');
-text=text.replace('>','&gt;');
-return text;
+	text = text.replace('\n', '');
+	text = text.replace('&', '&amp;');
+	text = text.replace('<', '&lt;');
+	text = text.replace('>', '&gt;');
+	return text;
 }
 
-FCKXml.prototype.GetHttpRequest = function()
-{
-	if ( window.XMLHttpRequest )		// Gecko
-		return new XMLHttpRequest() ;
-	else if ( window.ActiveXObject )	// IE
-		return new ActiveXObject("MsXml2.XmlHttp") ;
+FCKXml.prototype.GetHttpRequest = function () {
+	if (window.XMLHttpRequest)		// Gecko
+		return new XMLHttpRequest();
+	else if (window.ActiveXObject)	// IE
+		return new ActiveXObject("MsXml2.XmlHttp");
 }
 
-FCKXml.prototype.LoadUrl = function( urlToCall, asyncFunctionPointer )
-{
-	var oFCKXml = this ;
+FCKXml.prototype.LoadUrl = function (urlToCall, asyncFunctionPointer) {
+	var oFCKXml = this;
 
-	var bAsync = ( typeof(asyncFunctionPointer) == 'function' ) ;
+	var bAsync = (typeof (asyncFunctionPointer) == 'function');
 
-	var oXmlHttp = this.GetHttpRequest() ;
-	oXmlHttp.open( "GET", urlToCall, bAsync ) ;
-	
-	if ( bAsync )
-	{
-		oXmlHttp.onreadystatechange = function() 
-		{
-			if ( oXmlHttp.readyState == 4 )
-			{
-				oFCKXml.DOMDocument = oXmlHttp.responseXML ;
-				asyncFunctionPointer( oFCKXml ) ;
+	var oXmlHttp = this.GetHttpRequest();
+	oXmlHttp.open("GET", urlToCall, bAsync);
+
+	if (bAsync) {
+		oXmlHttp.onreadystatechange = function () {
+			if (oXmlHttp.readyState == 4) {
+				oFCKXml.DOMDocument = oXmlHttp.responseXML;
+				asyncFunctionPointer(oFCKXml);
 			}
 		}
 	}
-	
-	oXmlHttp.send( null ) ;
-	
-	if ( ! bAsync )
-		this.DOMDocument = oXmlHttp.responseXML ;
+
+	oXmlHttp.send(null);
+
+	if (!bAsync)
+		this.DOMDocument = oXmlHttp.responseXML;
 }
 
-FCKXml.prototype.SelectNodes = function( xpath )
-{
-	if ( document.all )		// IE
-		return this.DOMDocument.selectNodes( xpath ) ;
+FCKXml.prototype.SelectNodes = function (xpath) {
+	if (document.all)		// IE
+		return this.DOMDocument.selectNodes(xpath);
 	else					// Gecko
 	{
 		var aNodeArray = new Array();
 
-		var xPathResult = this.DOMDocument.evaluate( xpath, this.DOMDocument, 
-				this.DOMDocument.createNSResolver(this.DOMDocument.documentElement), XPathResult.ORDERED_NODE_ITERATOR_TYPE, null) ;
-		if ( xPathResult ) 
-		{
-			var oNode = xPathResult.iterateNext() ;
- 			while( oNode )
- 			{
- 				aNodeArray[aNodeArray.length] = oNode ;
- 				oNode = xPathResult.iterateNext();
- 			}
-		} 
-		return aNodeArray ;
+		var xPathResult = this.DOMDocument.evaluate(xpath, this.DOMDocument,
+			this.DOMDocument.createNSResolver(this.DOMDocument.documentElement), XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+		if (xPathResult) {
+			var oNode = xPathResult.iterateNext();
+			while (oNode) {
+				aNodeArray[aNodeArray.length] = oNode;
+				oNode = xPathResult.iterateNext();
+			}
+		}
+		return aNodeArray;
 	}
 }
 
-FCKXml.prototype.SelectSingleNode = function( xpath ) 
-{
-	if ( document.all )		// IE
-		return this.DOMDocument.selectSingleNode( xpath ) ;
+FCKXml.prototype.SelectSingleNode = function (xpath) {
+	if (document.all)		// IE
+		return this.DOMDocument.selectSingleNode(xpath);
 	else					// Gecko
 	{
-		var xPathResult = this.DOMDocument.evaluate( xpath, this.DOMDocument,
-				this.DOMDocument.createNSResolver(this.DOMDocument.documentElement), 9, null);
+		var xPathResult = this.DOMDocument.evaluate(xpath, this.DOMDocument,
+			this.DOMDocument.createNSResolver(this.DOMDocument.documentElement), 9, null);
 
-		if ( xPathResult && xPathResult.singleNodeValue )
-			return xPathResult.singleNodeValue ;
+		if (xPathResult && xPathResult.singleNodeValue)
+			return xPathResult.singleNodeValue;
 		else
-			return null ;
+			return null;
 	}
 }
