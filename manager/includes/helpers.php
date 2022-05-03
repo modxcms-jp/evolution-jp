606f55dbf285c0cbd3d805c40acd44c0e7848824
yamamoto <yamamoto@kyms.jp>
2022-05-03 11:42:19 +0900

    remove set()

--- manager/includes/helpers.php
+++ manager/includes/helpers.php
@@ -154,33 +154,6 @@ function alert()
     return $e;
 }
 
-function set(&$array, $key, $value)
-{
-    if (is_null($key)) {
-        return $array = $value;
-    }
-
-    $keys = explode('.', $key);
-
-    foreach ($keys as $i => $key) {
-        if (count($keys) === 1) {
-            break;
-        }
-
-        unset($keys[$i]);
-
-        if (!isset($array[$key]) || !is_array($array[$key])) {
-            $array[$key] = [];
-        }
-
-        $array = &$array[$key];
-    }
-
-    $array[array_shift($keys)] = $value;
-
-    return $array;
-}
-
 function array_get($array, $key = null, $default = null, $validate = null)
 {
     if ($key === null || trim($key) == '') {
