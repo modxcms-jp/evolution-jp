33f01e87d5a8a424d452fb7f05cfb62fe62f6e36
Yamamoto <yamamoto@kyms.jp>
2022-11-23 15:43:33 +0900

    refactor xml.format.inc.php

--- assets/snippets/ditto/formats/xml.format.inc.php
+++ assets/snippets/ditto/formats/xml.format.inc.php
@@ -1,141 +1,161 @@
 <?php
-
-/*
- * Title: XML
- * Purpose:
- *  	Collection of parameters, functions, and classes that expand
- *  	Ditto's output capabilities to include XML
-*/
-
 if (!defined('MODX_BASE_PATH') || strpos(str_replace('\\', '/', __FILE__), MODX_BASE_PATH) !== 0) exit;
 
-$modx->documentObject['contentType'] = 'application/xml';
-
-// set placeholders
-$xml_placeholders['[+xml_copyright+]'] = isset($copyright) ? $copyright : $_lang['default_copyright'];
-/*
-	Param: copyright
-
-	Purpose:
-	Copyright message to embed in the XML feed
-
-	Options:
-	Any text
-
-	Default:
-	[LANG]
-*/
-$xml_placeholders['[+xml_lang+]'] = (isset($abbrLanguage)) ? $abbrLanguage : $_lang['abbr_lang'];
-/*
-	Param: abbrLanguage
-
-	Purpose:
-	Language for the XML feed
-
-	Options:
-	Any valid 2 character language abbreviation
-
-	Default:
-	[LANG]
-
-	Related:
-	- <language>
-*/
-$xml_placeholders['[+xml_link+]'] = $modx->config['site_url'] . "[~" . $modx->documentObject['id'] . "~]";
-$xml_placeholders['[+xml_ttl+]'] = isset($ttl) ? intval($ttl) : 120;
-/*
-	Param: ttl
-
-	Purpose:
-	Time to live for the RSS feed
-
-	Options:
-	Any integer greater than 1
-
-	Default:
-	120
-*/
-$xml_placeholders['[+xml_charset+]'] = isset($charset) ? $charset : $modx->config['modx_charset'];
-/*
-	Param: charset
-
-	Purpose:
-	Charset to use for the RSS feed
-
-	Options:
-	Any valid charset identifier
-
-	Default:
-	MODX default charset
-*/
-$rss_placeholders['[+xml_xsl+]'] = isset($xsl) ? '<?xml-stylesheet type="text/xsl" href="' . $modx->config['site_url'] . $xsl . '" ?>' : '';
-/*
-	Param: xsl
-
-	Purpose:
-	XSL Stylesheet to format the XML feed with
-
-	Options:
-	The path to any valid XSL Stylesheet
-
-	Default:
-	None
-*/
-
-// set tpl xml placeholders
-$placeholders['*'] = "xml_parameters";
-if (!function_exists("xml_parameters")) {
-    function xml_parameters($placeholders)
-    {
-        global $modx;
-        $xmlArr = array();
-        foreach ($placeholders as $name => $value) {
-            $xmlArr["xml_" . $name] = htmlentities($value, ENT_NOQUOTES, $modx->config["modx_charset"]);
-        }
-        $placeholders = array_merge($xmlArr, $placeholders);
-        return $placeholders;
-    }
+if(PHP_MAJOR_VERSION < 7) {
+    exit('PHP 7 or higher required');
 }
-// set default templates
 
-$xml_header = <<<TPL
-<?xml version="1.0" encoding="[+xml_charset+]" ?>
-[+xml_xsl+]
-<xml version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
-		<channel>
-				<title>[*pagetitle*]</title>
-				<link>[+xml_link+]</link>
-				<description>[*description*]</description>
-				<language>[+xml_lang+]</language>
-				<copyright>[+xml_copyright+]</copyright>
-				<ttl>[+xml_ttl+]</ttl>
-TPL;
+/**
+ * XML format
+ *
+ * [Ditto?format=xml]
+ *
+ * Collection of parameters, functions, and classes that expand
+ * Ditto's output capabilities to include XML
+ *
+ * @link https://modx.jp/docs/extras/snippets/ditto/params/main/body.html
+*/
 
-$xml_tpl = <<<TPL
-		<item>
-			<title>[+xml_pagetitle+]</title>
-			<link>[(site_url)][~[+id+]~]</link>
-			<guid isPermaLink="true">[(site_url)][~[+id+]~]</guid>
-			<summary><![CDATA[ [+xml_introtext+] ]]></summary>
-			<date>[+xml_createdon+]</date>
-			<createdon>[+xml_createdon+]</createdon>
-			<author>[+xml_author+]</author>
-			[+tags+]
-		</item>
-TPL;
+// additional placeholders
+/*
+    Param:   &copyright
+    Purpose: Copyright message to embed in the XML feed
+    Options: Any text
+    Default: $_lang['default_copyright']
+*/
+if(!isset($copyright)) {
+    $copyright = $_lang['default_copyright'];
+}
 
-$xml_footer = <<<TPL
-			</channel>
-</xml>
-TPL;
+/*
+    Param  : &abbrLanguage
+    Purpose: Language for the XML feed
+    Options: Any valid 2 character language abbreviation
+    Default: [LANG]
+    Related: $_lang['abbr_lang']
+*/
+if(!isset($abbrLanguage)) {
+    $abbrLanguage = $_lang['abbr_lang'];
+}
+
+/*
+    Param  : &ttl
+    Purpose: Time to live for the RSS feed
+    Options: Any integer greater than 1
+    Default: 120
+*/
+if(!isset($ttl)) {
+    $ttl = 120;
+}
+
+/*
+    Param  : &charset
+    Purpose: Charset to use for the RSS feed
+    Options: Any valid charset identifier
+    Default: MODX default charset
+*/
+if(!isset($charset)) {
+    $charset = $modx->config['modx_charset'];
+}
+
+/*
+    Param  : &xsl
+    Purpose: XSL Stylesheet to format the XML feed with
+    Options: The path to any valid XSL Stylesheet
+    Default: None
+*/
+$xsl = isset($xsl)
+    ? '<?xml-stylesheet type="text/xsl" href="' . MODX_SITE_URL . $xsl . '" ?>'
+    : ''
+;
+
 
 // set template values
+if(!isset($header)) {
+    $header = $modx->parseText(
+        dittoFormatXmlHeaderTpl(), [
+            'xml_copyright' => $copyright,
+            'xml_lang'      => $abbrLanguage,
+            'xml_ttl'       => (int) $ttl,
+            'xml_charset'   => $charset,
+            'xml_xsl'       => $xsl,
+            'xml_link'      => MODX_SITE_URL . '[~' . $modx->documentIdentifier . '~]',
+        ]
+    );
+}
 
-$header = isset($header) ? $header : template::replace($xml_placeholders, $xml_header);
+if(!isset($tpl)) {
+    $tpl = '@CODE:' . dittoFormatXmlTpl();
+}
 
-$tpl = isset($tpl) ? $tpl : "@CODE:" . $xml_tpl;
+if(!isset($footer)) {
+    $footer = dittoFormatXmlFooterTpl();
+}
 
-$footer = isset($footer) ? $footer : $xml_footer;
+if(!isset($parents)) {
+    $parents = 0;
+}
 
 // set emptytext
 $noResults = '      ';
+
+$modx->documentObject['contentType'] = 'application/xml';
+
+
+// set tpl xml placeholders
+$placeholders['*'] = 'xml_parameters';
+
+
+
+
+if (!function_exists('xml_parameters')) {
+    function xml_parameters($placeholders) {
+        global $modx;
+        $xmlArr = [];
+        foreach ($placeholders as $name => $value) {
+            $xmlArr['xml_' . $name] = htmlentities(
+                $value,
+                ENT_NOQUOTES,
+                $modx->config['modx_charset'] ?: 'UTF-8'
+            );
+        }
+        return array_merge($xmlArr, $placeholders);
+    }
+}
+
+
+function dittoFormatXmlHeaderTpl() {
+    return <<<TPL
+<?xml version="1.0" encoding="[+xml_charset+]" ?>
+[+xml_xsl+]
+<xml version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
+    <channel>
+        <title>[*pagetitle*]</title>
+        <link>[+xml_link+]</link>
+        <description>[*description*]</description>
+        <language>[+xml_lang+]</language>
+        <copyright>[+xml_copyright+]</copyright>
+        <ttl>[+xml_ttl+]</ttl>
+TPL;
+}
+
+function dittoFormatXmlTpl() {
+    return <<< TPL
+    <item>
+        <title>[+xml_pagetitle+]</title>
+        <link>[(site_url)][~[+id+]~]</link>
+        <guid isPermaLink="true">[(site_url)][~[+id+]~]</guid>
+        <summary><![CDATA[ [+xml_introtext+] ]]></summary>
+        <lastmod>[+xml_editedon:date=`%Y-%m-%dT%H:%M:%S`+]</lastmod>
+        <author>[+xml_author+]</author>
+        [+tags+]
+    </item>
+TPL;
+}
+
+function dittoFormatXmlFooterTpl() {
+    return <<<TPL
+    </channel>
+</xml>
+TPL;
+}
