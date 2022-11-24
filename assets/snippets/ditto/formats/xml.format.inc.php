9680f4dcadbdd28442efb5dd5c582b5e5ad97542
Yamamoto <yamamoto@kyms.jp>
2022-11-24 15:20:22 +0900

    refactor

--- assets/snippets/ditto/formats/xml.format.inc.php
+++ assets/snippets/ditto/formats/xml.format.inc.php
@@ -8,7 +8,7 @@ if(PHP_MAJOR_VERSION < 7) {
 /**
  * XML format
  *
- * [Ditto?format=xml]
+ * [[Ditto?format=xml]]
  *
  * Collection of parameters, functions, and classes that expand
  * Ditto's output capabilities to include XML
@@ -16,69 +16,17 @@ if(PHP_MAJOR_VERSION < 7) {
  * @link https://modx.jp/docs/extras/snippets/ditto/params/main/body.html
 */
 
-// additional placeholders
-/*
-    Param:   &copyright
-    Purpose: Copyright message to embed in the XML feed
-    Options: Any text
-    Default: $_lang['default_copyright']
-*/
-if(!isset($copyright)) {
-    $copyright = $_lang['default_copyright'];
-}
-
-/*
-    Param  : &abbrLanguage
-    Purpose: Language for the XML feed
-    Options: Any valid 2 character language abbreviation
-    Default: [LANG]
-    Related: $_lang['abbr_lang']
-*/
-if(!isset($abbrLanguage)) {
-    $abbrLanguage = $_lang['abbr_lang'];
-}
-
-/*
-    Param  : &ttl
-    Purpose: Time to live for the RSS feed
-    Options: Any integer greater than 1
-    Default: 120
-*/
-if(!isset($ttl)) {
-    $ttl = 120;
-}
-
-/*
-    Param  : &charset
-    Purpose: Charset to use for the RSS feed
-    Options: Any valid charset identifier
-    Default: MODX default charset
-*/
-if(!isset($charset)) {
-    $charset = $modx->config['modx_charset'];
-}
-
-/*
-    Param  : &xsl
-    Purpose: XSL Stylesheet to format the XML feed with
-    Options: The path to any valid XSL Stylesheet
-    Default: None
-*/
-$xsl = isset($xsl)
-    ? '<?xml-stylesheet type="text/xsl" href="' . MODX_SITE_URL . $xsl . '" ?>'
-    : ''
-;
-
-
 // set template values
 if(!isset($header)) {
     $header = $modx->parseText(
         dittoFormatXmlHeaderTpl(), [
-            'xml_copyright' => $copyright,
-            'xml_lang'      => $abbrLanguage,
-            'xml_ttl'       => (int) $ttl,
-            'xml_charset'   => $charset,
-            'xml_xsl'       => $xsl,
+            'xml_copyright' => $copyright ?? $_lang['default_copyright'],
+            'xml_lang'      => $abbrLanguage ?? $_lang['abbr_lang'],
+            'xml_ttl'       => (int) ($ttl ?? 120),
+            'xml_charset'   => $charset ?? $modx->config['modx_charset'],
+            'xml_xsl'       => (isset($xsl)
+                            ? '<?xml-stylesheet type="text/xsl" href="' . MODX_SITE_URL . $xsl . '" ?>'
+                            : ''),
             'xml_link'      => MODX_SITE_URL . '[~' . $modx->documentIdentifier . '~]',
         ]
     );
