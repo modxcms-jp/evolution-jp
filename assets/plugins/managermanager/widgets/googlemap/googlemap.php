8eadbf7a90ba4c77d00d86fbf80c8168d96f62fe
noka <nishioka@pan-publicity.co.jp>
2023-06-10 17:14:15 +0900

    minor update googlemap

--- assets/plugins/managermanager/widgets/googlemap/googlemap.php
+++ assets/plugins/managermanager/widgets/googlemap/googlemap.php
@@ -3,50 +3,44 @@
 // mm_widget_googlemap ver 0.12
 // 2010 / Oori
 // Free for all
-//---------------------------------------------------------------------------------
-function mm_widget_googlemap($fields, $googleApiKey = '', $default = '', $roles = '', $templates = '')
-{
+//--------------------------------------------------------------------------------- 
+function mm_widget_googlemap($fields, $googleApiKey='', $default='', $roles='', $templates='') {
+	
+	global $modx, $mm_fields,$mm_current_page,$modx_lang_attribute;
+	$e = &$modx->event;
+	
+	if ($e->name==='OnDocFormRender'&&useThisRule($roles, $templates))
+	{
+		$output = '';
+		$callBack ='';
+		
+		$fields = makeArray($fields);
+		$count = tplUseTvs($mm_current_page['template'], $fields);
+		if ($count == false)
+		{
+			return;
+		}
+		
+		$output .= "//  -------------- googlemap widget ------------- \n";
+		$output .= includeJs($modx->config['base_url'] .'assets/plugins/managermanager/widgets/googlemap/googlemap.js');
 
-    global $modx, $mm_fields, $mm_current_page;
-    $e = &$modx->event;
+		foreach ($fields as $targetTv)
+		{
+			$tv_id = $mm_fields[$targetTv]['fieldname'];
+			$callBack .= "mm_gmap.init('$tv_id','$default');";
+		}
 
-    if ($e->name !== 'OnDocFormRender' || !useThisRule($roles, $templates)) {
-        return;
-    }
+      $googleApiKey = $googleApiKey ?? '';
 
-    $output = '';
-    $callBack = '';
-
-    $fields = makeArray($fields);
-    $count = tplUseTvs($mm_current_page['template'], $fields);
-    if ($count == false) {
-        return;
-    }
-
-    $output .= "//  -------------- googlemap widget ------------- \n";
-    $output .= includeJs(MODX_BASE_URL . 'assets/plugins/managermanager/widgets/googlemap/googlemap.js');
-
-    foreach ($fields as $targetTv) {
-        $tv_id = $mm_fields[$targetTv]['fieldname'];
-        $callBack .= "googlemap('$tv_id','$default');";
-    }
-
-    $params = '';
-    if (!empty($googleApiKey)) {
-        $params = 'key=' . trim($googleApiKey);
-    }
-
-    $output .= "
-    jQuery.getScript('https://www.google.com/jsapi', function()
-    {
-        google.load('maps', '3', { other_params: '" . $params . "', callback: function()
-        {
-    " .
-        $callBack
-        .
-        "
-        }});
-    });
-    ";
-    $e->output($output . "\n");    // Send the output to the browser
+	  $output .=<<<EOP
+        var gmap_script = document.createElement('script');
+        gmap_script.src = 'https://maps.googleapis.com/maps/api/js?v=3.51&key=$googleApiKey&callback=initMap';
+        gmap_script.async = true;
+        window.initMap = function(){
+          $callBack
+        }
+        document.body.appendChild(gmap_script);
+EOP;
+	  $e->output($output . "\n");	// Send the output to the browser
+	}
 }
