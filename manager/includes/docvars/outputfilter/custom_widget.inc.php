888ba358da80999ae3d19c4e661267c15a25a2ab
yamamoto <yamamoto@kyms.jp>
2024-01-14 20:31:38 +0900

    Fix widget output handling and error handling

--- manager/includes/docvars/outputfilter/custom_widget.inc.php
+++ manager/includes/docvars/outputfilter/custom_widget.inc.php
@@ -1,12 +1,19 @@
 <?php
 if (!defined('IN_PARSER_MODE') && !defined('IN_MANAGER_MODE')) exit();
 
-$widget_output = '';
+if (empty($params['output'])) {
+    throw new Exception('No output specified for widget');
+}
+
+$widget_output = null;
 $o = '';
 /* If we are loading a file */
 $params['output'] = $this->parseText($params['output'], ['value' => $value, 'tvname' => $name]);
 
-if (substr($params['output'], 0, 5) === '<?php') $params['output'] = "@EVAL:\n" . substr($params['output'], 5);
+if (substr($params['output'], 0, 5) === '<?php') {
+    $params['output'] = "@EVAL:\n" . substr($params['output'], 5);
+}
+
 $modx->tvfilter = new stdClass();
 $modx->tvfilter->vars['name'] = &$name;
 $modx->tvfilter->vars['value'] = &$value;
@@ -15,13 +22,21 @@ $modx->tvfilter->vars['docid'] = &$docid;
 
 if (substr($params['output'], 0, 5) == '@FILE') {
     $file_name = MODX_BASE_PATH . trim(substr($params['output'], 6));
-    if (is_file($file_name)) $widget_output = file_get_contents($file_name);
-    else                    $widget_output = $file_name . ' does not exist';
+    if (!is_file($file_name)) {
+        throw new Exception($file_name . ' does not exist');
+    }
+    $widget_output = file_get_contents($file_name);
 } elseif (substr($params['output'], 0, 8) == '@INCLUDE') {
     $file_name = MODX_BASE_PATH . trim(substr($params['output'], 9));
-    if (is_file($file_name)) include $file_name;
-    else                    $widget_output = $file_name . ' does not exist';
-    /* The included file needs to set $widget_output. Can be string, array, object */
+    if (!is_file($file_name)) {
+        throw new Exception($file_name . ' does not exist');
+    }
+    ob_start();
+    $return = include $file_name;
+    $incOut = ob_get_clean();
+    if ($widget_output === null) {
+        $widget_output = $incOut ?: $return;
+    }
 } elseif (substr($params['output'], 0, 6) == '@CHUNK' && $value !== '') {
     $chunk_name = trim(substr($params['output'], 7));
     $widget_output = $this->getChunk($chunk_name);
@@ -37,8 +52,6 @@ if (substr($params['output'], 0, 5) == '@FILE') {
     } else {
         $widget_output = $msg . $return;
     }
-} elseif ($params['output'] === '') {
-    return;
 } else {
     if ($value !== '') $widget_output = $params['output'];
     else            $widget_output = '';
@@ -46,13 +59,12 @@ if (substr($params['output'], 0, 5) == '@FILE') {
 
 $modx->tvfilter->vars = [];
 
-if (is_string($widget_output)) // Except @INCLUDE
-{
-    if (strpos($widget_output, '[+') !== false)
-        $widget_output = $this->parseText($widget_output, ['value' => $value, 'tvname' => $name], '[+', '+]', false);
-
-    $o = $this->parseDocumentSource($widget_output);
-} else {
-    $o = $widget_output;
+// Except @INCLUDE
+if (!is_string($widget_output)) {
+    return $widget_output;
 }
-return $o;
+if (strpos($widget_output, '[+') !== false) {
+    $widget_output = $this->parseText($widget_output, ['value' => $value, 'tvname' => $name], '[+', '+]', false);
+}
+
+return $this->parseDocumentSource($widget_output);
