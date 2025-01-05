d32bc87501bb1fea3627f92933bff8ed2c3d269a
yamamoto <yamamoto@kyms.jp>
2025-01-05 17:11:40 +0900

    fix: handle null values in lang_name variable in includeLang function

--- install/functions.php
+++ install/functions.php
@@ -36,7 +36,7 @@ function includeLang($lang_name, $dir = 'langs/')
     global $_lang;
 
     $_lang = array();
-    $lang_name = str_replace('\\', '/', $lang_name);
+    $lang_name = str_replace('\\', '/', $lang_name ?? '');
     if (strpos($lang_name, '/') !== false) {
         require_once(MODX_SETUP_PATH . 'langs/english.inc.php');
     } elseif (is_file(MODX_SETUP_PATH . $dir . $lang_name . '.inc.php')) {
