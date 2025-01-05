cabf604f1a5352210b55e6aee9bfb738d9109c30
yamamoto <yamamoto@kyms.jp>
2025-01-05 11:24:40 +0900

    fix: add writable check for temp directory in define-path.php

--- define-path.php
+++ define-path.php
@@ -1,7 +1,9 @@
 <?php
 define('MODX_BASE_PATH', str_replace('\\', '/', __DIR__) . '/');
 define('MODX_CACHE_PATH', MODX_BASE_PATH . 'temp/cache/');
-
+if (!is_writable(MODX_BASE_PATH . 'temp/')) {
+    die('Error: ' . MODX_BASE_PATH . 'temp/ is not writable.');
+}
 if (!is_dir(MODX_CACHE_PATH)) {
     mkdir(MODX_CACHE_PATH);
 }
