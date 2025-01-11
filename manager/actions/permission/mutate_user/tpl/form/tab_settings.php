ff6fa293599a4a6b896d3a693ee827892d3a3c38
yamamoto <yamamoto@kyms.jp>
2025-01-11 10:45:21 +0900

    fix: refactor user data access for improved consistency and clarity

--- manager/actions/permission/mutate_user/tpl/form/tab_settings.php
+++ manager/actions/permission/mutate_user/tpl/form/tab_settings.php
@@ -1,13 +1,16 @@
 <h2 class="tab"><?= lang('settings_users') ?></h2>
 <table class="settings">
+    <?php
+        $allowManagerAccess = user('allow_manager_access') === null || user('allow_manager_access') == 1;
+    ?>
     <tr>
         <th><?= lang('allow_mgr_access') ?></th>
         <td>
             <label><input type="radio" name="allow_manager_access"
-                          value="1" <?= !isset($user['allow_manager_access']) || $user['allow_manager_access'] == 1 ? 'checked="checked"' : '' ?> /> <?= lang('yes') ?>
+                          value="1" <?= $allowManagerAccess ? 'checked' : '' ?> /> <?= lang('yes') ?>
             </label><br/>
             <label><input type="radio" name="allow_manager_access"
-                          value="0" <?= isset($user['allow_manager_access']) && $user['allow_manager_access'] == 0 ? 'checked="checked"' : '' ?> /> <?= lang('no') ?>
+                          value="0" <?= !$allowManagerAccess ? 'checked' :'' ?> /> <?= lang('no') ?>
             </label>
             <div><?= lang('allow_mgr_access_message') ?></div>
         </td>
@@ -16,18 +19,18 @@
         <th><?= lang('user_allowed_parents') ?>:</th>
         <td>
             <input type="text" name="allowed_parents" class="inputBox"
-                   value="<?= htmlspecialchars($user['allowed_parents']) ?>"/>
+                   value="<?= htmlspecialchars(user('allowed_parents')) ?>"/>
             <div><?= lang('user_allowed_parents_message') ?></div>
         </td>
     </tr>
     <?php if (getv('a') == 12): ?>
         <tr>
             <th><?= lang('user_logincount') ?>:</th>
-            <td><?= $user['logincount'] ?></td>
+            <td><?= user('logincount') ?></td>
         </tr>
         <?php
-        if (!empty($user['lastlogin'])) {
-            $lastlogin = $modx->toDateFormat($user['lastlogin'] + $modx->config['server_offset_time']);
+        if (!empty(user('lastlogin'))) {
+            $lastlogin = $modx->toDateFormat(user('lastlogin') + $modx->config['server_offset_time']);
         } else {
             $lastlogin = '-';
         }
@@ -39,8 +42,8 @@
         <tr>
             <th><?= lang('user_failedlogincount') ?>:</th>
             <td>
-                <input type="hidden" name="failedlogincount" value="<?= $user['failedlogincount'] ?>">
-                <span id='failed'><?= $user['failedlogincount'] ?></span>&nbsp;&nbsp;&nbsp;[<a
+                <input type="hidden" name="failedlogincount" value="<?= user('failedlogincount') ?>">
+                <span id='failed'><?= user('failedlogincount') ?></span>&nbsp;&nbsp;&nbsp;[<a
                     href="javascript:resetFailed()"><?= lang('reset_failedlogins') ?></a>]
             </td>
         </tr>
@@ -68,7 +71,7 @@
             <th><?= lang('user_blockedafter') ?>:</th>
             <td>
                 <input type="text" id="blockedafter" name="blockedafter" class="DatePicker"
-                       value="<?php echo($user['blockedafter'] ? $modx->toDateFormat($user['blockedafter']) : ""); ?>"
+                       value="<?php echo(user('blockedafter') ? $modx->toDateFormat(user('blockedafter')) : ""); ?>"
                        onblur='documentDirty=true;' readonly="readonly">
                 <a onclick="document.userform.blockedafter.value=''; return true;"
                    style="cursor:pointer; cursor:hand"><img align="absmiddle"
@@ -84,7 +87,7 @@
                     id="blockeduntil"
                     name="blockeduntil"
                     class="DatePicker"
-                    value="<?php echo($user['blockeduntil'] ? $modx->toDateFormat($user['blockeduntil']) : ''); ?>"
+                    value="<?php echo(user('blockeduntil') ? $modx->toDateFormat(user('blockeduntil')) : ''); ?>"
                     onblur='documentDirty=true;' readonly="readonly">
                 <a onclick="document.userform.blockeduntil.value=''; return true;"
                    style="cursor:pointer; cursor:hand"><img align="absmiddle"
@@ -96,24 +99,24 @@
     <tr>
         <th><?= lang('login_allowed_ip') ?></th>
         <td><input type="text" maxlength='255' style="width: 300px;" name="allowed_ip"
-                   value="<?= $user['allowed_ip'] ?>"/>
+                   value="<?= user('allowed_ip') ?>"/>
             <div><?= lang('login_allowed_ip_message') ?></div>
         </td>
     </tr>
     <tr>
         <th><?= lang('login_allowed_days') ?></th>
         <td>
-            <?= checkbox('allowed_days[]', '1', lang('sunday'), strpos($user['allowed_days'], '1') !== false) ?>
-            <?= checkbox('allowed_days[]', '2', lang('monday'), strpos($user['allowed_days'], '2') !== false) ?>
+            <?= checkbox('allowed_days[]', '1', lang('sunday'), strpos(user('allowed_days'), '1') !== false) ?>
+            <?= checkbox('allowed_days[]', '2', lang('monday'), strpos(user('allowed_days'), '2') !== false) ?>
             <?= checkbox('allowed_days[]', '3', lang('tuesday'),
-                strpos($user['allowed_days'], '3') !== false); ?>
+                strpos(user('allowed_days'), '3') !== false); ?>
             <?= checkbox('allowed_days[]', '4', lang('wednesday'),
-                strpos($user['allowed_days'], '4') !== false); ?>
+                strpos(user('allowed_days'), '4') !== false); ?>
             <?= checkbox('allowed_days[]', '5', lang('thursday'),
-                strpos($user['allowed_days'], '5') !== false); ?>
-            <?= checkbox('allowed_days[]', '6', lang('friday'), strpos($user['allowed_days'], '6') !== false) ?>
+                strpos(user('allowed_days'), '5') !== false); ?>
+            <?= checkbox('allowed_days[]', '6', lang('friday'), strpos(user('allowed_days'), '6') !== false) ?>
             <?= checkbox('allowed_days[]', '7', lang('saturday'),
-                strpos($user['allowed_days'], '7') !== false); ?>
+                strpos(user('allowed_days'), '7') !== false); ?>
             <div><?= lang('login_allowed_days_message') ?></div>
         </td>
     </tr>
