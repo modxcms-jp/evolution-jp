381e737f7cfdb5db89cc3c6437092d15ec03e854
yamamoto <yamamoto@kyms.jp>
2025-02-11 23:00:53 +0900

    fix: simplify include statements for improved readability

--- install/connection.servertest.php
+++ install/connection.servertest.php
@@ -2,9 +2,9 @@
 include '../define-path.php';
 define('MODX_API_MODE', true);
 define('MODX_SETUP_PATH', MODX_BASE_PATH . 'install/');
-include_once(MODX_BASE_PATH . 'manager/includes/document.parser.class.inc.php');
+include_once MODX_BASE_PATH . 'manager/includes/document.parser.class.inc.php';
 $modx = new DocumentParser;
-require_once(MODX_BASE_PATH . 'install/functions.php');
+require_once MODX_BASE_PATH . 'install/functions.php';
 $_lang = includeLang(sessionv('install_language', 'english'));
 $modx->db->hostname = postv('host', 'localhost');
 $modx->db->username = postv('uid', 'root');
