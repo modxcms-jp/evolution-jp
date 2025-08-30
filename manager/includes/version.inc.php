2efa6a5bdeedb56ab6168a04b1caf6772ecf74e2
yamamoto <yamamoto@kyms.jp>
2025-08-30 13:02:39 +0900

    fix: update release date to 2025-08-30 in version.inc.php

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 $modx_version = '1.2.0J';    // Current version number
-$modx_release_date = '2024-05-24'; // Date of release
+$modx_release_date = '2025-08-30'; // Date of release
 $modx_branch = 'Evolution';  // Codebase name
 $modx_full_appname = sprintf('MODX %s %s (%s)', $modx_branch, $modx_version, $modx_release_date);
