e8a682a78ed8bdb29c4e5de0bc41ba86c10515d2
yamamoto <yamamoto@kyms.jp>
2025-12-21 15:23:43 +0900

    fix: update release date in version file to reflect correct date

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 $modx_version = '1.2.0J';    // Current version number
-$modx_release_date = '2025-11-02'; // Date of release
+$modx_release_date = '2025-12-21'; // Date of release
 $modx_branch = 'Evolution';  // Codebase name
 $modx_full_appname = sprintf('MODX %s %s (%s)', $modx_branch, $modx_version, $modx_release_date);
