bee7331a1cf8b556391a9658f14294c1ba09a643
yamamoto <yamamoto@kyms.jp>
2026-05-05 00:11:59 +0900

    fix(version): バージョン番号を1.2.0Jから1.2.1Jに更新し、リリース日を2025-12-27から2026-05-05に変更

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.2.0J';    // Current version number
-$modx_release_date = '2025-12-27'; // Date of release
+$modx_version = '1.2.1J';    // Current version number
+$modx_release_date = '2026-05-05'; // Date of release
 $modx_branch = 'Evolution';  // Codebase name
 $modx_full_appname = sprintf('MODX %s %s (%s)', $modx_branch, $modx_version, $modx_release_date);
