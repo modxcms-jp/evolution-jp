551917d4923f55d2e962eed141361f79b0015523
yamamoto <yamamoto@kyms.jp>
2026-05-13 14:41:13 +0900

    chore(version): バージョン番号を1.2.2Jに更新し、リリース日を2026-05-13に変更

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.2.1J';    // Current version number
-$modx_release_date = '2026-05-05'; // Date of release
+$modx_version = '1.2.2J';    // Current version number
+$modx_release_date = '2026-05-13'; // Date of release
 $modx_branch = 'Evolution';  // Codebase name
 $modx_full_appname = sprintf('MODX %s %s (%s)', $modx_branch, $modx_version, $modx_release_date);
